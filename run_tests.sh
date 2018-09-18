7f8f56f82a23eff2b5bc5ab3cf2aa6a16103bf84
Renato Lui Geh <renatogeh@gmail.com>
2018-09-17 21:55:44 -0300

    Addressed review feedback.
    Better documentation with regards to tests. run_tests.sh now has four
    possible operations: test single file, list all test files, display help
    message and run all tests. checkpatch_wrapper_test.sh now does not
    require internet connection. Tests now automatically detect if shunit2
    is in bash's $PATH. It prioritizes running the user's shunit2
    executable. In case such a script is not installed, kw's tests assume
    there exists a tests/shunit2 executable. Fixed a compatibility issue
    where older shunit2 versions do not accept single quotes for assert
    invocations. Added .gitignore for checkpatch.pl.
    
    Signed-off-by: Matheus Tavares <matheusmalavasi@uol.com.br>
    Signed-off-by: Renato Lui Geh <renatogeh@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-for test_entry in tests/*.sh; do
-  echo "Running test file $test_entry"
-  ./$test_entry
-done
+. ./tests/utils --source-only
+. ./src/miscellaneous.sh --source-only
 
+function run_tests
+{
+  local rc=0
+  for t in "$@"; do
+    echo "Running test file $t"
+    ./$t
+    if [[ "$?" -eq 1 ]]; then
+      rc=1
+    fi
+  done
+  if [[ "$rc" -ne 0 ]]; then
+    complain "Some test(s) FAILED"
+  fi
+}
+
+if [[ "$#" -eq 0 ]]; then
+  run_tests tests/*.sh
+elif [[ "$1" == "list" ]]; then
+  for t in tests/*.sh; do
+    echo "$t"
+  done
+elif [[ "$1" == "test" ]]; then
+  run_tests ${@:2}
+else
+  echo "Usage: $0 [help] [list] [test tfile1 tfile2 ... tfilen]"
+  echo "Run tests for kworkflow."
+  printf "Example: $0 test tests/kw_test.sh\n\n"
+  echo "  help - displays this help message"
+  echo "  list - lists all test files under tests/"
+  echo "  test - runs the given test files"
+fi
---
+++ tests/samples/codestyle_warning.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * codestyle.c
+ *
+ * Copyright (C) 2018 John Doe
+ *
+ * This file is used for codestyle checking.
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *args[])
+{
+	int i;
+	printf("%d\n", argc);
+	for (i = 0; i < argc; ++i)
+		printf("%s ", args[i]);
+	return 0;
+}
