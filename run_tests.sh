391d363ce0575dac3a1cb3aa9b82d249f57a59bd
Renato Lui Geh <renatogeh@gmail.com>
2018-09-19 15:02:28 -0300

    More flexible run_tests.sh arguments
    Previously run_tests.sh relied on passing the full path and filenames of
    test files when using test. E.g.:
    
    ./run_tests test tests/kw_test.sh
    
    A desired feature would be to pass only the test name (e.g. kw_test
    instead of tests/kw_test.sh). This commit allows for such invocation of
    run_tests.sh. Additionally run_tests.sh now lists only test names
    instead of full path.

--- run_tests.sh
+++ run_tests.sh
@@ -3,12 +3,22 @@
 . ./tests/utils --source-only
 . ./src/miscellaneous.sh --source-only
 
+function show_help
+{
+  echo "Usage: $0 [help] [list] [test tfile1 tfile2 ... tfilen]"
+  echo "Run tests for kworkflow."
+  printf "Example: $0 test kw_test\n\n"
+  echo "  help - displays this help message"
+  echo "  list - lists all test files under tests/"
+  echo "  test - runs the given test files"
+}
+
 function run_tests
 {
   local rc=0
-  for t in "$@"; do
-    echo "Running test file $t"
-    ./$t
+  for current_test in "${TESTS[@]}"; do
+    echo "Running test [${current_test}]"
+    ./tests/${current_test}.sh
     if [[ "$?" -eq 1 ]]; then
       rc=1
     fi
@@ -18,19 +28,27 @@ function run_tests
   fi
 }
 
+declare -a TESTS
+function strip_path
+{
+  TESTS=( )
+  for file in "$@"; do
+    local base=$(basename ${file})
+    TESTS+=("${base%.*}")
+  done
+}
+
 if [[ "$#" -eq 0 ]]; then
-  run_tests tests/*.sh
+  strip_path tests/*.sh
+  run_tests
 elif [[ "$1" == "list" ]]; then
-  for t in tests/*.sh; do
-    echo "$t"
+  strip_path tests/*.sh
+  for test_name in "${TESTS[@]}"; do
+    echo "${test_name}"
   done
 elif [[ "$1" == "test" ]]; then
-  run_tests ${@:2}
+  strip_path ${@:2}
+  run_tests
 else
-  echo "Usage: $0 [help] [list] [test tfile1 tfile2 ... tfilen]"
-  echo "Run tests for kworkflow."
-  printf "Example: $0 test tests/kw_test.sh\n\n"
-  echo "  help - displays this help message"
-  echo "  list - lists all test files under tests/"
-  echo "  test - runs the given test files"
+  show_help
 fi
