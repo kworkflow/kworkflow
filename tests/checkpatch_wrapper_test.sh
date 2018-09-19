1aae9cd5963a82f5dd9e7999c8aa67cf4b19e88f
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-09-19 14:45:26 -0300

    Aesthetics and standardization in checkpatch tests
    Added comments explaning the checkpatch messages used
    in the test. Splitted hashtable declaration in one
    variable per line, for better readability. Standardized
    sample codes for checkpatch test with same funcionalities.
    
    Signed-off-by: Matheus Tavares <matheusmalavasi@uol.com.br>

--- tests/checkpatch_wrapper_test.sh
+++ tests/checkpatch_wrapper_test.sh
@@ -12,13 +12,20 @@ function suite
   suite_addTest "testChecks"
 }
 
-declare -A MSG=( ["correct"]=CORRECT_MSG ["warning"]=WARNING_MSG ["error"]=ERROR_MSG \
-  ["check"]=CHECK_MSG )
-
+# Those variables hold the last line execute_checkpatch prints in a code that is correct, has
+# 1 warning, has 1 erros and has 1 check, respectively. The sample codes used in this test are
+# in tests/samples/
 CORRECT_MSG="========================================================="
-WARNING_MSG="total: 0 errors, 1 warnings, 0 checks, 20 lines checked"
-ERROR_MSG="total: 1 errors, 0 warnings, 0 checks, 20 lines checked"
+WARNING_MSG="total: 0 errors, 1 warnings, 0 checks, 25 lines checked"
+ERROR_MSG="total: 1 errors, 0 warnings, 0 checks, 25 lines checked"
 CHECK_MSG="total: 0 errors, 0 warnings, 1 checks, 26 lines checked"
+
+declare -A MSG=( \
+    ["correct"]=CORRECT_MSG \
+    ["warning"]=WARNING_MSG \
+    ["error"]=ERROR_MSG \
+    ["check"]=CHECK_MSG \
+)
 
 function checkpatch
 {
