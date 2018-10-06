cc2e42f93652ab11bbf437a482ca09a93d69d91b
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-10-06 14:25:11 -0300

    Add option not to print newline in colored_print
    Previously, colored_print and all functions that use this function in
    miscellaneous.sh (say, complain, warning and success) always printed a
    trailing newline. The -n option has now been added, which when used in
    these functions will result in no newline printing.
    
    Signed-off-by: Matheus Tavares <matheusmalavasi@uol.com.br>

--- src/miscellaneous.sh
+++ src/miscellaneous.sh
@@ -1,20 +1,34 @@
-declare -r BLUECOLOR="\033[1;34;49m%s\033[m\n"
-declare -r REDCOLOR="\033[1;31;49m%s\033[m\n"
-declare -r YELLOWCOLOR="\033[1;33;49m%s\033[m\n"
-declare -r GREENCOLOR="\033[1;32;49m%s\033[m\n"
+declare -r BLUECOLOR="\033[1;34;49m%s\033[m"
+declare -r REDCOLOR="\033[1;31;49m%s\033[m"
+declare -r YELLOWCOLOR="\033[1;33;49m%s\033[m"
+declare -r GREENCOLOR="\033[1;32;49m%s\033[m"
 declare -r SEPARATOR="========================================================="
 
 # Print colored message. This function verifies if stdout
 # is open and print it with color, otherwise print it without color.
-# @param $1 ${@:2} it receives the variable defining the color
-# to be used and text message to be printed.
+#
+# @param $1 [${@:2}] [-n ${@:3}] it receives the variable defining
+# the color to be used and two optional params:
+#   - the option '-n', to not output the trailing newline
+#   - text message to be printed
+#
 function colored_print()
 {
-    message="${@:2}"
-    if [ -t 1 ]; then
-      printf ${!1} "$message"
+    local message="${@:2}"
+
+    if [[ $# -ge 2 && $2 = "-n" ]]; then
+        message="${@:3}"
+        if [ -t 1 ]; then
+            printf ${!1} "$message"
+        else
+            echo -n "$message"
+        fi
     else
-      echo "$message"
+        if [ -t 1 ]; then
+            printf "${!1}\n" "$message"
+        else
+            echo "$message"
+        fi
     fi
 }
 
