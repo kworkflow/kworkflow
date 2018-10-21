6476d005646f25dae2e4c0b57d8231c9829bf7ba
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-10-20 23:45:08 -0300

    Refactor and improve prints at get_maintainer test
    This patch refactors the code at get_maintainer_wrapper_test.sh to be
    smaller and simpler. Also, before printing some command outputs composed
    of multiple lines, they are, now, prefixed with ">" for better
    readability. This is done with a function 'prefix_multiline' added to
    tests/utils.
    
    Signed-off-by: Matheus Tavares <matheusmalavasi@uol.com.br>

--- tests/get_maintainer_wrapper_test.sh
+++ tests/get_maintainer_wrapper_test.sh
@@ -11,52 +11,37 @@ function suite
   suite_addTest "testPrintFileAuthorForDir"
 }
 
-# Hold the the lines print_files_authors should print when given the file
-# samples/print_file_author_test_dir directory and
-# samples/print_file_author_test_dir/code1.c file, respectively
-CORRECT_DIR_MSG=(
-    "========================================================="
-    "MODULE AUTHORS:"
-    "code1.c: John Doe <johndoe@community.com>, Caesar Doe <caesar@community.com>, Michael Doe <michael@community.com>"
-    "code2.c: Bob Hilson <bob@opensource.com>"
-)
-CORRECT_FILE_MSG=(
-    "========================================================="
-    "MODULE AUTHORS:"
-    "code1.c: John Doe <johndoe@community.com>, Caesar Doe <caesar@community.com>, Michael Doe <michael@community.com>"
-)
+# The following variables hold the the lines print_files_authors should
+# print when given the file samples/print_file_author_test_dir directory
+# and samples/print_file_author_test_dir/code1.c file, respectively.
+CORRECT_DIR_MSG="=========================================================
+MODULE AUTHORS:
+code1.c: John Doe <johndoe@community.com>, Caesar Doe <caesar@community.com>, Michael Doe <michael@community.com>
+code2.c: Bob Hilson <bob@opensource.com>"
+CORRECT_FILE_MSG="=========================================================
+MODULE AUTHORS:
+code1.c: John Doe <johndoe@community.com>, Caesar Doe <caesar@community.com>, Michael Doe <michael@community.com>"
 
 function testPrintFileAuthorForFile
 {
-  local counter=0
-  while read -r line
-  do
-    local expected=${CORRECT_FILE_MSG[$counter]}
-    if [[ "$line" != "$expected" ]]; then
-        fail "Expecting line $counter to be:\n\"$expected\"\nBut got:\n\"$line\""
-        true # Reset return value
-        return
-    fi
-    ((counter+=1))
-  done < <(print_files_authors "tests/samples/print_file_author_test_dir/code1.c")
+  local -r ret=$(print_files_authors "tests/samples/print_file_author_test_dir/code1.c")
+  if [[ "$ret" != "$CORRECT_FILE_MSG" ]]; then
+    local -r expected_prefixed=$(prefix_multiline "$CORRECT_FILE_MSG")
+    local -r got_prefixed=$(prefix_multiline "$ret")
+    fail "Expecting return:\n$expected_prefixed\nBut got:\n$got_prefixed"
+  fi
   true # Reset return value
 }
 
 function testPrintFileAuthorForDir
 {
-    local counter=0
-    while read -r line
-    do
-      local expected=${CORRECT_DIR_MSG[$counter]}
-      if [[ "$line" != "$expected" ]]; then
-          fail "Expecting line $counter to be:\n\"$expected\"\nBut got:\n\"$line\""
-          true # Reset return value
-          return
-      fi
-      ((counter+=1))
-    done < <(print_files_authors "tests/samples/print_file_author_test_dir")
-    true # Reset return value
+  local -r ret=$(print_files_authors "tests/samples/print_file_author_test_dir")
+  if [[ "$ret" != "$CORRECT_DIR_MSG" ]]; then
+    local -r expected_prefixed=$(prefix_multiline "$CORRECT_DIR_MSG")
+    local -r got_prefixed=$(prefix_multiline "$ret")
+    fail "Expecting return:\n$expected_prefixed\nBut got:\n$got_prefixed"
+  fi
+  true # Reset return value
 }
-
 
 invoke_shunit
