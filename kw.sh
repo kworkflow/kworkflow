33e0086c8d9a16f7e1d8f05223219c7958dfa1bd
Shayenne Moura <shayenneluzmoura@gmail.com>
2019-01-30 15:32:10 -0200

    Improve autocomplete for kw
    Add file completion functionality to kw autocomplete.
    
    v2:
      - Add Siqueira's suggestions
      - Comment unnecessary if
    
    Signed-off-by: Shayenne Moura <shayenneluzmoura@gmail.com>

--- kw.sh
+++ kw.sh
@@ -9,8 +9,29 @@ config_files_path=${config_files_path:-"$HOME/.config/$EASY_KERNEL_WORKFLOW/etc"
 # Export external variables required by kworkflow
 export EASY_KERNEL_WORKFLOW
 
-complete -W "export e build b bi install i prepare p new n ssh s mail mount mo
-             umount um boot bo vars v up u codestyle c maintainers m help h" kw
+_kw_autocomplete()
+{
+    local current_command previous_command kw_options
+    COMPREPLY=()
+    current_command="${COMP_WORDS[COMP_CWORD]}"
+    previous_command="${COMP_WORDS[COMP_CWORD-1]}"
+    kw_options="export e build b bi install i prepare p new n ssh s
+                mail mount mo umount um boot bo vars v up u codestyle c
+                maintainers m help h"
+
+    # By default, autocomplete with kw_options
+    if [[ ${previous_command} == kw ]] ; then
+        COMPREPLY=( $(compgen -W "${kw_options}" -- ${current_command}) )
+        return 0
+    fi
+
+    # For maintainers and m options, autocomplete with folder
+    # if [ ${previous_command} == maintainers ] || [ ${previous_command} == m ] ; then
+    #   COMPREPLY=( $(compgen -d -- ${current_command}) )
+    #   return 0
+    # fi
+}
+complete -o default -F _kw_autocomplete kw
 
 function kw()
 {
