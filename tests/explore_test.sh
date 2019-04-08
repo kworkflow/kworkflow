67710c9705ac3524277ee98ec42411c8668facfe
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2019-04-08 17:42:15 -0300

    Update explore_test to get only the first match
    The explore_test.sh have a particular case that checks for the word GNU
    in the LICENSE file. After we changed the LICENSE from GPLv3 to GPLv2
    this test started to break, because GPLv2 has a double match in the test
    case instead of one. With this idea in mind, this commit adds the '-m'
    flag to the grep command with match limit of one.
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- tests/explore_test.sh
+++ tests/explore_test.sh
@@ -14,7 +14,7 @@ function testExplore
   local LOG_OUT="Initial commit"
 
   assertEquals "Expected an error message." "Expected path or 'log'" "$(explore)"
-  assertEquals "$MSG_OUT nopath" "$(explore LICENSE | grep "$MSG_OUT" | awk '{print $2}') nopath"
+  assertEquals "$MSG_OUT nopath" "$(explore LICENSE | grep -m 1 "$MSG_OUT" | awk '{print $2}') nopath"
   assertEquals "$MSG_OUT path" "$(explore "$MSG_OUT" tests | grep "$MSG_OUT" | cut -d '"' -f2) path"
   assertEquals "$LOG_OUT" "$(explore log LICENSE | grep "Initial commit" | awk '{print $1, $2}')"
   true
