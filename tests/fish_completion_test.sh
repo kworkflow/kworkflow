650f716132d221e787061e54e53d0c58e8805253
Lucas Oshiro <lucasseikioshiro@gmail.com>
2019-04-16 19:16:30 -0300

    src: autocomplete: Remove auxiliary file
    Remove tests/_fish_completion.fish, and add its content into
    getSourceCompletions function from fish_completion_test.sh.
    
    Signed-off-by: Lucas Oshiro <lucasseikioshiro@gmail.com>

--- tests/fish_completion_test.sh
+++ tests/fish_completion_test.sh
@@ -7,6 +7,20 @@ function suite()
     suite_addTest "testKwCompletion"
 }
 
+function getSortedCompletions()
+{
+    local env_path=".:/bin"
+    local env_fish_complete_path=""
+    local completion="./etc/fish_completion/kw.fish"
+    local cmd=$1
+    local fish_cmd="set PATH $env_path;
+                    set fish_complete_path $env_fish_complete_path;
+                    source $completion;
+                    complete -C'$1 '"
+
+    fish -c "$fish_cmd" | awk '{print $1}' | sort
+}
+
 function testKwCompletion()
 {
     local kw_options="explore e build b bi install i prepare p new n ssh s
@@ -14,7 +28,7 @@ function testKwCompletion()
                       maintainers m help h"
 
     local sorted_kw_options="$(echo $kw_options | awk '{gsub(" ", "\n"); print $0}' | sort)"
-    local sorted_kw_fish_completions="$(fish ./tests/_fish_completion.fish | sort)"
+    local sorted_kw_fish_completions="$(getSortedCompletions 'kw')"
 
     if [[ "$sorted_kw_fish_completions" != "$sorted_kw_options" ]]; then
 	local difference="$(diff <(echo "$sorted_kw_options") <(echo "$sorted_kw_fish_completions"))"
