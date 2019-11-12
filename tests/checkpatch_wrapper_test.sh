f4471605c1800c711b2d53771196c3ff2a038ddb
Matheus Tavares <matheus.bernardino@usp.br>
2019-11-12 14:48:03 -0300

    tests: checkpatch: fix no codestyle error test case
    We currently use 'testNothing' to test `kw c`'s output for a file that
    doesn't contain codestyle errors. But there're some bugs limiting the
    efficiency of this test.
    
    The test is currently failing because there's a typo 'assertFail' ->
    'assertFalse'. Fixing this would make the test succeed, however, it
    still wouldn't be testing what we need in this situation, since the
    given path is invalid. (It will always succeed because the output will
    be 'Invalid path', and we only test if the output does not contain
    'total')
    
    So, instead, let's remove this case and reintroduce the 'testCorrect'
    case, added in bd93d69, which looks for a "=*" string in kw's output.
    This is still not the desired behavior, as kw should not print anything
    when no codestyle error is found. But this is what kw is currently
    doing, so let's adjust the test to check this string (at least for
    now).
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>
    Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- tests/checkpatch_wrapper_test.sh
+++ tests/checkpatch_wrapper_test.sh
@@ -8,17 +8,19 @@ function suite
   suite_addTest "testWarning"
   suite_addTest "testError"
   suite_addTest "testChecks"
-  suite_addTest "testNothing"
+  suite_addTest "testCorrect"
 }
 
-# Those variables hold the last line execute_checkpatch prints in a code that is correct, has
-# 1 warning, has 1 erros and has 1 check, respectively. The sample codes used in this test are
-# in tests/samples/
+# Those variables hold the last line execute_checkpatch prints in a code that is
+# correct, has 1 warning, has 1 erros and has 1 check, respectively. The sample
+# codes used in this test are in tests/samples/
+CORRECT_MSG="========================================================="
 WARNING_MSG="total: 0 errors, 1 warnings, 0 checks, 25 lines checked"
 ERROR_MSG="total: 1 errors, 0 warnings, 0 checks, 25 lines checked"
 CHECK_MSG="total: 0 errors, 0 warnings, 1 checks, 26 lines checked"
 
 declare -A MSG=( \
+    ["correct"]=CORRECT_MSG \
     ["warning"]=WARNING_MSG \
     ["error"]=ERROR_MSG \
     ["check"]=CHECK_MSG \
@@ -65,12 +67,6 @@ function testChecks
 function testCorrect
 {
   checkpatch "correct"
-}
-
-function testNothing
-{
-  res=$(execute_checkpatch "tests/samples/codestyle_nothing.c" 2>&1)
-  assertFail "Checkpatch should not show anything" '[[ $res =~ total ]]'
 }
 
 invoke_shunit
