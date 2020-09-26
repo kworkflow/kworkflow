9c2137b9f0d747937055ab5bfac5143c7570fa54
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2020-09-26 09:45:26 -0400

    setup: Rework setup
    This commit rework the setup script in the following areas:
    * Improve output message (noverbose and verbose option)
    * Validate command failures
    * Check package dependencies
    * Add force option
    
    Closes #65
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 declare -r APPLICATIONNAME="kw"
 declare -r SRCDIR="src"
 declare -r CONFIG_DIR="etc"
@@ -18,6 +16,64 @@ declare -r FISH_COMPLETION_PATH="$FISH_CONFIG_PATH/completions"
 declare -r CONFIGS_PATH="configs"
 
 . src/kwio.sh --source-only
+. src/kwlib.sh --source-only
+
+# List of dependences per distro
+arch_packages=(qemu bash git tar python-docutils pulseaudio libpulse dunst python-sphinx)
+debian_packages=(qemu git tar python-docutils pulseaudio-utils dunst sphinx-doc)
+
+SILENT=1
+VERBOSE=0
+FORCE=0
+
+function check_dependencies()
+{
+  local distro=$(detect_distro "/")
+  local package_list=""
+  local cmd=""
+
+  if [[ "$distro" =~ "arch" ]]; then
+    for package in "${arch_packages[@]}"; do
+      installed=$(pacman -Qs "$package" > /dev/null)
+      [[ "$?" != 0 ]] && package_list="$package $package_list"
+    done
+    cmd="pacman -S $package_list"
+  elif [[ "$distro" =~ "debian" ]]; then
+    for package in "${debian_packages[@]}"; do
+      installed=$(dpkg-query -W --showformat='${Status}\n' "$package" 2>/dev/null | grep -c "ok installed")
+      [[ "$installed" -eq 0 ]] && package_list="$package $package_list"
+    done
+    cmd="apt install $package_list"
+  else
+    warning "Unfortunately, we do not have official support for your distro (yet)"
+    warning "Please, try to find the following packages: ${arch_packages[@]}"
+    return 0
+  fi
+
+  if [[ ! -z "$package_list"  ]]; then
+    if [[ "$FORCE" == 0 ]]; then
+      if [[ $(ask_yN "Can we install the following dependencies $package_list ?") =~ "0" ]]; then
+        return 0
+      fi
+    fi
+    eval "sudo $cmd"
+  fi
+}
+
+function cmd_output_manager()
+{
+  local cmd="$1"
+  local unsilent_flag="$2"
+
+  if [[ -z "$unsilent_flag" ]]; then
+    cmd="$cmd >/dev/null"
+  else
+    echo "$cmd"
+  fi
+
+  eval "$cmd"
+  return "$?"
+}
 
 function usage()
 {
@@ -27,6 +83,8 @@ function usage()
   say "--help      | -h     Display this usage message"
   say "--install   | -i     Install $APPLICATIONNAME"
   say "--uninstall | -u     Uninstall $APPLICATIONNAME"
+  say "--verbose            Explain what is being done"
+  say "--force              Never prompt"
   say "--completely-remove  Remove $APPLICATIONNAME and all files under its responsibility"
   say "--docs               Build $APPLICATIONNAME's documentation as HTML pages into ./build"
 }
@@ -42,13 +100,12 @@ function confirm_complete_removal()
 
 function clean_legacy()
 {
-  say "Removing ..."
   local trash=$(mktemp -d)
-  local completely_remove=$1
+  local completely_remove="$1"
 
   local toDelete="$APPLICATIONNAME"
   eval "sed -i '/$toDelete/d' $HOME/.bashrc"
-  if [[ $completely_remove =~ "-d" ]]; then
+  if [[ "$completely_remove" =~ "-d" ]]; then
     mv "$INSTALLTO" "$trash"
     return 0
   fi
@@ -58,7 +115,7 @@ function clean_legacy()
     # If we have configs, we should keep it
     if [ -d "$INSTALLTO/$CONFIGS_PATH" ]; then
         for content in "$INSTALLTO"/*; do
-          if [[ $content =~ "configs" || $content =~ "statistics" ]]; then
+          if [[ "$content" =~ "configs" || "$content" =~ "statistics" ]]; then
             continue
           fi
           mv "$content" "$trash"
@@ -79,7 +136,6 @@ function clean_legacy()
 
 function setup_config_file()
 {
-  say "Setting up global configuration file"
   local config_files_path="$INSTALLTO/$CONFIG_DIR"
   local config_file_template="$config_files_path/kworkflow_template.config"
   local global_config_name="kworkflow.config"
@@ -88,8 +144,13 @@ function setup_config_file()
     cp "$config_file_template" "$config_files_path/$global_config_name"
     sed -i -e "s/USERKW/$USER/g" -e "s,INSTALLPATH,$INSTALLTO,g" \
            -e "/^#?.*/d" "$config_files_path/$global_config_name"
+    ret="$?"
+    if [[ "$ret" != 0 ]]; then
+      return "$ret"
+    fi
   else
     warning "setup could not find $config_file_template"
+    return 2
   fi
 }
 
@@ -99,29 +160,51 @@ function synchronize_fish()
 
     say "Fish detected. Setting up fish support."
     mkdir -p "$FISH_COMPLETION_PATH"
-    rsync -vr $SRCDIR/kw.fish "$FISH_COMPLETION_PATH"/kw.fish
+    cmd_output_manager "rsync -vr $SRCDIR/kw.fish $FISH_COMPLETION_PATH/kw.fish"
 
-    if ! grep -F "$kw_fish_path" "$FISH_CONFIG_PATH"/config.fish; then
+    if ! grep -F "$kw_fish_path" "$FISH_CONFIG_PATH"/config.fish > /dev/null ; then
        echo "$kw_fish_path" >> "$FISH_CONFIG_PATH"/config.fish
     fi
+}
+
+function ASSERT_IF_NOT_EQ_ZERO()
+{
+  local msg="$1"
+  local ret="$2"
+  if [[ "$ret" != 0 ]]; then
+    complain "$msg"
+    exit "$ret"
+  fi
 }
 
 # Synchronize .vim and .vimrc with repository.
 function synchronize_files()
 {
-  say "Installing ..."
+  verbose=""
 
   mkdir -p "$INSTALLTO"
 
+  [[ "$VERBOSE" == 1 ]] && verbose=1
+
   # Copy the script
-  cp $APPLICATIONNAME "$INSTALLTO"
-  rsync -vr $SRCDIR "$INSTALLTO"
-  rsync -vr $SOUNDS "$INSTALLTO"
-  rsync -vr $DOCUMENTATION "$INSTALLTO"
+  cmd_output_manager "cp $APPLICATIONNAME $INSTALLTO" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'cp $APPLICATIONNAME $INSTALLTO' failed" "$?"
+
+  cmd_output_manager "rsync -vr $SRCDIR $INSTALLTO" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $SRCDIR $INSTALLTO' failed" "$?"
+
+  cmd_output_manager "rsync -vr $SOUNDS $INSTALLTO" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $SOUNDS $INSTALLTO' failed" "$?"
+
+  cmd_output_manager "rsync -vr $DOCUMENTATION $INSTALLTO" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $DOCUMENTATION $INSTALLTO' failed" "$?"
 
   # Configuration
-  rsync -vr $CONFIG_DIR "$INSTALLTO"
+  cmd_output_manager "rsync -vr $CONFIG_DIR $INSTALLTO" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $CONFIG_DIR $INSTALLTO' failed" "$?"
+
   setup_config_file
+  ASSERT_IF_NOT_EQ_ZERO "Config file failed" "$?"
 
   if [ -f "$HOME/.bashrc" ]; then
       # Add to bashrc
@@ -158,16 +241,33 @@ EOF
 
 function install_home()
 {
+  # Check Dependencies
+  say "Checking dependencies ..."
+  check_dependencies
   # First clean old installation
   clean_legacy
   # Synchronize source files
+  say "Installing ..."
   synchronize_files
   # Update version based on the current branch
   update_version
 }
 
 # Options
-case $1 in
+for arg do
+  shift
+  if [ "$arg" = "--verbose" ]; then
+    VERBOSE=1
+    continue
+  fi
+  if [ "$arg" = "--force" ]; then
+    FORCE=1
+    continue
+  fi
+  set -- "$@" "$arg"
+done
+
+case "$1" in
   --install | -i)
     install_home
     ;;
