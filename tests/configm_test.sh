ebc175f68a708769d3dbbc8c1a0a165e59cae0e1
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2021-01-23 12:42:02 -0500

    tests: configm_test: Rework test
    This commit reworks the configm_test for improving its readability and
    maintainability.
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- tests/configm_test.sh
+++ tests/configm_test.sh
@@ -40,13 +40,12 @@ function suite
 
 function setUp()
 {
-  local -r test_path="tests/.tmp"
   local -r current_path="$PWD"
 
-  rm -rf "$test_path"
+  rm -rf "$TMP_TEST_DIR"
 
-  mkdir -p "$test_path"
-  cd "$test_path"
+  mkdir -p "$TMP_TEST_DIR"
+  cd "$TMP_TEST_DIR"
   touch .config
   echo "$CONTENT" > .config
   cd "$current_path"
@@ -54,18 +53,7 @@ function setUp()
 
 function tearDown()
 {
-  local -r test_path="tests/.tmp"
-
-  rm -rf "$test_path"
-}
-
-function test_expected_string()
-{
-  local msg="$1"
-  local expected="$2"
-  local target="$3"
-
-  assertEquals "$msg" "$target" "$expected"
+  rm -rf "$TMP_TEST_DIR"
 }
 
 function execute_config_manager_SAVE_fails_Test
@@ -73,50 +61,49 @@ function execute_config_manager_SAVE_fails_Test
   local msg_prefix=" --save"
 
   ret=$(execute_config_manager --save)
-  test_expected_string "$msg_prefix" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save --lala)
-  test_expected_string "$msg_prefix --lala" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix --lala" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -n)
-  test_expected_string "$msg_prefix -n" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -n" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -d)
-  test_expected_string "$msg_prefix -d" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -d" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -n -d)
-  test_expected_string "$msg_prefix -n -d" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -n -d" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -n -lulu)
-  test_expected_string "$msg_prefix -n -lulu" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -n -lulu" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -d)
-  test_expected_string "$msg_prefix -d" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -d" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -d "lalala and xpto")
-  test_expected_string "$msg_prefix -d" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -d" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --save -f)
-  test_expected_string "$msg_prefix -f" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix -f" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 }
 
 function save_config_file_check_save_failures_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
   local force=0
   local ret=0
 
   # Test without config file -> should fail
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   rm -f .config
   ret=$(save_config_file "$NO_FORCE $NAME_1" "$DESCRIPTION_1")
-  assertEquals "No .config file should return ENOENT" "$?" "2"
+  assert_equals_helper 'No .config file should return ENOENT' "$LINENO" "$?" "2"
 
   # Test with different name
   touch .configuration
   ret=$(save_config_file "$NO_FORCE $NAME_1" "$DESCRIPTION_1")
-  assertEquals "Should return ENOENT, because '.config' != '.configuration'" "$?" "2"
+  assert_equals_helper "Should return ENOENT, because '.config' != '.configuration'" "$LINENO" "$?" "2"
   rm .configuration
 
   cd "$current_path"
@@ -124,137 +111,154 @@ function save_config_file_check_save_failures_Test()
 
 function save_config_file_check_directories_creation_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
-  config_files_path="$current_path/$test_path"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
+  local configs_path="$config_files_path/configs"
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_1 "$DESCRIPTION_1")
   cd "$current_path"
 
   # Check if all the expected files were created
-  assertTrue "The configs dir was not created" '[[ -d $config_files_path/configs ]]'
-  assertTrue "The repository configs does not have .git" '[[ -d $config_files_path/configs/.git ]]'
-  assertTrue "The metadata dir is not available" '[[ -d $config_files_path/configs/metadata ]]'
-  assertTrue "The configs dir is not available" '[[ -d $config_files_path/configs/configs ]]'
+  assertTrue "$LINENO: The configs dir was not created" '[[ -d $configs_path ]]'
+  assertTrue "$LINENO: The repository configs does not have .git" '[[ -d $configs_path/.git ]]'
+  assertTrue "$LINENO: The metadata dir is not available" '[[ -d $configs_path/metadata ]]'
+  assertTrue "$LINENO: The configs dir is not available" '[[ -d $configs_path/configs ]]'
 }
 
 function save_config_file_check_saved_config_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
+  local configs_path="$config_files_path/configs"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local msg
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_1 "$DESCRIPTION_1")
   cd "$current_path"
 
-  assertTrue "Failed to find $NAME_1" '[[ -f $config_files_path/configs/configs/$NAME_1 ]]'
-  assertTrue "Failed the metadata related to $NAME_1" '[[ -f $config_files_path/configs/metadata/$NAME_1 ]]'
+  msg="Failed to find $NAME_1"
+  assertTrue "$LINENO: $msg" '[[ -f $configs_path/configs/$NAME_1 ]]'
+  msg="Failed the metadata related to $NAME_1"
+  assertTrue "$LINENO: $msg"  '[[ -f $configs_path/metadata/$NAME_1 ]]'
 
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_2)
   cd "$current_path"
 
-  assertTrue "Failed to find $NAME_2" '[[ -f $config_files_path/configs/configs/$NAME_2 ]]'
-  assertTrue "Failed the metadata related to $NAME_2" '[[ -f $config_files_path/configs/metadata/$NAME_2 ]]'
+  msg="Failed to find $NAME_2"
+  assertTrue "$LINENO: $msg" '[[ -f $configs_path/configs/$NAME_2 ]]'
+  msg="Failed the metadata related to $NAME_2"
+  assertTrue "$LINENO: $msg" '[[ -f $configs_path/metadata/$NAME_2 ]]'
 
-  local tmp=$(cat $config_files_path/configs/configs/$NAME_2)
-  assertTrue "Content in the file does not match" '[[ $tmp = $CONTENT ]]'
+  local tmp=$(cat $configs_path/configs/$NAME_2)
+  msg="Content in the file does not match"
+  assertTrue "$LINENO: $msg" '[[ $tmp = $CONTENT ]]'
 }
 
 function save_config_file_check_description_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
+  local configs_path="$config_files_path/configs"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local msg
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_1 "$DESCRIPTION_1")
   cd "$current_path"
 
-  local tmp=$(cat $config_files_path/configs/metadata/$NAME_1)
-  assertTrue "The description content for $NAME_1 does not match" '[[ $tmp = $DESCRIPTION_1 ]]'
+  local tmp=$(cat $configs_path/metadata/$NAME_1)
+  msg="The description content for $NAME_1 does not match"
+  assertTrue "$LINENO: $msg" '[[ $tmp = $DESCRIPTION_1 ]]'
 
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_2 "$DESCRIPTION_2")
   cd "$current_path"
 
-  tmp=$(cat $config_files_path/configs/metadata/$NAME_2)
-  assertTrue "The description content for $NAME_2 does not match" '[[ $tmp = $DESCRIPTION_2 ]]'
+  tmp=$(cat $configs_path/metadata/$NAME_2)
+  msg="The description content for $NAME_2 does not match"
+  assertTrue "$LINENO: $msg" '[[ $tmp = $DESCRIPTION_2 ]]'
 }
 
 function save_config_file_check_git_save_schema_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_1 "$DESCRIPTION_1")
   ret=$(save_config_file $NO_FORCE $NAME_2 "$DESCRIPTION_2")
   cd "configs"
   ret=$(git rev-list --all --count)
   cd "$current_path"
 
-  assertTrue "We expected 2 commits, but we got $ret" '[[ $ret = "2" ]]'
+  assertTrue "$LINENO: We expected 2 commits, but we got $ret" '[[ $ret = "2" ]]'
 }
 
 function save_config_file_check_force_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $YES_FORCE $NAME_2 "$DESCRIPTION_2")
   ret=$(save_config_file $YES_FORCE $NAME_2 "$DESCRIPTION_2")
   cd "$current_path"
-  assertTrue "We expected no changes" '[[ $ret =~ Warning ]]'
+  assertTrue "$LINENO: We expected no changes" '[[ $ret =~ Warning ]]'
 }
 
 function list_config_check_when_there_is_no_config_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
 
   # There's no configs yet, initialize it
   ret=$(list_configs)
-  assertTrue "We expected no changes" '[[ $ret =~ $LS_NO_FILES ]]'
+  assertTrue "$LINENO: We expected no changes" '[[ $ret =~ $LS_NO_FILES ]]'
 }
 
 function list_config_normal_output_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local msg
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $YES_FORCE $NAME_1 "$DESCRIPTION_1")
   ret=$(save_config_file $YES_FORCE $NAME_2 "$DESCRIPTION_2")
   cd "$current_path"
 
   # There's no configs yet, initialize it
   ret=$(list_configs)
-  assertTrue "We expected 'Name' in the output, but we got $ret" '[[ $ret =~ Name ]]'
-  assertTrue "We expected 'Description' in the output, but we got $ret" '[[ $ret =~ Description ]]'
-  assertTrue "We expected $NAME_1 in the output, but we got $ret" '[[ $ret =~ $NAME_1 ]]'
-  assertTrue "We expected $DESCRIPTION_1 in the output, but we got $ret" '[[ $ret =~ $DESCRIPTION_1 ]]'
-  assertTrue "We expected $NAME_2 in the output, but we got $ret" '[[ $ret =~ $NAME_2 ]]'
-  assertTrue "We expected $DESCRIPTION_2 in the output, but we got $ret" '[[ $ret =~ $DESCRIPTION_2 ]]'
+  msg="We expected 'Name' in the output, but we got $ret"
+  assertTrue "$LINENO: $msg" '[[ $ret =~ Name ]]'
+
+  msg="We expected 'Description' in the output, but we got $ret"
+  assertTrue "$LINENO: $msg" '[[ $ret =~ Description ]]'
+
+  msg="We expected $NAME_1 in the output, but we got $ret"
+  assertTrue "$LINENO: $msg" '[[ $ret =~ $NAME_1 ]]'
+
+  msg="We expected $DESCRIPTION_1 in the output, but we got $ret"
+  assertTrue "$LINENO: $msg" '[[ $ret =~ $DESCRIPTION_1 ]]'
+
+  msg="We expected $NAME_2 in the output, but we got $ret"
+  assertTrue "$LINENO: $msg" '[[ $ret =~ $NAME_2 ]]'
+
+  msg="We expected $DESCRIPTION_2 in the output, but we got $ret"
+  assertTrue "$LINENO:$msg" '[[ $ret =~ $DESCRIPTION_2 ]]'
 }
 
 function execute_config_manager_get_config_invalid_option_Test()
@@ -262,44 +266,43 @@ function execute_config_manager_get_config_invalid_option_Test()
   local msg_prefix=" --get"
 
   ret=$(execute_config_manager --get)
-  test_expected_string "$msg_prefix" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager -get)
-  test_expected_string "$msg_prefix" "$COMMAND_MSG_UNKNOWN" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_MSG_UNKNOWN" "$ret"
 
   ret=$(execute_config_manager --get something_wrong)
-  test_expected_string "$msg_prefix" "$COMMAND_NO_SUCH_FILE: something_wrong" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_NO_SUCH_FILE: something_wrong" "$ret"
 }
 
 function get_config_with_force_Test()
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
   local ret=0
-  config_files_path="$current_path/$test_path"
 
   # There's no configs yet, initialize it
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_1 "$DESCRIPTION_1")
   ret=$(save_config_file $NO_FORCE $NAME_2 "$DESCRIPTION_2")
   cd "$current_path"
 
   # Case 1: There's no local .config file
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   rm -f .config
   get_config "$NAME_1" 1 > /dev/null 2>&1
   ret=$(cat .config)
   cd "$current_path"
 
-  assertTrue "We expected $CONTENT, but we got $ret" '[[ $ret =~ $CONTENT ]]'
+  assertTrue "$LINENO: We expected $CONTENT, but we got $ret" '[[ $ret =~ $CONTENT ]]'
 
   # Case 2: There's a .config file
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   get_config "$NAME_2" 1 > /dev/null 2>&1
   ret=$(cat .config)
   cd "$current_path"
 
-  assertTrue "We expected $CONTENT, but we got $ret" '[[ $ret =~ $CONTENT ]]'
+  assertTrue "$LINENO: We expected $CONTENT, but we got $ret" '[[ $ret =~ $CONTENT ]]'
 }
 
 function execute_config_manager_remove_that_should_fail_Test()
@@ -307,23 +310,22 @@ function execute_config_manager_remove_that_should_fail_Test()
   local msg_prefix=" -rm"
 
   ret=$(execute_config_manager -rm)
-  test_expected_string "$msg_prefix" "$COMMAND_MSG_INVALID_ARG" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_MSG_INVALID_ARG" "$ret"
 
   ret=$(execute_config_manager --rm)
-  test_expected_string "$msg_prefix" "$COMMAND_MSG_UNKNOWN" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_MSG_UNKNOWN" "$ret"
 
   ret=$(execute_config_manager -rm something_wrong)
-  test_expected_string "$msg_prefix" "$COMMAND_NO_SUCH_FILE: something_wrong" "$ret"
+  assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_NO_SUCH_FILE: something_wrong" "$ret"
 }
 
 function remove_config_Test
 {
-  local -r test_path="tests/.tmp"
   local current_path="$PWD"
   local ret=0
-  config_files_path="$current_path/$test_path"
+  local config_files_path="$current_path/$TMP_TEST_DIR"
 
-  cd "$test_path"
+  cd "$TMP_TEST_DIR"
   ret=$(save_config_file $NO_FORCE $NAME_1 "$DESCRIPTION_1")
   ret=$(save_config_file $NO_FORCE $NAME_2 "$DESCRIPTION_2")
   ret=$(ls configs/configs -1 | wc -l)
@@ -333,11 +335,11 @@ function remove_config_Test
   # Case 2: Remove one config file
   remove_config "$NAME_1" 1  > /dev/null 2>&1
   ret=$(ls configs/configs -1 | wc -l)
-  assertTrue "We expected , 1 files but got $ret" '[[ $ret = "1" ]]'
+  assertTrue "$LINENO: We expected , 1 files but got $ret" '[[ $ret = "1" ]]'
 
   # Case 2: Remove all config files
   remove_config "$NAME_2" 1  > /dev/null 2>&1
-  assertTrue "We expected no file related to config" '[[ ! -f configs/configs ]]'
+  assertTrue "$LINENO: We expected no file related to config" '[[ ! -f configs/configs ]]'
 
   cd "$current_path"
 }
