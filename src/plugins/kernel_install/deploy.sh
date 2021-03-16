6089a5204f2b2cdbda7ca4f5f2f0c42cd3129825
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2021-03-16 18:56:14 -0400

    plugins: kernel_install: Move code from deploy to util
    The deploy script had some generic functions that make much more sense
    to be added inside utils.sh. This commit moves those functions to
    util.sh.
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- src/plugins/kernel_install/deploy.sh
+++ src/plugins/kernel_install/deploy.sh
@@ -36,54 +36,6 @@ cd "$target_kw_deploy"
 . distro_deploy.sh --source-only
 . utils.sh --source-only
 
-# ATTENTION:
-# This function follows the cmd_manager signature (src/kwlib.sh) because we
-# share the specific distro in the kw main code. However, when we deploy for a
-# remote machine, we need this function, and this is the reason that we added
-# this function.
-function cmd_manager()
-{
-  local flag="$1"
-
-  case "$flag" in
-    SILENT)
-      shift 1
-      ;;
-    WARNING)
-      shift 1
-      echo "WARNING"
-      echo "$@"
-      ;;
-    SUCCESS)
-      shift 1
-      echo "SUCCESS"
-      echo "$@"
-      ;;
-    TEST_MODE)
-      shift 1
-      echo "$@"
-      return 0
-      ;;
-    *)
-      echo "$@"
-      ;;
-  esac
-
-  eval "$@"
-}
-
-function ask_yN()
-{
-  local message=$@
-
-  read -r -p "$message [y/N] " response
-  if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-    echo "1"
-  else
-    echo "0"
-  fi
-}
-
 case "$1" in
   --modules)
     shift # Get rid of --modules
