kworkflow/kworkflow

commit 5d75cfa3eb1b1e1045c1026f87fe92ff8b62870f

src: mk: Add support for VM deploy
Kw did not have support for VM deploy; this commit adds support to kw
deploy for ArchLinux and Debian vm space. Additionally, to achieve this
goal, this commit had to refactor some of the VM function and introduce
unit tests.

Closes #139

Changes since v2:
* fix regression in remote deployment
* update codingstyle documentation
* replace errno code to a return value on kernel deploy
* drop outdated comment
* use double quote on arguments
* include return value comment
* replace = for == on reboot condition
* change displayed information for grub update
* refactoring to use cmd_manager
* add test to vm mount and unmount

Signed-off-by: Melissa Wen <melissa.srw@gmail.com>
Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

src/plugins/kernel_install/arch.sh
@@ -23,20 +23,72 @@ function install_modules()
   fi
 }
 
+# Make initcpio and update grub on VM using Guestfish
+#
+# After configuring the handle (adding a disk image in write mode), the
+# guestfish performes the followed steps: (1) mount image;
+# (2) dracut (updates kernel images list); (3) create a dummy device.map
+# that tells Grub to look for /dev/sda; (4) install and update grub.
+#
+# Note: The virtual machine must be shut down and umounted before you use this
+# command, and disk images must not be edited concurrently.
+#
+# @name Kernel name for the deploy
+# @cmd_grub Command to update grub
+# mkinitcpio -g /boot/initramfs-linux.img -k /boot/vmlinuz-linux
+function vm_update_boot_loader()
+{
+  local name="$1"
+  local cmd_grub="$2"
+  local ret=0
+  local cmd=""
+  local mount_root=": mount /dev/sda1 /"
+  local cmd_init="dracut --regenerate-all -f"
+  local mkdir_grub=": mkdir-p /boot/grub"
+  local setup_grub=": write /boot/grub/device.map '(hd0,1) /dev/sda'"
+  local grub_install="grub-install --directory=/usr/lib/grub/i386-pc --target=i386-pc --boot-directory=/boot --recheck --debug /dev/sda"
+  local flag
+
+  flag=${flag:-"SILENT"}
+
+  sleep 0.5s
+
+  cmd="guestfish --rw -a ${configurations[qemu_path_image]} run \
+      $mount_root : command '$cmd_init' \
+      $mkdir_grub $setup_grub : command '$grub_install' \
+      : command '$cmd_grub'"
+
+  if [[ -f ${configurations[qemu_path_image]} ]]; then
+    warning " -> Updating grub and boot files for $name. This can take a few minutes. Please, be patient..."
+    {
+      cmd_manager "$flag" "$cmd"
+    } > /dev/null
+    say "Done."
+  else
+    complain "There is no VM in ${configurations[qemu_path_image]}"
+    return 125 # ECANCELED
+  fi
+}
+
 # Update boot loader API
 function update_boot_loader()
 {
   local name="$1"
-  local local="$2"
+  local target="$2"
   local flag="$3"
 
-  if [[ ! -z "$local" ]]; then
+  if [[ ! -z "$target" ]]; then
     sudo_cmd="sudo -E"
   fi
 
-  # Update grub
   cmd="$sudo_cmd grub-mkconfig -o /boot/grub/grub.cfg"
-  cmd_manager "$flag" "$cmd"
+
+  # Update grub
+  if [[ "$target" == 'vm' ]] ; then
+    vm_update_boot_loader "$name" "$cmd"
+  else
+    cmd_manager "$flag" "$cmd"
+  fi
 }
 
 # Install kernel
@@ -46,14 +98,15 @@ function install_kernel()
   local kernel_image_name="$2"
   local reboot="$3"
   local architecture="$4"
-  local local="$5"
+  local target="$5"
   local flag="$6"
   local sudo_cmd=""
   local LOCAL_KW_ETC="$KW_ETC_DIR/template_mkinitcpio.preset"
+  local path_prefix=""
 
   flag=${flag:-"SILENT"}
 
-  if [[ "$local" == 'local' ]]; then
+  if [[ "$target" == 'local' ]]; then
     sudo_cmd="sudo -E"
   fi
 
@@ -62,38 +115,56 @@ function install_kernel()
     return 22
   fi
 
+  if [[ "$target" == 'vm' ]]; then
+    # Check if vm is mounted and get its path
+    if [[ $(findmnt "${configurations[mount_point]}") ]]; then
+      path_prefix="${configurations[mount_point]}"
+    else
+      complain "Did you check if your VM is mounted?"
+      return 125 # ECANCELED
+    fi
+  fi
+
   # Copy kernel image
-  if [[ -f "/boot/vmlinuz-$name" ]]; then
-    cmd="$sudo_cmd cp /boot/vmlinuz-$name /boot/vmlinuz-$name.old"
+  if [[ -f "$path_prefix/boot/vmlinuz-$name" ]]; then
+    cmd="$sudo_cmd cp $path_prefix/boot/vmlinuz-$name $path_prefix/boot/vmlinuz-$name.old"
     cmd_manager "$flag" "$cmd"
   fi
 
-  if [[ ! -z "$local" ]]; then
+  if [[ ! -z "$target" ]]; then
     [[ -z "$architecture" ]] && architecture="x86_64"
     cmd="$sudo_cmd cp -v arch/$architecture/boot/$kernel_image_name /boot/vmlinuz-$name"
     cmd_manager "$flag" "$cmd"
   else
-    cmd="$sudo_cmd cp -v vmlinuz-$name /boot/vmlinuz-$name"
+    cmd="$sudo_cmd cp -v vmlinuz-$name $path_prefix/boot/vmlinuz-$name"
     cmd_manager "$flag" "$cmd"
   fi
 
   # Update mkinitcpio
-  if [[ ! -z "$local" ]]; then
-    cmd="$sudo_cmd cp -v $LOCAL_KW_ETC /etc/mkinitcpio.d/$name.preset"
+  if [[ ! -z "$target" ]]; then
+    cmd="$sudo_cmd cp -v $LOCAL_KW_ETC $path_prefix/etc/mkinitcpio.d/$name.preset"
     cmd_manager "$flag" "$cmd"
-    cmd="$sudo_cmd sed -i -e \"s/NAME/$name/g\" \"/etc/mkinitcpio.d/$name.preset\""
+    cmd="$sudo_cmd sed -i -e \"s/NAME/$name/g\" \"$path_prefix/etc/mkinitcpio.d/$name.preset\""
     cmd_manager "$flag" "$cmd"
   else
-    cp -v "$name.preset" /etc/mkinitcpio.d/
+    cp -v "$name.preset" $path_prefix/etc/mkinitcpio.d/
   fi
 
-  cmd="$sudo_cmd mkinitcpio -p $name"
-  cmd_manager "$flag" "$cmd"
+  if [[ "$target" != 'local' ]]; then
+    # Update mkinitcpio
+    cmd="$sudo_cmd mkinitcpio -p $name"
+    cmd_manager "$flag" "$cmd"
+  fi
 
-  update_boot_loader "$name" "$local" "$flag"
+  # If VM is mounted, umount before update boot loader
+  if [[ "$target" == 'vm' ]]; then
+    [[ $(findmnt "${configurations[mount_point]}") ]] && vm_umount
+  fi
+
+  update_boot_loader "$name" "$target" "$flag"
 
   # Reboot
-  if [[ "$reboot" = "1" ]]; then
+  if [[ "$target" != 'vm' && "$reboot" == "1" ]]; then
     cmd="$sudo_cmd reboot"
     cmd_manager "$flag" "$cmd"
   fi

src/mk.sh
@@ -17,8 +17,7 @@
 # root password.
 #
 
-. "$KW_LIB_DIR/vm.sh" --source-only # It includes kw_config_loader.sh
-. "$KW_LIB_DIR/kwlib.sh" --source-only
+. "$KW_LIB_DIR/vm.sh" --source-only # It includes kw_config_loader.sh and kwlib.sh
 . "$KW_LIB_DIR/remote.sh" --source-only
 
 # Hash containing user options
@@ -46,22 +45,6 @@ function modules_install_to()
   local cmd="make INSTALL_MOD_PATH=$install_to modules_install"
   set +e
   cmd_manager "$flag" "$cmd"
-}
-
-function vm_modules_install
-{
-  # Attention: The vm code have to be loaded before this function.
-  # Take a look at the beginning of kworkflow.sh.
-  vm_mount
-
-  if [ "$?" != 0 ] ; then
-    complain "Did you check if your VM is running?"
-    return 125 # ECANCELED
-  fi
-
-  modules_install_to "${configurations[mount_point]}"
-
-  vm_umount
 }
 
 # Get the kernel release based on the command kernel release.
@@ -113,7 +96,6 @@ function cleanup_after_deploy
 # @target Target machine
 function modules_install
 {
-  local ret
   local flag="$1"
   local target="$2"
   local formatted_remote="$3"
@@ -127,7 +109,15 @@ function modules_install
 
   case "$target" in
     1) # VM_TARGET
-      vm_modules_install
+      local distro=$(detect_distro "${configurations[mount_point]}/")
+
+      if [[ "$distro" =~ "none" ]]; then
+        complain "Unfortunately, there's no support for the target distro"
+        vm_umount
+        exit 95 # ENOTSUP
+      fi
+
+      modules_install_to "${configurations[mount_point]}" "$flag"
       ;;
     2) # LOCAL_TARGET
       cmd="sudo -E make modules_install"
@@ -275,10 +265,17 @@ function kernel_install
 
   case "$target" in
     1) # VM_TARGET
-      # TODO: See issue #139
-      echo "Unfortunately, we don't support kernel image deploy in a VM with" \
-           "libguestfs yet; however, an alternative is using the remote" \
-           "option."
+      local distro=$(detect_distro "${configurations[mount_point]}/")
+
+      if [[ "$distro" =~ "none" ]]; then
+        complain "Unfortunately, there's no support for the target distro"
+        vm_umount
+        exit 95 # ENOTSUP
+      fi
+
+      . "$KW_PLUGINS_DIR/kernel_install/$distro.sh" --source-only
+      install_kernel "$name" "$kernel_img_name" "$reboot" "$arch_target" 'vm' "$flag"
+      return "$?"
     ;;
     2) # LOCAL_TARGET
       local distro=$(detect_distro "/")
@@ -291,6 +288,7 @@ function kernel_install
       # Local Deploy
       . "$KW_PLUGINS_DIR/kernel_install/$distro.sh" --source-only
       install_kernel "$name" "$kernel_img_name" "$reboot" "$arch_target" 'local' "$flag"
+      return "$?"
     ;;
     3) # REMOTE_TARGET
       local preset_file="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$name.preset"
@@ -401,6 +399,7 @@ function kernel_deploy
   local start=0
   local end=0
   local runtime=0
+  local ret=0
 
   deploy_parser_options "$@"
   if [[ "$?" -gt 0 ]]; then
@@ -448,6 +447,15 @@ function kernel_deploy
     exit 125 # ECANCELED
   fi
 
+  if [[ "$target" == "$VM_TARGET" ]] ; then
+    vm_mount
+    ret="$?"
+    if [[ "$ret" != 0 ]] ; then
+      complain "Please shutdown or umount your VM to continue."
+      exit "$ret"
+    fi
+  fi
+
   # NOTE: If we deploy a new kernel image that does not match with the modules,
   # we can break the boot. For security reason, every time we want to deploy a
   # new kernel version we also update all modules; maybe one day we can change
@@ -468,6 +476,11 @@ function kernel_deploy
     statistics_manager "deploy" "$runtime"
   else
     statistics_manager "Modules_deploy" "$runtime"
+  fi
+
+  if [[ "$target" == "$VM_TARGET" ]] ; then
+    # Umount VM if it remains mounted
+    vm_umount
   fi
 
   if [[ "$target" == "$REMOTE_TARGET" ]]; then

src/plugins/kernel_install/debian.sh
@@ -23,20 +23,76 @@ function install_modules()
   fi
 }
 
+# Update initramfs and grub on VM using Guestfish
+#
+# After configuring the handle (adding a disk image in write mode), the
+# guestfish performs the followed steps: (1) mount image;
+# (2) update-initramfs (needs kernel image name); (3) create a dummy device.map
+# that tells Grub to look for /dev/sda; (4) install and update grub.
+#
+# Note: The virtual machine must be turned off and umounted before you use this
+# command, and disk images must not be edited concurrently.
+#
+# @name Kernel name for the deploy
+# @cmd_grub Command to update grub
+#
+# Returns: if there is no VM in the expected path, the function is
+# canceled and returns 125
+function vm_update_boot_loader()
+{
+  local name="$1"
+  local cmd_grub="$2"
+  local cmd=""
+  local mount_root=": mount /dev/sda1 /"
+  local mkdir_init=": mkdir-p /etc/initramfs-tools"
+  local cmd_init="update-initramfs -c -k $name"
+  local mkdir_grub=": mkdir-p /boot/grub"
+  local setup_grub=": write /boot/grub/device.map '(hd0) /dev/sda'"
+  local grub_install="grub-install --root-directory=/ --target=i386-pc --force /dev/sda1"
+  local flag
+
+  flag=${flag:-"SILENT"}
+
+  sleep 0.5s
+
+  cmd="guestfish --rw -a ${configurations[qemu_path_image]} run \
+      $mount_root \
+      $mkdir_init : command '$cmd_init' \
+      $setup_grub : command '$grub_install' : command '$cmd_grub'"
+
+  if [[ -f "${configurations[qemu_path_image]}" ]]; then
+    warning " -> Updating initramfs and grub for $name on VM. This can take a few minutes."
+    {
+      cmd_manager "$flag" "$cmd"
+    } 1> /dev/null # No visible stdout but still shows errors
+    say "Done."
+  else
+    complain "There is no VM in ${configurations[qemu_path_image]}"
+    return 125 # ECANCELED
+  fi
+
+  return 0
+}
+
 # Update boot loader API
 function update_boot_loader()
 {
   local name="$1"
-  local local="$2"
+  local target="$2"
   local flag="$3"
 
-  if [[ "$local" == 'local' ]]; then
+  if [[ "$target" == 'local' ]]; then
     sudo_cmd="sudo -E"
   fi
 
-  # Update grub
   cmd="$sudo_cmd grub-mkconfig -o /boot/grub/grub.cfg"
-  cmd_manager "$flag" "$cmd"
+
+  # Update grub
+  if [[ "$target" == 'vm' ]] ; then
+    vm_update_boot_loader "$name" "$cmd"
+  else
+    cmd_manager "$flag" "$cmd"
+  fi
 }
 
 # Install kernel
@@ -46,14 +102,15 @@ function install_kernel()
   local kernel_image_name="$2"
   local reboot="$3"
   local architecture="$4"
-  local local="$5"
+  local target="$5"
   local flag="$6"
   local sudo_cmd=""
   local cmd=""
+  local path_prefix=""
 
   flag=${flag:-"SILENT"}
 
-  if [[ ! -z "$local" ]]; then
+  if [[ "$target" == 'local' ]]; then
     sudo_cmd="sudo -E"
   fi
 
@@ -62,29 +119,49 @@ function install_kernel()
     return 22
   fi
 
+  if [[ "$target" == 'vm' ]]; then
+    # Check if vm is mounted and get its path
+    if [[ $(findmnt "${configurations[mount_point]}") ]]; then
+      path_prefix="${configurations[mount_point]}"
+
+      # Copy config file
+      cp -v .config "$path_prefix/boot/config-$name"
+    else
+      complain "Did you check if your VM is mounted?"
+      return 125 # ECANCELED
+    fi
+  fi
+
   # Copy kernel image
-  if [[ -f "/boot/vmlinuz-$name" ]]; then
-    cmd="$sudo_cmd cp /boot/vmlinuz-$name /boot/vmlinuz-$name.old"
+  if [[ -f "$path_prefix/boot/vmlinuz-$name" ]]; then
+    cmd="$sudo_cmd cp $path_prefix/boot/vmlinuz-$name $path_prefix/boot/vmlinuz-$name.old"
     cmd_manager "$flag" "$cmd"
   fi
 
-  if [[ ! -z "$local" ]]; then
+  if [[ ! -z "$target" ]]; then
     [[ -z "$architecture" ]] && architecture="x86_64"
     cmd="$sudo_cmd cp -v arch/$architecture/boot/$kernel_image_name /boot/vmlinuz-$name"
     cmd_manager "$flag" "$cmd"
   else
-    cmd="$sudo_cmd cp -v vmlinuz-$name /boot/vmlinuz-$name"
+    cmd="$sudo_cmd cp -v vmlinuz-$name $path_prefix/boot/vmlinuz-$name"
     cmd_manager "$flag" "$cmd"
   fi
 
-  # Update initramfs
-  cmd="$sudo_cmd update-initramfs -c -k $name"
-  cmd_manager "$flag" "$cmd"
+  if [[ "$target" != 'vm' ]]; then
+    # Update initramfs
+    cmd="$sudo_cmd update-initramfs -c -k $name"
+    cmd_manager "$flag" "$cmd"
+  fi
 
-  update_boot_loader "$name" "$local" "$flag"
+  # If VM is mounted, umount before update boot loader
+  if [[ "$target" == 'vm' ]]; then
+    [[ $(findmnt "${configurations[mount_point]}") ]] && vm_umount
+  fi
+
+  update_boot_loader "$name" "$target" "$flag"
 
   # Reboot
-  if [[ "$reboot" = "1" ]]; then
+  if [[ "$target" != 'vm' && "$reboot" == "1" ]]; then
     cmd="$sudo_cmd reboot"
     cmd_manager "$flag" "$cmd"
   fi

src/vm.sh
@@ -1,36 +1,62 @@
 . "$KW_LIB_DIR/kw_config_loader.sh" --source-only
+. "$KW_LIB_DIR/kwlib.sh" --source-only
 
 function vm_mount
 {
-  local ret=0
-  mkdir -p ${configurations[mount_point]}
+  local flag="$1"
+  local qemu_img_path="$2"
+  local mount_point_path="$3"
+  local guestmount_cmd
+  local ret
 
-  say "Mount ${configurations[qemu_path_image]}" \
-      "in ${configurations[mount_point]}"
+  flag=${flag:-"SILENT"}
+  qemu_img_path="${qemu_img_path:-${configurations[qemu_path_image]}}"
+  mount_point_path="${mount_point_path:-${configurations[mount_point]}}"
 
-  guestmount -a ${configurations[qemu_path_image]} \
-             -i ${configurations[mount_point]}
+  [[ $(findmnt "$mount_point_path") ]] && return 125
 
-  if [ "$?" != 0 ] ; then
-    complain "Something went wrong when tried to mount" \
-        "${configurations[qemu_path_image]} in ${configurations[mount_point]}"
+  mkdir -p $mount_point_path
+
+  say "Mount $qemu_img_path in $mount_point_path"
+
+  guestmount_cmd="guestmount -a $qemu_img_path -i $mount_point_path 2>&1"
+  cmd_manager "$flag" "$guestmount_cmd"
+  if [[ "$ret" ]] ; then
+    complain "Something went wrong when tried to mount $qemu_img_path" \
+	     "in $mount_point_path"
+    return "$ret"
   fi
 
-  return "$ret"
+  return 0
 }
 
 function vm_umount
 {
-  local ret=0
-  say "Unmount ${configurations[mount_point]}"
-  guestunmount ${configurations[mount_point]}
-  ret="$?"
-  if [ "$ret" != 0 ] ; then
-    complain "Something went wrong when tried to unmount" \
-        "${configurations[qemu_path_image]} in ${configurations[mount_point]}"
+  local flag="$1"
+  local qemu_img_path="$2"
+  local mount_point_path="$3"
+  local guestumount_cmd
+  local ret
+
+  flag=${flag:-"SILENT"}
+  qemu_img_path="${qemu_img_path:-${configurations[qemu_path_image]}}"
+  mount_point_path="${mount_point_path:-${configurations[mount_point]}}"
+
+  if [[ $(findmnt "$mount_point_path") ]]; then
+    say "Unmount $mount_point_path"
+
+    guestumount_cmd="guestunmount $mount_point_path"
+    cmd_manager "$flag" "$guestumount_cmd"
+    ret="$?"
+    if [[ "$ret" != 0 ]] ; then
+      complain "Something went wrong when tried to unmount $qemu_img_path" \
+         "in $mount_point_path"
+      return "$ret"
+    fi
+    return 0
   fi
 
-  return "$ret"
+  return 125 #ECANCELED
 }
 
 function vm_boot

tests/mk_test.sh
@@ -54,6 +54,7 @@ function setUp
   export KW_CACHE_DIR="$test_path"
   export KW_ETC_DIR="$PWD/$SAMPLES_DIR/etc"
   export DEPLOY_SCRIPT="$test_path/$kernel_install_path/deploy.sh"
+  export KW_PLUGINS_DIR="$PWD/src/plugins/"
   export modules_path="$test_path/$kernel_install_path/lib/modules"
   if [ -x "$(command -v nproc)" ] ; then
     PARALLEL_CORES=$(nproc --all)
@@ -332,7 +333,6 @@ function kernel_install_Test
 
   ID=3
   # We want to test an corner case described by the absence of mkinitcpio
-
   cd "$original"
   tearDown
   setUp "no_mkinitcpio"
@@ -467,6 +467,17 @@ function kernel_modules_Test
     fi
     ((count++))
   done <<< "$output"
+
+  ID=2
+  output=$(modules_install "TEST_MODE" "1" "")
+  expected_output="make INSTALL_MOD_PATH=/home/lala modules_install"
+  assertEquals "$ID: " "$output" "$expected_output"
+
+  ID=3
+  output=$(modules_install "TEST_MODE" "2" "")
+  expected_output="sudo -E make modules_install"
+  assertEquals "$ID: " "$output" "$expected_output"
+
 
   cd "$original"
 }

tests/vm_test.sh
@@ -1,0 +1,128 @@
+#!/bin/bash
+
+. ./tests/utils --source-only
+. ./src/vm.sh --source-only
+
+function suite
+{
+  suite_addTest "vm_mount_Test"
+  suite_addTest "vm_umount_Test"
+}
+
+declare -r test_path="tests/.tmp"
+
+function setUp
+{
+  local -r current_path="$PWD"
+
+  rm -rf "$test_path"
+
+  mkdir -p $test_path
+
+  cp -f tests/samples/kworkflow.config $test_path
+}
+
+function tearDown
+{
+  rm -rf "$test_path"
+}
+
+function vm_mount_Test
+{
+  local ID
+  local mount_point="$test_path/lala"
+  local qemu_path="/any/path"
+  local -r current_path="$PWD"
+  local ret
+  local expected_ret
+
+  # Message to user
+  local say_msg="Mount $qemu_path in $mount_point"
+
+  # Guestmount cmd
+  local guestmount_cmd="guestmount -a $qemu_path -i $mount_point 2>&1"
+
+  declare -a expected_cmd=(
+    "$say_msg"
+    "$guestmount_cmd"
+  )
+
+  setUp
+
+  cd "$test_path"
+
+  ID=1
+  output=$(vm_mount "TEST_MODE")
+  ret="$?"
+  expected_ret="125"
+  assertEquals "($ID) - Expected 125" "$expected_ret" "$ret"
+
+  ID=2
+  output=$(vm_mount "TEST_MODE" "$qemu_path" "$mount_point")
+  ret="$?"
+  assertTrue "($ID)" "$ret"
+
+  ID=3
+  output=$(vm_mount "TEST_MODE" "$qemu_path" "$mount_point")
+  compare_command_sequence expected_cmd[@] "$output" "$ID"
+
+  load_configuration "$KW_CONFIG_SAMPLE"
+
+  ID=4
+  say_msg="Mount ${configurations[qemu_path_image]} in $mount_point"
+  guestmount_cmd="guestmount -a ${configurations[qemu_path_image]} -i $mount_point 2>&1"
+  expected_cmd[0]="$say_msg"
+  expected_cmd[1]="$guestmount_cmd"
+
+  output=$(vm_mount "TEST_MODE" "" "$mount_point")
+  compare_command_sequence expected_cmd[@] "$output" "$ID"
+
+  cd "$current_path"
+
+  tearDown
+}
+
+function vm_umount_Test
+{
+  local ID
+  local mount_point="/"
+  local -r current_path="$PWD"
+  local ret
+  local expected_ret
+
+  # Message to user
+  local say_msg="Unmount $mount_point"
+
+  # Guestunmount cmd
+  local guestmount_cmd="guestunmount $mount_point"
+
+  declare -a expected_cmd=(
+    "$say_msg"
+    "$guestmount_cmd"
+  )
+
+  setUp
+
+  cd "$test_path"
+
+  ID=1
+  output=$(vm_umount "TEST_MODE")
+  ret="$?"
+  expected_ret="125"
+  assertEquals "($ID) - Expected 125" "$expected_ret" "$ret"
+
+  ID=2
+  output=$(vm_umount "TEST_MODE" "" "$mount_point")
+  ret="$?"
+  assertTrue "($ID): We got: $ret" "$ret"
+
+  ID=3
+  output=$(vm_umount "TEST_MODE" "" "$mount_point")
+  compare_command_sequence expected_cmd[@] "$output" "$ID"
+
+  cd "$current_path"
+
+  tearDown
+}
+
+invoke_shunit

commit 656c4cc4e048593d47ce501cb6156093a4a9392f

plugins: arch: fix the misuse of mkinitcpio on vm deployment
The mkinitcpio is only used for remote deployment and vm does not
require this operation.

Fixes: 5d75cfa3 ("Add support for VM deploy")
Signed-off-by: Melissa Wen <melissa.srw@gmail.com>

src/plugins/kernel_install/arch.sh
@@ -150,7 +150,7 @@ function install_kernel()
     cp -v "$name.preset" $path_prefix/etc/mkinitcpio.d/
   fi
 
-  if [[ "$target" != 'local' ]]; then
+  if [[ "$target" == 'remote' ]]; then
     # Update mkinitcpio
     cmd="$sudo_cmd mkinitcpio -p $name"
     cmd_manager "$flag" "$cmd"