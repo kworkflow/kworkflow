d5116508ecb5c1e8b52463c018c31c444d9d9ed5
Magali Lemes <magalilemes00@gmail.com>
2021-04-09 22:19:54 -0300

    src: mk: Remove unnecessary sound notification
    Whenever a user runs 'kw deploy' with an invalid option, kw still
    alerts the user with either a sound or a visual notification. For
    instance, if you invoke 'kw deploy --something', you could hear the
    sound alert. This commit intends to fix this behavior.

--- src/mk.sh
+++ src/mk.sh
@@ -404,7 +404,7 @@ function kernel_deploy
   deploy_parser_options "$@"
   if [[ "$?" -gt 0 ]]; then
     complain "Invalid option: ${options_values['ERROR']}"
-    return 22
+    exit 22
   fi
 
   target="${options_values['TARGET']}"
