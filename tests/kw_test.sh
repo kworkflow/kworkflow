cb14c317ccad3341485716b94fcea132ce7dfe39
Antonio Terceiro <antonio.terceiro@linaro.org>
2021-05-17 21:14:55 -0300

    kw: make it possible to run without installing
    This allows running kw directly from the source directory.  For those
    working on kw itself, this is a lot more practical than a
    hack/install/run cycle. Just symlink the `kw` in your $PATH, and it
    will work.
    
    bash completion can also be used from the source directory. I have the
    following bits in ~/.bashrc, which allow me to just symlink
    ${KW_SOURCE}/src/bash_autocomplete.sh into ~/.bash_completion.d/ and
    have it working.
    
        if [ -d ~/.bash_completion.d ]; then
            for file in ~/.bash_completion.d/*; do
                . $file
            done
        fi
    
    The code supporting running uninstalled gets removed by setup.sh on
    installation.
    
    Signed-off-by: Antonio Terceiro <antonio.terceiro@linaro.org>
    Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- tests/kw_test.sh
+++ tests/kw_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 . ./tests/utils --source-only
+unset -v KW_LIB_DIR  # to be able to test the developer mode
 . ./kw --source-only > /dev/null
 
 function suite
 {
   suite_addTest "validate_global_variables_Test"
   suite_addTest "check_kworkflow_global_variable_Test"
+  suite_addTest 'set_KW_LIB_DIR_in_dev_mode_Test'
 }
 
 function validate_global_variables_Test
@@ -24,6 +26,12 @@ function check_kworkflow_global_variable_Test
     [[ $(declare -p $v)  =~ ^declare\ -[aAilrtu]*x[aAilrtu]*\  ]] ||
       fail "Variable $v should have been exported"
   done
+}
+
+function set_KW_LIB_DIR_in_dev_mode_Test
+{
+  lib="${KW_LIB_DIR}/kwlib.sh"
+  test -f "${lib}" || fail "kwlib.sh not found (${lib} not found)!"
 }
 
 invoke_shunit
