2f9557240c15475aabff2f87e21d6b8d3e9e2c49
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2021-05-19 21:09:59 -0400

    src: kw_string: Add a file dedicated to handling string
    Unfortunately, some of the shell parameter expansion for handling
    strings are not obvious at first glance; in particular, those not used
    to how bash works might not understand the syntax. For trying to make kw
    code more readable, this commit starts a tiny library that comprises
    some of the basic string manipulation currently used in the source.
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

---
+++ tests/kw_string_test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+. ./src/kw_string.sh --source-only
+. ./tests/utils --source-only
+
+function suite
+{
+  suite_addTest 'chop_Test'
+  suite_addTest 'last_char_Test'
+  suite_addTest 'str_is_a_number_Test'
+}
+
+function chop_Test
+{
+  local output
+  local str_test='1234567'
+
+  output=$(chop "$str_test")
+  assert_equals_helper 'Chop did not work as expected' "$LINENO" '123456' "$output"
+
+  output=$(chop "$output")
+  assert_equals_helper 'Chop did not work as expected' "$LINENO" '12345' "$output"
+
+  output=$(chop '')
+  assert_equals_helper 'Expected an empty string' "$LINENO" "" "$output"
+}
+
+function last_char_Test
+{
+  local output
+  local str_test='kworkflow'
+
+  output=$(last_char "$str_test")
+  assert_equals_helper 'We did not get the last char' "$LINENO" 'w' "$output"
+
+  str_test='something$'
+  output=$(last_char "$str_test")
+  assert_equals_helper 'We did not get the last char' "$LINENO" '$' "$output"
+
+  str_test=''
+  output=$(last_char "$str_test")
+  assert_equals_helper 'We did not get the last char' "$LINENO" '' "$output"
+}
+
+function str_is_a_number_Test
+{
+  local output
+  local str_test=333
+
+  str_is_a_number "$str_test"
+  output="$?"
+  assert_equals_helper 'We did not get the last char' "$LINENO" '0' "$output"
+
+  str_is_a_number 234232
+  output="$?"
+  assert_equals_helper 'We did not get the last char' "$LINENO" '0' "$output"
+
+  str_is_a_number 'kworkflow'
+  output="$?"
+  assert_equals_helper 'We did not get the last char' "$LINENO" '1' "$output"
+}
+
+invoke_shunit
