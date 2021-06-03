bd6e39d930ca0b1719ce1327dac7b32ba44c9425
Rubens Gomes Neto <rubens.gomes.neto@usp.br>
2021-06-03 18:09:53 -0300

    src: remote: refactor ssh functionality
    Some of the code dealing with the ssh functionality didn't make sense in
    it's new context as a part of `remote.sh`.
    Also some improvements were made with the handling of variables.
    
    Closes #150
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@usp.br>
    Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- tests/remote_test.sh
+++ tests/remote_test.sh
@@ -14,10 +14,10 @@ function suite
   suite_addTest "preapre_host_deploy_dir_Test"
   suite_addTest "prepare_remote_dir_Test"
   suite_addTest "generate_tarball_Test"
-  suite_addTest "vm_ssh_check_fail_cases_Test"
-  suite_addTest "vm_ssh_basic_Test"
-  suite_addTest "vm_ssh_command_Test"
-  suite_addTest "vm_ssh_script_Test"
+  suite_addTest "kw_ssh_check_fail_cases_Test"
+  suite_addTest "kw_ssh_basic_Test"
+  suite_addTest "kw_ssh_command_Test"
+  suite_addTest "kw_ssh_script_Test"
 }
 
 function which_distro_mock()
@@ -327,77 +327,77 @@ function tearDownSsh
   rm -rf $TEST_PATH
 }
 
-function vm_ssh_check_fail_cases_Test
+function kw_ssh_check_fail_cases_Test
 {
   setupSsh
 
   local args="--lala"
-  local ret=$(vm_ssh $args)
+  local ret=$(kw_ssh $args)
 
   assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
   args="-m"
-  ret=$(vm_ssh $args)
+  ret=$(kw_ssh $args)
   assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
   args="-d="
-  ret=$(vm_ssh $args)
+  ret=$(kw_ssh $args)
   assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
   args="-c"
-  ret=$(vm_ssh $args)
+  ret=$(kw_ssh $args)
   assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
   args="-s"
-  ret=$(vm_ssh $args)
+  ret=$(kw_ssh $args)
   assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
   args="-s="
-  ret=$(vm_ssh $args)
+  ret=$(kw_ssh $args)
   assertTrue "We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
 
   args="--script="
-  ret=$(vm_ssh $args)
+  ret=$(kw_ssh $args)
   assertTrue "We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
 
   tearDownSsh
 }
 
-function vm_ssh_basic_Test
+function kw_ssh_basic_Test
 {
   setupSsh
 
-  ret=$(vm_ssh 2>&1)
+  ret=$(kw_ssh 2>&1)
 
   assertTrue "We expected a substring \"$SSH_OK\", but we got \"$ret\"" '[[ $ret =~ "$SSH_OK" ]]'
 
   tearDownSsh
 }
 
-function vm_ssh_command_Test
+function kw_ssh_command_Test
 {
   setupSsh
 
-  ret=$(vm_ssh -c="pwd" 2>&1)
+  ret=$(kw_ssh -c="pwd" 2>&1)
   msg="$SSH_OK pwd"
   assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
 
-  ret=$(vm_ssh --command="ls /etc/" 2>&1)
+  ret=$(kw_ssh --command="ls /etc/" 2>&1)
   msg="$SSH_OK ls /etc/"
   assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
 
   tearDownSsh
 }
 
-function vm_ssh_script_Test
+function kw_ssh_script_Test
 {
   setupSsh
 
-  ret=$(vm_ssh -s="/not/a/valid/path/xpto" 2>&1)
+  ret=$(kw_ssh -s="/not/a/valid/path/xpto" 2>&1)
   msg="$NO_SUCH_FILE: \"/not/a/valid/path/xpto\""
   assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
 
-  ret=$(vm_ssh -s="$TEST_PATH/dmesg" 2>&1)
+  ret=$(kw_ssh -s="$TEST_PATH/dmesg" 2>&1)
   msg="$SSH_OK \"bash -s\" -- < $TEST_PATH/dmesg"
   assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
 
