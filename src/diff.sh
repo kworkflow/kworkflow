kworkflow/kworkflow

commit 5244fc03f972397a0b6995badad7c3acb984181f

code: separate declarations and assignements
When a declaration and an assignement happen at the same time, the
return code may be masked, so in this commit we separate them to avoid
this.

This commit fixes all occurrences of the SC2155 shellcheck error
Closes #322
Signed-off-by: Alan Barzilay <alan.barzilay@gmail.com>
Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

src/diff.sh
@@ -103,9 +103,10 @@ function diff_side_by_side()
   local file_2="$2"
   local interactive="$3"
   local flag="$4"
-  local columns=$(tput cols)
   local diff_cmd="diff -y --color=always --width=$columns"
+  local columns
 
+  columns=$(tput cols)
   flag=${flag:-""}
 
   if [[ ! -f "$file_1" || ! -f "$file_2" ]]; then

run_tests.sh
@@ -162,9 +162,10 @@ function run_tests()
 declare -a TESTS
 function strip_path()
 {
+  local base
   TESTS=()
   for file in "$@"; do
-    local base=$(basename ${file})
+    base=$(basename ${file})
     TESTS+=("${base%.*}")
   done
 }

setup.sh
@@ -47,9 +47,11 @@ declare -r CONFIGS_PATH="configs"
 
 function check_dependencies()
 {
-  local distro=$(detect_distro "/")
   local package_list=""
   local cmd=""
+  local distro
+
+  distro=$(detect_distro "/")
 
   if [[ "$distro" =~ "arch" ]]; then
     for package in "${arch_packages[@]}"; do
@@ -162,10 +164,12 @@ function confirm_complete_removal()
 
 function clean_legacy()
 {
-  local trash=$(mktemp -d)
   local completely_remove="$1"
-
   local toDelete="$app_name"
+  local trash
+
+  trash=$(mktemp -d)
+
   eval "sed -i '/\<$toDelete\>/d' $HOME/.bashrc"
 
   # Remove kw binary
@@ -321,9 +325,13 @@ function append_bashcompletion()
 
 function update_version()
 {
-  local head_hash=$(git rev-parse --short HEAD)
-  local branch_name=$(git rev-parse --short --abbrev-ref HEAD)
-  local base_version=$(cat "$libdir/VERSION" | head -n 1)
+  local head_hash
+  local branch_name
+  local base_version
+
+  head_hash=$(git rev-parse --short HEAD)
+  branch_name=$(git rev-parse --short --abbrev-ref HEAD)
+  base_version=$(cat "$libdir/VERSION" | head -n 1)
 
   cat > "$libdir/VERSION" << EOF
 $base_version

src/config_manager.sh
@@ -75,6 +75,8 @@ function save_config_file()
 function list_configs()
 {
   local -r dot_configs_dir="$KW_DATA_DIR/configs"
+  local name
+  local content
 
   if [[ ! -d "$dot_configs_dir" || ! -d "$dot_configs_dir/$metadata_dir" ]]; then
     say "There's no tracked .config file"
@@ -85,8 +87,8 @@ function list_configs()
   echo
   for filename in $dot_configs_dir/$metadata_dir/*; do
     [[ ! -f "$filename" ]] && continue
-    local name=$(basename "$filename")
-    local content=$(cat "$filename")
+    name=$(basename "$filename")
+    content=$(cat "$filename")
     printf "%-30s | %-30s\n" "$name" "$content"
   done
 }

src/kw_config_loader.sh
@@ -44,7 +44,9 @@ function show_variables()
 function parse_configuration()
 {
   local config_path="$1"
-  local filename="$(basename "$config_path")"
+  local filename
+
+  filename="$(basename "$config_path")"
 
   if [ ! -f "$config_path" ] || [ "$filename" != kworkflow.config ]; then
     return 22 # 22 means Invalid argument - EINVAL

src/kw_include.sh
@@ -8,7 +8,9 @@
 function include()
 {
   local filepath="$1"
-  local varname=$(basename "$filepath" .sh)
+  local varname
+
+  varname=$(basename "$filepath" .sh)
 
   if [[ ! -e "$filepath" ]]; then
     echo "File $filepath could not be found, check your file path."

src/kwlib.sh
@@ -156,12 +156,13 @@ function find_kernel_root()
 function is_a_patch()
 {
   local -r FILE_PATH="$@"
+  local file_content
 
   if [[ ! -f "$FILE_PATH" ]]; then
     return 1
   fi
 
-  local file_content=$(cat "$FILE_PATH")
+  file_content=$(cat "$FILE_PATH")
 
   # The following array stores strings that are expected to be present
   # in a patch file. The absence of any of these strings makes the
@@ -224,8 +225,10 @@ function detect_distro()
 {
   local root_path="$1"
   local str_check="$2"
-  local etc_path=$(join_path $root_path /etc)
   local distro="none"
+  local etc_path
+
+  etc_path=$(join_path $root_path /etc)
 
   if [[ ! -z "$str_check" ]]; then
     distro="$str_check"
@@ -257,9 +260,13 @@ function statistics_manager()
 {
   local label="$1"
   local value="$2"
-  local day=$(date +%d)
-  local year_month_dir=$(date +%Y/%m)
-  local day_path="$KW_DATA_DIR/statistics/$year_month_dir/$day"
+  local day
+  local year_month_dir
+  local day_path
+
+  day=$(date +%d)
+  year_month_dir=$(date +%Y/%m)
+  day_path="$KW_DATA_DIR/statistics/$year_month_dir/$day"
 
   elapsed_time=$(date -d@$value -u +%H:%M:%S)
   say "-> Execution time: $elapsed_time"

src/mk.sh
@@ -119,6 +119,9 @@ function modules_install()
   local flag="$1"
   local target="$2"
   local formatted_remote="$3"
+  local remote
+  local port
+  local distro
 
   if ! is_kernel_root "$PWD"; then
     complain "Execute this command in a kernel tree."
@@ -129,7 +132,7 @@ function modules_install()
 
   case "$target" in
     1) # VM_TARGET
-      local distro=$(detect_distro "${configurations[mount_point]}/")
+      distro=$(detect_distro "${configurations[mount_point]}/")
 
       if [[ "$distro" =~ "none" ]]; then
         complain "Unfortunately, there's no support for the target distro"
@@ -147,8 +150,8 @@ function modules_install()
       # 1. Preparation steps
       prepare_host_deploy_dir
 
-      local remote=$(get_based_on_delimiter "$formatted_remote" ":" 1)
-      local port=$(get_based_on_delimiter "$formatted_remote" ":" 2)
+      remote=$(get_based_on_delimiter "$formatted_remote" ":" 1)
+      port=$(get_based_on_delimiter "$formatted_remote" ":" 2)
       # User may specify a hostname instead of bare IP
       remote=$(get_based_on_delimiter "$remote" "@" 2)
 
@@ -190,6 +193,8 @@ function mk_list_installed_kernels()
   local single_line="$2"
   local target="$3"
   local unformatted_remote="$4"
+  local remote
+  local port
 
   flag=${flag:-"SILENT"}
 
@@ -213,8 +218,8 @@ function mk_list_installed_kernels()
       ;;
     3) # REMOTE_TARGET
       local cmd="bash $REMOTE_KW_DEPLOY/deploy.sh --list_kernels $single_line"
-      local remote=$(get_based_on_delimiter "$unformatted_remote" ":" 1)
-      local port=$(get_based_on_delimiter "$unformatted_remote" ":" 2)
+      remote=$(get_based_on_delimiter "$unformatted_remote" ":" 1)
+      port=$(get_based_on_delimiter "$unformatted_remote" ":" 2)
       remote=$(get_based_on_delimiter "$remote" "@" 2)
 
       prepare_remote_dir "$remote" "$port" "" "$flag"
@@ -249,6 +254,9 @@ function kernel_install()
   local mkinitcpio_name="${configurations[mkinitcpio_name]}"
   local arch_target="${configurations[arch]}"
   local kernel_img_name="${configurations[kernel_img_name]}"
+  local remote
+  local port
+  local distro
 
   if ! is_kernel_root "$PWD"; then
     complain "Execute this command in a kernel tree."
@@ -281,7 +289,7 @@ function kernel_install()
 
   case "$target" in
     1) # VM_TARGET
-      local distro=$(detect_distro "${configurations[mount_point]}/")
+      distro=$(detect_distro "${configurations[mount_point]}/")
 
       if [[ "$distro" =~ "none" ]]; then
         complain "Unfortunately, there's no support for the target distro"
@@ -295,7 +303,7 @@ function kernel_install()
       return "$?"
       ;;
     2) # LOCAL_TARGET
-      local distro=$(detect_distro "/")
+      distro=$(detect_distro "/")
 
       if [[ "$distro" =~ "none" ]]; then
         complain "Unfortunately, there's no support for the target distro"
@@ -321,8 +329,8 @@ function kernel_install()
         sed -i "s/NAME/$name/g" "$preset_file"
       fi
 
-      local remote=$(get_based_on_delimiter "$formatted_remote" ":" 1)
-      local port=$(get_based_on_delimiter "$formatted_remote" ":" 2)
+      remote=$(get_based_on_delimiter "$formatted_remote" ":" 1)
+      port=$(get_based_on_delimiter "$formatted_remote" ":" 2)
       remote=$(get_based_on_delimiter "$remote" "@" 2)
 
       distro_info=$(which_distro "$remote" "$port" "$user")
@@ -362,6 +370,8 @@ function mk_kernel_uninstall()
   local kernels_target="$4"
   local flag="$5"
   local distro
+  local remote
+  local port
 
   flag=${flag:-""}
 
@@ -384,8 +394,8 @@ function mk_kernel_uninstall()
       kernel_uninstall "$reboot" 'local' "$kernels_target" "$flag"
       ;;
     3) # REMOTE_TARGET
-      local remote=$(get_based_on_delimiter "$formatted_remote" ":" 1)
-      local port=$(get_based_on_delimiter "$formatted_remote" ":" 2)
+      remote=$(get_based_on_delimiter "$formatted_remote" ":" 1)
+      port=$(get_based_on_delimiter "$formatted_remote" ":" 2)
       remote=$(get_based_on_delimiter "$remote" "@" 2)
 
       prepare_remote_dir "$remote" "$port" "" "$flag"
@@ -532,16 +542,21 @@ function deploy_help()
 # will be compiled.
 function build_info()
 {
-  local flag="$1"
-  local kernel_name=$(get_kernel_release "$flag")
-  local kernel_version=$(get_kernel_version "$flag")
+  local flag
+  local kernel_name
+  local kernel_version
+  local compiled_modules
+
+  flag="$1"
+  kernel_name=$(get_kernel_release "$flag")
+  kernel_version=$(get_kernel_version "$flag")
 
   say "Kernel source information"
   echo -e "\tName: $kernel_name"
   echo -e "\tVersion: $kernel_version"
 
   if [[ -f '.config' ]]; then
-    local compiled_modules=$(grep -c '=m' .config)
+    compiled_modules=$(grep -c '=m' .config)
     echo -e "\tTotal modules to be compiled: $compiled_modules"
   fi
 }

commit 45779a3185bdfc3ad6d27c6b57cf0affec86cf91

src: diff: fix diff functionality
Executing kw diff <file1> <file2> doesn't work since 5244fc0 which
inverted the order of a declaration and assignment, which resulted in
the diff command receiving option --width= which is invalid.

How to reproduce
Execute kw diff <file1> <file2> with any valid files.
The interactive less displayed is empty and upon return the following
message can be seen:

diff -y --color=always --width= <file1> <file2> | less -R
diff: invalid width ''
diff: Try 'diff --help' for more information.
Using the --no-interactive option has no effect.

This commit fix this issue by moving diff_cmd after columns.

Fixes #386

Fixes: 5244fc03f972 (code: separate declarations and assignements)
Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@usp.br>
Reviewed-by: Alan Barzilay <alan.barzilay@gmail.com>
Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

src/diff.sh
@@ -103,10 +103,11 @@ function diff_side_by_side()
   local file_2="$2"
   local interactive="$3"
   local flag="$4"
-  local diff_cmd="diff -y --color=always --width=$columns"
+  local diff_cmd
   local columns
 
   columns=$(tput cols)
+  diff_cmd="diff -y --color=always --width=$columns"
   flag=${flag:-""}
 
   if [[ ! -f "$file_1" || ! -f "$file_2" ]]; then
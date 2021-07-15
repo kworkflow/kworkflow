45779a3185bdfc3ad6d27c6b57cf0affec86cf91
Rubens Gomes Neto <rubens.gomes.neto@usp.br>
2021-07-14 23:26:53 -0300

    src: diff: fix diff functionality
    Executing kw diff <file1> <file2> doesn't work since 5244fc0 which
    inverted the order of a declaration and assignment, which resulted in
    the diff command receiving option --width= which is invalid.
    
    How to reproduce
    Execute kw diff <file1> <file2> with any valid files.
    The interactive less displayed is empty and upon return the following
    message can be seen:
    
    diff -y --color=always --width= <file1> <file2> | less -R
    diff: invalid width ''
    diff: Try 'diff --help' for more information.
    Using the --no-interactive option has no effect.
    
    This commit fix this issue by moving diff_cmd after columns.
    
    Fixes #386
    
    Fixes: 5244fc03f972 (code: separate declarations and assignements)
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@usp.br>
    Reviewed-by: Alan Barzilay <alan.barzilay@gmail.com>
    Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- src/diff.sh
+++ src/diff.sh
@@ -103,10 +103,11 @@ function diff_side_by_side()
   local file_2="$2"
   local interactive="$3"
   local flag="$4"
-  local diff_cmd="diff -y --color=always --width=$columns"
+  local diff_cmd
   local columns
 
   columns=$(tput cols)
+  diff_cmd="diff -y --color=always --width=$columns"
   flag=${flag:-""}
 
   if [[ ! -f "$file_1" || ! -f "$file_2" ]]; then
