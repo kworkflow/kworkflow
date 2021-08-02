ef1d69fe6bf6c10077c93602ab4da363074b947b
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2021-08-02 09:44:46 -0400

    src: kw_ssh: Create a file dedicated to handling ssh connections
    The code related to ssh was moved to the remote file; however, this is
    incorrect because 'remote.sh' should work as a library. This commit
    creates a new file named 'kw_ssh.sh' and 'test/kw_ssh_test.sh' and moves
    all the ssh functions from 'remote.sh' to these files. This commit did
    not change the logic; it just moved the code and made the necessary
    adaptations.
    
    Closes #395
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- tests/remote_test.sh
+++ tests/remote_test.sh
@@ -29,9 +29,6 @@ function oneTimeSetUp()
   mkdir -p "$FAKE_KW"
   mkdir -p "$TEST_PATH"
 
-  cp -f 'tests/samples/kworkflow.config' "$TEST_PATH"
-  cp -f 'tests/samples/dmesg' "$TEST_PATH"
-
   local -r current_path="$PWD"
 
   cp -f tests/samples/kworkflow.config "$TEST_PATH"
@@ -42,7 +39,6 @@ function oneTimeSetUp()
     return
   }
   load_configuration
-  populate_remote_info ''
   cd "$current_path" || {
     fail "($LINENO) It was not possible return to original directory"
     return
@@ -59,7 +55,6 @@ function oneTimeSetUp()
   export INVALID_ARG='Invalid arguments'
   export NO_SUCH_FILE='No such file'
   export SSH_OK='ssh -p 3333 127.0.0.1'
-
   rm -rf "$FAKE_KW"
 
   mk_fake_remote "$FAKE_KW" "$modules_path"
@@ -314,73 +309,6 @@ function test_prepare_remote_dir()
   compare_command_sequence 'expected_cmd_sequence' "$output" "$LINENO"
 
   tearDownMockFunctions
-}
-
-#### SSH tests ###
-##
-## NOTE: We're not testing the ssh command here, just the kw ssh operation
-##
-
-function test_kw_ssh_check_fail_cases()
-{
-  local args='--lala'
-  local ret
-
-  ret=$(kw_ssh "$args")
-  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
-
-  args='-m'
-  ret=$(kw_ssh "$args")
-  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
-
-  args='-d='
-  ret=$(kw_ssh "$args")
-  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
-
-  args='-c'
-  ret=$(kw_ssh "$args")
-  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
-
-  args='-s'
-  ret=$(kw_ssh "$args")
-  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
-
-  args='-s='
-  ret=$(kw_ssh $args)
-  assertTrue "($LINENO): We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
-
-  args='--script='
-  ret=$(kw_ssh "$args")
-  assertTrue "($LINENO): We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
-}
-
-function test_kw_ssh_basic()
-{
-  ret=$(kw_ssh 2>&1)
-
-  assertTrue "We expected a substring \"$SSH_OK\", but we got \"$ret\"" '[[ $ret =~ "$SSH_OK" ]]'
-}
-
-function test_kw_ssh_command()
-{
-  ret=$(kw_ssh -c="pwd" 2>&1)
-  msg="$SSH_OK pwd"
-  assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
-
-  ret=$(kw_ssh --command="ls /etc/" 2>&1)
-  msg="$SSH_OK ls /etc/"
-  assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
-}
-
-function test_kw_ssh_script()
-{
-  ret=$(kw_ssh -s="/not/a/valid/path/xpto" 2>&1)
-  msg="$NO_SUCH_FILE: \"/not/a/valid/path/xpto\""
-  assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
-
-  ret=$(kw_ssh -s="$TEST_PATH/dmesg" 2>&1)
-  msg="$SSH_OK \"bash -s\" -- < $TEST_PATH/dmesg"
-  assertTrue "We expected a substring \"$msg\", but we got \"$ret\"" '[[ $ret =~ "$msg" ]]'
 }
 
 invoke_shunit
