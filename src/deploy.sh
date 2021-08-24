kworkflow/kworkflow

commit 30b0459c51f2ad58e595350f8e9bf45e62a2299c

src: kwlib: move generate_tarball from src/remote.sh
generate_tarball() was previously in src/remote.sh. Now it's been
generalized and moved to src/kwlib.sh. Tests and functions which used
this function were changed accordingly.

Signed-off-by: Magali Lemes <magalilemes00@gmail.com>
Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

src/deploy.sh
@@ -429,7 +429,8 @@ function modules_install()
 
       release=$(get_kernel_release "$flag")
       success "Kernel: $release"
-      generate_tarball "$release" '' "$flag"
+      generate_tarball "$KW_CACHE_DIR/$LOCAL_REMOTE_DIR/lib/modules/$release" \
+        "$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$release.tar" '' "$flag"
 
       local tarball_for_deploy_path="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$release.tar"
       cp_host2remote "$tarball_for_deploy_path" \

src/kwlib.sh
@@ -1,5 +1,9 @@
 # NOTE: src/kw_config_loader.sh must be included before this file
 
+# Array with compression programs accepted by tar
+declare -ga compression_programs=('gzip' 'bzip2' 'lzip' 'lzma' 'lzop' 'zstd' 'xz'
+  'auto-compress')
+
 # A common task used inside kw is a string separation based on a delimiter, for
 # this reason, this function tries to handle this scenario by getting a
 # delimiter character followed by the position that the users want to retrieve.
@@ -417,4 +421,48 @@ function kw_parse_get_errors()
       --longoptions "$long_options" \
       -- "$@" > /dev/null
   } 2>&1
+}
+
+# This function compresses a given path to a .tar.gz file
+#
+# @path_to_compress The directory to compress
+# @file_path Where the compressed directory will be stored
+# @compression_type compression program used
+# @flag How to display (or not) the command used
+function generate_tarball()
+{
+  local path_to_compress="$1"
+  local file_path="$2"
+  local compression_type="$3"
+  local flag="$4"
+  local ret
+  local cmd
+
+  flag=${flag:-'SILENT'}
+
+  if [[ ! -d "$path_to_compress" ]]; then
+    complain "$path_to_compress" 'does not exist'
+    exit 22 #EINVAL
+  fi
+
+  if [[ -n "$compression_type" ]]; then
+    if [[ "${compression_programs[*]}" =~ $compression_type ]]; then
+      compression_type="--$compression_type"
+    else
+      complain 'Invalid compression type:' "$compression_type"
+      return 22 # EINVAL
+    fi
+  fi
+
+  compression_type=${compression_type:-'--auto-compress'}
+
+  cmd="tar -C $path_to_compress $compression_type -cf $file_path ."
+  cmd_manager "$flag" "$cmd"
+
+  ret="$?"
+
+  if [[ "$ret" != 0 ]]; then
+    complain 'Error archiving modules.'
+    exit "$ret"
+  fi
 }

tests/deploy_test.sh
@@ -312,7 +312,7 @@ function test_kernel_modules()
   local expected_output="Kernel: $version"
 
   # Compress modules for sending
-  local compress_cmd="tar -C $local_remote_path/lib/modules/ -cf $to_deploy_path/$version.tar $version"
+  local compress_cmd="tar -C $local_remote_path/lib/modules/$version --auto-compress -cf $to_deploy_path/$version.tar ."
 
   # Rsync modules
   local rsync_tarball="$rsync_cmd $to_deploy_path/$version.tar $remote_access:$remote_path --rsync-path='sudo rsync'"
@@ -343,6 +343,9 @@ function test_kernel_modules()
   }
 
   setupRemote
+
+  # Create folder so generate_tarball won't complain
+  mkdir -p "$local_remote_path/lib/modules/$version"
 
   ID=1
   output=$(modules_install "TEST_MODE" 3)

tests/kwlib_test.sh
@@ -419,4 +419,38 @@ function test_kw_parse_get_errors()
   compare_command_sequence 'expected_output' "$out" "$LINENO"
 }
 
+function test_generate_tarball()
+{
+  local path_to_compress="$SHUNIT_TMPDIR/files"
+  local file_path="$SHUNIT_TMPDIR/compressed.tar.gz"
+  local output
+
+  mkdir -p "$SHUNIT_TMPDIR/files"
+  touch "$SHUNIT_TMPDIR/files/file1"
+  touch "$SHUNIT_TMPDIR/files/file2"
+
+  declare -a expected_files=(
+    './'
+    './file1'
+    './file2'
+  )
+
+  output=$(generate_tarball "$path_to_compress" "$file_path" 'gzip' 'SUCCESS')
+  assertEquals "($LINENO)" "tar -C $path_to_compress --gzip -cf $file_path ." "$output"
+
+  assertTrue 'Compressed file was not created' "[[ -f $SHUNIT_TMPDIR/compressed.tar.gz ]]"
+
+  output=$(tar -taf "$file_path" | sort -d)
+  compare_command_sequence expected_files "$output" "$LINENO"
+
+  output=$(generate_tarball "$SHUNIT_TMPDIR/vacation/photos" "$file_path" 'gzip' 'SUCCESS')
+  assertEquals "($LINENO)" "$SHUNIT_TMPDIR/vacation/photos does not exist" "$output"
+
+  output=$(generate_tarball "$path_to_compress" "$file_path" 'zipper')
+  assertEquals "($LINENO)" 'Invalid compression type: zipper' "$output"
+
+  output=$(generate_tarball "$path_to_compress" "$SHUNIT_TMPDIR/file/file" 2> /dev/null)
+  assertEquals "($LINENO)" 'Error archiving modules.' "$output"
+}
+
 invoke_shunit

setup.sh
@@ -4,8 +4,8 @@
 . src/kwlib.sh --source-only
 
 # List of dependences per distro
-arch_packages=(qemu bash git tar python-docutils pulseaudio libpulse dunst python-sphinx imagemagick graphviz python-virtualenv texlive-bin librsvg)
-debian_packages=(qemu git tar python3-docutils pulseaudio-utils dunst sphinx-doc imagemagick graphviz dvipng python3-venv latexmk librsvg2-bin texlive-xetex python3-sphinx python3-dask-sphinx-theme)
+arch_packages=(qemu bash git tar python-docutils pulseaudio libpulse dunst python-sphinx imagemagick graphviz python-virtualenv texlive-bin librsvg bzip2 lzip lzop zstd xz)
+debian_packages=(qemu git tar python3-docutils pulseaudio-utils dunst sphinx-doc imagemagick graphviz dvipng python3-venv latexmk librsvg2-bin texlive-xetex python3-sphinx python3-dask-sphinx-theme bzip2 lzip xz-utils lzop zstd)
 
 SILENT=1
 VERBOSE=0

src/remote.sh
@@ -178,46 +178,6 @@ function prepare_remote_dir()
     "$port" "$user" "$flag"
 }
 
-# This function generates a tarball file to be sent to the target machine.
-# Notice that we rely on the directory "~/kw/remote".
-#
-# @kernel_release Kernel release name.
-# @files_path Point to the directory with the modules files to be deployed. By
-#   default we set "$kw_remote_dir/lib/modules" if the user does not indicate
-#   any path
-# @flag How to display a command, the default value is
-#   "HIGHLIGHT_CMD". For more options see `src/kwlib.sh` function `cmd_manager`
-function generate_tarball()
-{
-  local kernel_release=$1
-  local files_path=$2
-  local flag=$3
-  local ret
-  local tarball_name=""
-  local compress_cmd=""
-  local kw_remote_dir="$KW_CACHE_DIR/$LOCAL_REMOTE_DIR"
-
-  files_path=${files_path:-"$kw_remote_dir/lib/modules/"}
-  kernel_release=${kernel_release:-"no_release"}
-  flag=${flag:-""}
-  tarball_name="$kernel_release.tar"
-
-  # Anyone can read the documentation about tar, and understood what this
-  # command does. However, it does not hurt explain it a little bit here.
-  # -C: Go to $files_path directory
-  # -cf: Compress the directory named $kernel_release (inside $files_path) to
-  #      $kw_remote_dir/$tarball_name
-  compress_cmd="tar -C $files_path -cf $KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$tarball_name $kernel_release"
-
-  cmd_manager "$flag" "$compress_cmd"
-  ret=$?
-
-  if [[ "$ret" != 0 ]]; then
-    complain "Error archiving modules."
-    exit $ret
-  fi
-}
-
 # Populate remote info
 #
 # @parameters: Command line parameter to be parsed

tests/remote_test.sh
@@ -285,39 +285,6 @@ function test_prepare_remote_dir()
   tearDownMockFunctions
 }
 
-function test_generate_tarball()
-{
-  local kernel_release="test"
-  local tarball_name="$kernel_release.tar"
-  local count=0
-  local ID
-
-  declare -a expected_files=(
-    "test/"
-    "test/file1"
-    "test/file2"
-  )
-
-  expected_cmd="tar -C $FAKE_KW/kernel_install/lib/modules -cf $FAKE_KW/$LOCAL_TO_DEPLOY_DIR/$tarball_name $kernel_release"
-  ID=1
-  output=$(generate_tarball "$kernel_release" "$modules_path")
-
-  assertTrue "$ID - We expected a tarball" '[[ -f $KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$tarball_name ]]'
-
-  ID=2
-  output=$(tar -taf "$FAKE_KW/$LOCAL_TO_DEPLOY_DIR/$tarball_name" | sort -d)
-  while read -r f; do
-    if [[ ${expected_files[$count]} != "${f}" ]]; then
-      fail "$ID - Expected file \"${expected_files[$count]}\" to be \"${f}\""
-    fi
-    ((count++))
-  done <<< "$output"
-
-  ID=3
-  output=$(generate_tarball "$kernel_release" "$modules_path" "TEST_MODE")
-  assertEquals "Command did not match ($ID)" "$expected_cmd" "$output"
-}
-
 ### SSH tests ###
 #
 # NOTE: We're not testing the ssh command here, just the kw ssh operation

commit e2a2c2ee9e9cf35d594ab60ca26cad19f8e53100

src: kwlib: change behavior from generate_tarball
The way generate_tarball was implemented in 30b0459c51f2 led to the
deploy malfunctioning in remote machines. This commit fixes this by
adding a new parameter to the function.

Fixes: 30b0459c51f2 ("src: kwlib: move generate_tarball from src/remote.sh")
Signed-off-by: Magali Lemes <magalilemes00@gmail.com>

src/deploy.sh
@@ -429,8 +429,8 @@ function modules_install()
 
       release=$(get_kernel_release "$flag")
       success "Kernel: $release"
-      generate_tarball "$KW_CACHE_DIR/$LOCAL_REMOTE_DIR/lib/modules/$release" \
-        "$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$release.tar" '' "$flag"
+      generate_tarball "$KW_CACHE_DIR/$LOCAL_REMOTE_DIR/lib/modules/" \
+        "$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$release.tar" '' "$release" "$flag"
 
       local tarball_for_deploy_path="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/$release.tar"
       cp_host2remote "$tarball_for_deploy_path" \

src/kwlib.sh
@@ -425,23 +425,26 @@ function kw_parse_get_errors()
 
 # This function compresses a given path to a .tar.gz file
 #
-# @path_to_compress The directory to compress
+# @go_to_path_to_compress Directory to go into
 # @file_path Where the compressed directory will be stored
 # @compression_type compression program used
+# @dir_name The directory to be compressed, inside go_to_path_to_compress
 # @flag How to display (or not) the command used
 function generate_tarball()
 {
-  local path_to_compress="$1"
+  local go_to_path_to_compress="$1"
   local file_path="$2"
   local compression_type="$3"
-  local flag="$4"
+  local dir_name="$4"
+  local flag="$5"
   local ret
   local cmd
 
   flag=${flag:-'SILENT'}
+  dir_name=${dir_name:-'.'}
 
-  if [[ ! -d "$path_to_compress" ]]; then
-    complain "$path_to_compress" 'does not exist'
+  if [[ ! -d "$go_to_path_to_compress" ]]; then
+    complain "$go_to_path_to_compress" 'does not exist'
     exit 22 #EINVAL
   fi
 
@@ -456,7 +459,10 @@ function generate_tarball()
 
   compression_type=${compression_type:-'--auto-compress'}
 
-  cmd="tar -C $path_to_compress $compression_type -cf $file_path ."
+  # -C: Go to $go_to_path_to_compress directory
+  # -cf: Compress the directory named $dir_name (inside $go_to_path_to_compress) to
+  #      $file_path
+  cmd="tar -C $go_to_path_to_compress $compression_type -cf $file_path $dir_name"
   cmd_manager "$flag" "$cmd"
 
   ret="$?"

tests/deploy_test.sh
@@ -312,7 +312,7 @@ function test_kernel_modules()
   local expected_output="Kernel: $version"
 
   # Compress modules for sending
-  local compress_cmd="tar -C $local_remote_path/lib/modules/$version --auto-compress -cf $to_deploy_path/$version.tar ."
+  local compress_cmd="tar -C $local_remote_path/lib/modules/ --auto-compress -cf $to_deploy_path/$version.tar $version"
 
   # Rsync modules
   local rsync_tarball="$rsync_cmd $to_deploy_path/$version.tar $remote_access:$remote_path --rsync-path='sudo rsync'"

tests/kwlib_test.sh
@@ -435,7 +435,7 @@ function test_generate_tarball()
     './file2'
   )
 
-  output=$(generate_tarball "$path_to_compress" "$file_path" 'gzip' 'SUCCESS')
+  output=$(generate_tarball "$path_to_compress" "$file_path" 'gzip' '' 'SUCCESS')
   assertEquals "($LINENO)" "tar -C $path_to_compress --gzip -cf $file_path ." "$output"
 
   assertTrue 'Compressed file was not created' "[[ -f $SHUNIT_TMPDIR/compressed.tar.gz ]]"
@@ -443,7 +443,7 @@ function test_generate_tarball()
   output=$(tar -taf "$file_path" | sort -d)
   compare_command_sequence expected_files "$output" "$LINENO"
 
-  output=$(generate_tarball "$SHUNIT_TMPDIR/vacation/photos" "$file_path" 'gzip' 'SUCCESS')
+  output=$(generate_tarball "$SHUNIT_TMPDIR/vacation/photos" "$file_path" 'gzip' '' 'SUCCESS')
   assertEquals "($LINENO)" "$SHUNIT_TMPDIR/vacation/photos does not exist" "$output"
 
   output=$(generate_tarball "$path_to_compress" "$file_path" 'zipper')