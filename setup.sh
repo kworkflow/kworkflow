kworkflow/kworkflow

commit e30c0a86916b6affc3188610e5c5a187d9e77f7f

Make kw compliant with XDG Base Directory Specification
User customizations for kw folder should now be done with the XDG_*_HOME
environmental variables, instead of PREFIX. Also, ~/.kw is no longer
recognized as a path for a configuration file (unless it is supplied in
XDG_CONFIG_DIRS). This also fixes some problems with DEV-MODE: corrects
the paths to documentation, cache and data folders.

Closes: #296 and #385

Signed-off-by: João Seckler <jseckler@riseup.net>
Reviewed-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

setup.sh
@@ -10,7 +10,6 @@ debian_packages=(qemu git tar python3-docutils pulseaudio-utils dunst sphinx-doc
 SILENT=1
 VERBOSE=0
 FORCE=0
-PREFIX="${PREFIX:-$HOME/.local}"
 
 declare -r app_name="kw"
 
@@ -18,17 +17,16 @@ declare -r app_name="kw"
 ## Following are the install paths
 ##
 # Paths used during the installation process
-declare -r kwbinpath="$PREFIX/bin/kw"
-declare -r binpath="$PREFIX/bin"
-declare -r libdir="$PREFIX/lib/$app_name"
-declare -r sharedir="$PREFIX/share/"
-declare -r sharedocdir="$sharedir/doc"
-declare -r sharemandir="$sharedir/man"
-declare -r sharesounddir="$sharedir/sound/kw"
-declare -r etcdir="$PREFIX/etc/kw"
-# User specific data
-declare -r datadir="$HOME/.$app_name"
-declare -r cachedir="$HOME/.cache/$app_name"
+declare -r kwbinpath="$HOME/.local/bin/$app_name"
+declare -r binpath="$HOME/.local/bin"
+declare -r libdir="$HOME/.local/lib/$app_name"
+declare -r sharedir="${XDG_DATA_HOME:-"$HOME/.local/share"}/$app_name"
+declare -r docdir="$sharedir/doc"
+declare -r mandir="$sharedir/man"
+declare -r sounddir="$sharedir/sound"
+declare -r datadir="${XDG_DATA_HOME:-"$HOME/.local/share"}/$app_name"
+declare -r etcdir="${XDG_CONFIG_HOME:-"$HOME/.config"}/$app_name"
+declare -r cachedir="${XDG_CACHE_HOME:-"$HOME/.cache/$app_name"}"
 
 ##
 ## Source code references
@@ -36,8 +34,7 @@ declare -r cachedir="$HOME/.cache/$app_name"
 declare -r SRCDIR="src"
 declare -r MAN="documentation/man/"
 declare -r CONFIG_DIR="etc"
-declare -r INSTALLTO="$PREFIX"
-declare -r KW_CACHE_DIR="$HOME/.cache/$app_name"
+declare -r KW_CACHE_DIR="$cachedir"
 
 declare -r SOUNDS="sounds"
 declare -r BASH_AUTOCOMPLETE="bash_autocomplete"
@@ -162,6 +159,30 @@ function confirm_complete_removal()
   fi
 }
 
+# This function moves the folders from the old directory structure to
+# folders of the new one.
+function legacy_folders()
+{
+  local prefix="$HOME/.local"
+
+  if [[ -d "$prefix/share/doc" && -f "$prefix/share/man/kw.rst" &&
+    -d "$prefix/share/sound/kw" && -d "$prefix/etc/kw" &&
+    -d "$HOME/.kw" ]]; then
+
+    say 'Found an obsolete installation of kw:'
+
+    say "Moving files in $HOME/.kw/ to $datadir..."
+    mv "$HOME/.kw/"* "$datadir"
+    say "Moving $prefix/etc/kw to $etcdir..."
+    mv "$prefix/etc/kw" "$etcdir"
+    rm -rf "$prefix/share/doc"
+    rm -rf "$prefix/share/man/kw.rst"
+    rm -rf "$prefix/share/sound/kw"
+    rmdir --ignore-fail-on-non-empty "$prefix/share/man" "$prefix/share/sound" \
+      "$prefix/etc" "$HOME/.kw"
+  fi
+}
+
 function clean_legacy()
 {
   local completely_remove="$1"
@@ -179,13 +200,13 @@ function clean_legacy()
   [[ -d "$libdir" ]] && mv "$libdir" "$trash/lib"
 
   # Remove doc dir
-  [[ -d "$sharedocdir" ]] && mv "$sharedocdir" "$trash"
+  [[ -d "$docdir" ]] && mv "$docdir" "$trash"
 
   # Remove man
-  [[ -d "$sharemandir" ]] && mv "$sharemandir" "$trash"
+  [[ -d "$mandir" ]] && mv "$mandir" "$trash"
 
   # Remove sound files
-  [[ -d "$sharesounddir" ]] && mv "$sharesounddir" "$trash/sound"
+  [[ -d "$sounddir" ]] && mv "$sounddir" "$trash/sound"
 
   # Remove etc files
   [[ -d "$etcdir" ]] && mv "$etcdir" "$trash/etc"
@@ -216,7 +237,7 @@ function setup_config_file()
 
   if [[ -f "$config_file_template" ]]; then
     cp "$config_file_template" "$config_files_path/$global_config_name"
-    sed -i -e "s/USERKW/$USER/g" -e "s,SOUNDPATH,$sharesounddir,g" \
+    sed -i -e "s/USERKW/$USER/g" -e "s,SOUNDPATH,$sounddir,g" \
       -e "/^#?.*/d" "$config_files_path/$global_config_name"
     ret="$?"
     if [[ "$ret" != 0 ]]; then
@@ -250,36 +271,33 @@ function synchronize_files()
   cmd_output_manager "cp $app_name $binpath" "$verbose"
   ASSERT_IF_NOT_EQ_ZERO "The command 'cp $app_name $binpath' failed" "$?"
 
-  sed -i -e "s,##KW_INSTALL_PREFIX_TOKEN##,$PREFIX,g" "$binpath/$app_name"
-  sed -i -e "/##BEGIN-DEV-MODE##/,/##END-DEV-MODE##/ d" "$binpath/$app_name"
-
   # Lib files
   mkdir -p "$libdir"
   cmd_output_manager "rsync -vr $SRCDIR/ $libdir" "$verbose"
   ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $SRCDIR $libdir' failed" "$?"
 
   # Sound files
-  mkdir -p "$sharesounddir"
-  cmd_output_manager "rsync -vr $SOUNDS/ $sharesounddir" "$verbose"
-  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $SOUNDS $sharesounddir' failed" "$?"
+  mkdir -p "$sounddir"
+  cmd_output_manager "rsync -vr $SOUNDS/ $sounddir" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $SOUNDS $sounddir' failed" "$?"
   ## TODO: Remove me one day
   # Old kworkflow.config uses complete.wav instead of bell
-  ln -s "$sharesounddir/bell.wav" "$sharesounddir/complete.wav"
+  ln -s "$sounddir/bell.wav" "$sounddir/complete.wav"
 
   # Documentation files
-  mkdir -p "$sharedocdir"
-  cmd_output_manager "rsync -vr $DOCUMENTATION/ $sharedocdir" "$verbose"
-  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $DOCUMENTATION $sharedocdir' failed" "$?"
+  mkdir -p "$docdir"
+  cmd_output_manager "rsync -vr $DOCUMENTATION/ $docdir" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $DOCUMENTATION $docdir' failed" "$?"
 
   # man file
-  mkdir -p "$sharemandir"
-  cmd_output_manager "sphinx-build -nW -b man $DOCUMENTATION $sharemandir" "$verbose"
-  ASSERT_IF_NOT_EQ_ZERO "'sphinx-build -nW -b man $DOCUMENTATION $sharemandir' failed" "$?"
+  mkdir -p "$mandir"
+  cmd_output_manager "sphinx-build -nW -b man $DOCUMENTATION $mandir" "$verbose"
+  ASSERT_IF_NOT_EQ_ZERO "'sphinx-build -nW -b man $DOCUMENTATION $mandir' failed" "$?"
 
   # etc files
   mkdir -p "$etcdir"
   cmd_output_manager "rsync -vr $CONFIG_DIR/ $etcdir" "$verbose"
-  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $CONFIG_DIR $INSTALLTO' failed" "$?"
+  ASSERT_IF_NOT_EQ_ZERO "The command 'rsync -vr $CONFIG_DIR/ $etcdir $verbose' failed" "$?"
 
   # User data
   mkdir -p "$datadir"
@@ -317,7 +335,7 @@ function synchronize_files()
   say "$SEPARATOR"
   # Create ~/.cache/kw for support some of the operations
   mkdir -p "$cachedir"
-  say "$app_name installed into $PREFIX"
+  say "$app_name installed into $HOME"
   warning " -> For a better experience with kw, please, open a new terminal."
 }
 
@@ -358,6 +376,8 @@ function install_home()
   # Check Dependencies
   say "Checking dependencies ..."
   check_dependencies
+  # Move old folder structure to new one
+  legacy_folders
   # First clean old installation
   clean_legacy
   # Synchronize source files
@@ -376,10 +396,6 @@ for arg; do
   fi
   if [ "$arg" = "--force" ]; then
     FORCE=1
-    continue
-  fi
-  if [[ "$arg" =~ "--prefix=" ]]; then
-    PREFIX=${arg#*=}
     continue
   fi
   set -- "$@" "$arg"

kw
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-# Note: the following line should not be modified, and neither the token should
-# be repeated in this file because It will be replaced during installation with
-# the respective installation prefix.
-KW_INSTALL_PREFIX=${KW_INSTALL_PREFIX:-"##KW_INSTALL_PREFIX_TOKEN##"}
-
 KWORKFLOW=${KWORKFLOW:-"kw"}
+
+# Global paths
+
+# Kw shell files
+KW_LIB_DIR="${KW_LIB_DIR:-"$HOME/.local/lib"}/$KWORKFLOW"
+KW_PLUGINS_DIR="${KW_PLUGINS_DIR:-"$KW_LIB_DIR/plugins"}"
+
+# Share files
+KW_SHARE_DIR="${XDG_DATA_HOME:-"$HOME/.local/share"}/$KWORKFLOW"
+KW_DOC_DIR="$KW_SHARE_DIR/doc"
+KW_MAN_DIR="$KW_SHARE_DIR/man"
+KW_SOUND_DIR="$KW_SHARE_DIR/sound"
+
+# User specific data files (currently this collapses with the share dir,
+# but would not for a system-wide installation)
+KW_DATA_DIR="${XDG_DATA_HOME:-"$HOME/.local/share"}/$KWORKFLOW"
+
+# Configuration files
+KW_ETC_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}/$KWORKFLOW"
+
+# Cache folder
+KW_CACHE_DIR="${XDG_CACHE_HOME:-"$HOME/.cache"}/$KWORKFLOW"
+
+# State files
+KW_STATE_DIR="${XDG_STATE_HOME:-"$HOME/.local/state"}/$KWORKFLOW"
 
 ##BEGIN-DEV-MODE##
 KW_BIN="$(readlink -f "${BASH_SOURCE[0]}")"
@@ -13,19 +33,15 @@ KW_BASE_DIR="$(dirname "${KW_BIN}")"
 if [ -f "${KW_BASE_DIR}/src/kwlib.sh" ]; then
   # running from source directory
   KW_LIB_DIR="${KW_BASE_DIR}/src"
-  KW_INSTALL_PREFIX="${KW_BASE_DIR}"
+  # KW_DATA_DIR # use default data folder
+  KW_DOC_DIR="${KW_BASE_DIR}/documentation"
+  KW_MAN_DIR="${KW_BASE_DIR}/documentation/man"
+  KW_SOUND_DIR="${KW_BASE_DIR}/sound"
+  KW_ETC_DIR="${KW_BASE_DIR}/etc"
+  KW_PLUGINS_DIR="${KW_LIB_DIR}/plugins"
+  # KW_CACHE_DIR # use default cache folder
 fi
 ##END-DEV-MODE##
-
-# Global paths
-KW_LIB_DIR=${KW_LIB_DIR:-"$KW_INSTALL_PREFIX/lib/kw"}
-KW_SHARE_DOC_DIR=${KW_SHARE_DIR:-"$KW_INSTALL_PREFIX/share/doc/kw/"}
-KW_SHARE_MAN_DIR=${KW_SHARE_DIR:-"$KW_INSTALL_PREFIX/share/man"}
-KW_SHARE_SOUND_DIR=${KW_SHARE_DIR:-"$KW_INSTALL_PREFIX/share/sound/kw"}
-KW_ETC_DIR=${KW_ETC_DIR:-"$KW_INSTALL_PREFIX/etc/kw"}
-KW_DATA_DIR=${KW_DATA_DIR:-"$HOME/.kw"}
-KW_PLUGINS_DIR=${KW_PLUGINS_DIR:-"$KW_LIB_DIR/plugins"}
-KW_CACHE_DIR="$HOME/.cache/kw"
 
 # Export external variables required by kworkflow
 export KWORKFLOW

src/help.sh
@@ -41,7 +41,7 @@ function kworkflow_man()
 {
   feature="$1"
   flag=${2:-'SILENT'}
-  doc="$KW_SHARE_MAN_DIR"
+  doc="$KW_MAN_DIR"
 
   if [[ -z "$feature" ]]; then
     feature='kw'

src/init.sh
@@ -29,7 +29,7 @@ function init_kw()
 
   if [[ -f "$config_file_template" ]]; then
     cp "$config_file_template" "$PWD/$name"
-    sed -i -e "s/USERKW/$USER/g" -e "s,SOUNDPATH,$KW_SHARE_SOUND_DIR,g" -e "/^#?.*/d" \
+    sed -i -e "s/USERKW/$USER/g" -e "s,SOUNDPATH,$KW_SOUND_DIR,g" -e "/^#?.*/d" \
       "$PWD/$name"
   else
     complain "No such: $config_file_template"

src/kw_config_loader.sh
@@ -141,8 +141,27 @@ function parse_configuration()
 # higher level setting definitions to overwrite lower level ones.
 function load_configuration()
 {
+  local -a config_dirs
+  local config_dirs_size
+  local IFS=:
+  read -ra config_dirs <<< "${XDG_CONFIG_DIRS:-"/etc/xdg"}"
+  unset IFS
+
   parse_configuration "$KW_ETC_DIR/$CONFIG_FILENAME"
-  parse_configuration "$HOME/.kw/$CONFIG_FILENAME"
+
+  # XDG_CONFIG_DIRS is a colon-separated list of directories for config
+  # files to be searched, in order of preference. Since this function
+  # reads config files in a reversed order of preference, we must
+  # traverse it from back to top. Example: if
+  # XDG_CONFIG_DIRS=/etc/xdg:/home/user/myconfig:/etc/myconfig
+  # we will want to parse /etc/myconfig, then /home/user/myconfig, then
+  # /etc/xdg.
+  config_dirs_size="${#config_dirs[@]}"
+  for ((i = config_dirs_size - 1; i >= 0; i--)); do
+    parse_configuration "${config_dirs["$i"]}/$KWORKFLOW/$CONFIG_FILENAME"
+  done
+
+  parse_configuration "${XDG_CONFIG_HOME:-"$HOME/.config"}/$KWORKFLOW/$CONFIG_FILENAME"
   parse_configuration "$PWD/$CONFIG_FILENAME"
 }
 

tests/help_test.sh
@@ -12,7 +12,7 @@ function test_kworkflow_help()
 
 function test_kworkflow_man()
 {
-  export KW_SHARE_MAN_DIR="$SHUNIT_TMPDIR"
+  export KW_MAN_DIR="$SHUNIT_TMPDIR"
   touch "$SHUNIT_TMPDIR/kw.1"
   expect="man -l $SHUNIT_TMPDIR/kw.1"
   output=$(kworkflow_man '' 'TEST_MODE')

tests/init_test.sh
@@ -8,11 +8,11 @@ FAKE_CONFIG_PATH="$FAKE_DIR/.config"
 
 function setUp()
 {
-  export KW_ETC_DIR="tests/samples"
-  export KW_SHARE_SOUND_DIR="tests/samples/share/sound/kw"
+  export KW_ETC_DIR='tests/samples'
+  export KW_SOUND_DIR='tests/samples/share/sound/kw'
   export HOME="$FAKE_DIR"
-  export USER="kw_test"
-  export KWORKFLOW="kw_dir_test"
+  export USER='kw_test'
+  export KWORKFLOW='kw_dir_test'
   export PWD="$FAKE_CONFIG_PATH/$KWORKFLOW"
   mkdir -p "$FAKE_DIR"
   mkdir -p "$FAKE_CONFIG_PATH/$KWORKFLOW"
@@ -35,8 +35,8 @@ function test_init_kw()
 
   assertEquals "($LINENO): USERKW wasn't updated to $USER" "$USER" "$kworkflow_content"
 
-  kworkflow_content=$(grep "$KW_SHARE_SOUND_DIR" -o "$path_config" | head -n 1)
-  assertEquals "($LINENO): SOUNDPATH wasn't updated to $KW_SHARE_SOUND_DIR" "$KW_SHARE_SOUND_DIR" "$kworkflow_content"
+  kworkflow_content=$(grep "$KW_SOUND_DIR" -o "$path_config" | head -n 1)
+  assertEquals "($LINENO): SOUNDPATH wasn't updated to $KW_SOUND_DIR" "$KW_SOUND_DIR" "$kworkflow_content"
 
   output=$(init_kw --force)
   if [[ ! -f "$path_config.old" ]]; then

tests/kw_config_loader_test.sh
@@ -3,6 +3,8 @@
 include './tests/utils.sh'
 include './src/kw_config_loader.sh'
 
+KWORKFLOW='kw'
+
 TMP_DIR=tests/.tmp_kw_config_loader_test
 
 function setUp()
@@ -119,11 +121,52 @@ function test_parse_configuration_standard_config()
   true # Reset return value
 }
 
+# To test the order of config file loading, we will put a file named
+# kworkflow.config in each place, in order, and remove the previous one.
+# The order is: PWD, XDG_CONFIG_HOME, XDG_CONFIG_DIRS, KW_ETC_DIR
 function test_parse_configuration_files_loading_order()
 {
-  expected="$KW_ETC_DIR/$CONFIG_FILENAME
-$HOME/.kw/$CONFIG_FILENAME
-$PWD/$CONFIG_FILENAME"
+  local expected
+  local original_dir="$PWD"
+
+  cd "$SHUNIT_TMPDIR" || {
+    fail "($LINENO) It was nos possible to move to temporary directory"
+    return
+  }
+
+  KW_ETC_DIR="1"
+  XDG_CONFIG_DIRS="2:3:4"
+  XDG_CONFIG_HOME="5"
+
+  expected=(
+    "1/$CONFIG_FILENAME"
+    "4/$KWORKFLOW/$CONFIG_FILENAME"
+    "3/$KWORKFLOW/$CONFIG_FILENAME"
+    "2/$KWORKFLOW/$CONFIG_FILENAME"
+    "5/$KWORKFLOW/$CONFIG_FILENAME"
+    "$PWD/$CONFIG_FILENAME"
+  )
+
+  output="$(
+    function parse_configuration()
+    {
+      echo "$@"
+    }
+    load_configuration
+  )"
+  compare_command_sequence 'expected' "$output" "$LINENO - Wrong config file reading order."
+
+  # IF XDG global variables are not defined
+  unset XDG_CONFIG_DIRS
+  unset XDG_CONFIG_HOME
+  HOME="5"
+
+  expected=(
+    "1/$CONFIG_FILENAME"
+    "/etc/xdg/$KWORKFLOW/$CONFIG_FILENAME"
+    "5/.config/$KWORKFLOW/$CONFIG_FILENAME"
+    "$PWD/$CONFIG_FILENAME"
+  )
 
   output="$(
     function parse_configuration()
@@ -133,10 +176,12 @@ $PWD/$CONFIG_FILENAME"
     load_configuration
   )"
 
-  expected_vs_got="Expected:\n>>>$expected<<<\nGot:\n>>>$output<<<"
+  compare_command_sequence 'expected' "$output" "$LINENO - Wrong config file reading order."
 
-  [[ "$output" == "$expected" ]]
-  assertTrue "Wrong config file reading order.\n$expected_vs_got" $?
+  cd "$original_dir" || {
+    fail "($LINENO) It was nos possible to move back to original directory"
+    return
+  }
 }
 
 function test_show_variables_completeness()

commit 542bc7d43cc91b78893f8512d2a1fbfe4b058457

setup: Fix legacy_folders condition
We recently updated kw to make it compliant with XDG, and we added a
function to migrate the old kw config to the new model automatically.
However, one of the conditions is always false, and for this reason, we
could not update the file location for features like configm and
pomodoro. This commit fixes this issue by handling the migration case by
case.

Fixes: e30c0a86 ("Make kw compliant with XDG Base Directory Specification")
Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

setup.sh
@@ -165,22 +165,29 @@ function legacy_folders()
 {
   local prefix="$HOME/.local"
 
-  if [[ -d "$prefix/share/doc" && -f "$prefix/share/man/kw.rst" &&
-    -d "$prefix/share/sound/kw" && -d "$prefix/etc/kw" &&
-    -d "$HOME/.kw" ]]; then
-
+  if [[ -d "$HOME/.kw" ]]; then
     say 'Found an obsolete installation of kw:'
-
     say "Moving files in $HOME/.kw/ to $datadir..."
-    mv "$HOME/.kw/"* "$datadir"
-    say "Moving $prefix/etc/kw to $etcdir..."
-    mv "$prefix/etc/kw" "$etcdir"
-    rm -rf "$prefix/share/doc"
-    rm -rf "$prefix/share/man/kw.rst"
-    rm -rf "$prefix/share/sound/kw"
-    rmdir --ignore-fail-on-non-empty "$prefix/share/man" "$prefix/share/sound" \
-      "$prefix/etc" "$HOME/.kw"
+    rsync -a "$HOME/.kw/" "$datadir"
+
+    rm -rf "$HOME/.kw"
   fi
+
+  [[ -d "$prefix/share/doc" ]] && rm -rf "$prefix/share/doc"
+  [[ -f "$prefix/share/man/kw.rst" ]] && rm -rf "$prefix/share/man/kw.rst"
+  [[ -d "$prefix/share/sound/kw" ]] && rm -rf "$prefix/share/sound/kw"
+  [[ -d "$prefix/share/man" ]] && rm -rf "$prefix/share/man"
+  [[ -d "$prefix/share/sound" ]] && rm -rf "$prefix/share/sound"
+
+  # Legacy global config
+  if [[ -d "$prefix/etc/kw/" ]]; then
+    say "Moving $prefix/etc/kw to $etcdir..."
+    rsync -a "$prefix/etc/kw/" "$etcdir"
+    # We already check "$prefix"
+    # shellcheck disable=SC2115
+    rm -rf "$prefix/etc"
+  fi
+
 }
 
 function clean_legacy()