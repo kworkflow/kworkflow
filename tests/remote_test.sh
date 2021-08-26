kworkflow/kworkflow

commit e5e8d371eab517adbb8ec5621ba195fb4b7e59d5

src: remote: Get user as a parameter
We currently don't have a simple way to get user information under kw,
limiting our scope. This commit introduces the support for handling
users to be used via SSH.

Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
Reviewed-by: Alan Barzilay <alan.barzilay@gmail.com>
Reviewed-by: João Seckler <jseckler@riseup.net>

src/remote.sh
@@ -235,22 +235,31 @@ function generate_tarball()
 function populate_remote_info()
 {
   local ip="$1"
-  local port
+  local temp_ip
+  local port=22
+  local user='root'
 
   if [[ -z "$ip" ]]; then
     remote_parameters['REMOTE_IP']=${configurations[ssh_ip]}
     remote_parameters['REMOTE_PORT']=${configurations[ssh_port]}
-  else
-    temp_ip=$(get_based_on_delimiter "$ip" ":" 1)
-    # 22 in the conditon refers to EINVAL
-    if [[ "$?" == 22 ]]; then
-      remote_parameters['REMOTE_IP']="$ip"
-      remote_parameters['REMOTE_PORT']=22
-    else
-      port=$(get_based_on_delimiter "$ip" ":" 2)
-      remote_parameters['REMOTE_IP']="$temp_ip"
-      remote_parameters['REMOTE_PORT']="$port"
-    fi
+    remote_parameters['REMOTE_USER']=${configurations[ssh_user]}
+  else # CLI
+    # Handling port
+    remote_parameters['REMOTE_PORT']="$port"
+    port=$(get_based_on_delimiter "$ip" ':' 2)
+    # Use the specific port
+    [[ "$?" != 22 ]] && remote_parameters['REMOTE_PORT']="$port"
+
+    # Handling user
+    remote_parameters['REMOTE_USER']="$user"
+    user=$(get_based_on_delimiter "$ip" '@' 1)
+    # There is no specific user, let's assume root
+    [[ "$?" != 22 ]] && remote_parameters['REMOTE_USER']="$user"
+
+    # Handling IP by making sure that we eliminate user and port
+    temp_ip=$(get_based_on_delimiter "$ip" ':' 1)
+    temp_ip=$(get_based_on_delimiter "$temp_ip" '@' 2)
+    remote_parameters['REMOTE_IP']="$temp_ip"
   fi
 
   ip="${remote_parameters['REMOTE_IP']}:${remote_parameters['REMOTE_PORT']}"

tests/remote_test.sh
@@ -69,20 +69,33 @@ function test_populate_remote_info()
   # Force an unspected error
   configurations=()
 
+  populate_remote_info 'localhost'
+  assertEquals "($LINENO) Expected localhost" 'localhost' "${remote_parameters['REMOTE_IP']}"
+  assertEquals "($LINENO) Expected 22" 22 "${remote_parameters['REMOTE_PORT']}"
+  assertEquals "($LINENO) Expected root" 'root' "${remote_parameters['REMOTE_USER']}"
+
   populate_remote_info 'localhost:6789'
   assertEquals "($LINENO) Expected localhost" 'localhost' "${remote_parameters['REMOTE_IP']}"
   assertEquals "($LINENO) Expected 6789" 6789 "${remote_parameters['REMOTE_PORT']}"
+  assertEquals "($LINENO) Expected root" 'root' "${remote_parameters['REMOTE_USER']}"
 
   populate_remote_info 'localhost'
   assertEquals "($LINENO) Expected localhost" 'localhost' "${remote_parameters['REMOTE_IP']}"
   assertEquals "($LINENO) Expected 22" 22 "${remote_parameters['REMOTE_PORT']}"
+  assertEquals "($LINENO) Expected root" 'root' "${remote_parameters['REMOTE_USER']}"
+
+  populate_remote_info 'ada@localhost:3773'
+  assertEquals "($LINENO) Expected localhost" 'localhost' "${remote_parameters['REMOTE_IP']}"
+  assertEquals "($LINENO) Expected 3773" 3773 "${remote_parameters['REMOTE_PORT']}"
+  assertEquals "($LINENO) Expected ada" 'ada' "${remote_parameters['REMOTE_USER']}"
 
   # Let's check with a config file information
   parse_configuration "$KW_CONFIG_SAMPLE"
 
-  populate_remote_info
+  populate_remote_info ''
   assertEquals "($LINENO) Expected 127.0.0.1" '127.0.0.1' "${remote_parameters['REMOTE_IP']}"
   assertEquals "($LINENO) Expected 3333" 3333 "${remote_parameters['REMOTE_PORT']}"
+  assertEquals "($LINENO) Expected juca" 'juca' "${remote_parameters['REMOTE_USER']}"
 
   # Let's check a failure case
   remote_parameters=()

etc/kworkflow_template.config
@@ -6,6 +6,9 @@
 #? - USERKW will be replaced by the current user
 #? - SOUNDPATH will be replaced by kw's install path
 
+# Default ssh user
+ssh_user=root
+
 # Default ssh ip
 ssh_ip=localhost
 

src/kw_config_loader.sh
@@ -33,6 +33,7 @@ function show_variables()
   fi
 
   local -Ar ssh=(
+    [ssh_user]='SSH user'
     [ssh_ip]='SSH address'
     [ssh_port]='SSH port'
   )

tests/kw_config_loader_test.sh
@@ -60,6 +60,7 @@ function test_parse_configuration_output()
     [cross_compile]="aarch64-linux-gnu-"
     [virtualizer]="libvirt"
     [qemu_path_image]="/home/xpto/p/virty.qcow2"
+    [ssh_user]='juca'
     [ssh_ip]="127.0.0.1"
     [ssh_port]="3333"
     [mount_point]="/home/lala"
@@ -99,6 +100,7 @@ function test_parse_configuration_standard_config()
     [qemu_path_image]="/home/USERKW/p/virty.qcow2"
     [qemu_hw_options]="-enable-kvm -daemonize -smp 2 -m 1024"
     [qemu_net_options]="-nic user,hostfwd=tcp::2222-:22,smb=/home/USERKW"
+    [ssh_user]='root'
     [ssh_ip]="localhost"
     [ssh_port]="22"
     [mount_point]="/home/USERKW/p/mount"

tests/samples/kworkflow.config
@@ -1,3 +1,4 @@
+ssh_user=juca
 ssh_ip=127.0.0.1
 ssh_port=3333
 arch=arm64

commit a407f44350b744444b0f358864365064d9212b07

src: remote: Update code to correctly uses remote_parameters
I made a patch that refactors how we use remote info to enable users to
specify a user name for ssh command. Unfortunately, I did not replace
the configurations[ssh_*] to remote_parameters[*] everywhere, and some
inconsistent results appeared. This commit addresses some of these
issues and also updates the remote test to this new context.

Fixes: e5e8d37 ("src: remote: Get user as a parameter")
Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

src/deploy.sh
@@ -65,7 +65,6 @@ function kernel_deploy()
   single_line="${options_values['LS_LINE']}"
   list="${options_values['LS']}"
   uninstall="${options_values['UNINSTALL']}"
-  remote="${remote_parameters['REMOTE']}"
 
   if [[ "$list" == 1 || "$single_line" == 1 ]]; then
     say "Available kernels:"
@@ -166,7 +165,9 @@ function parse_deploy_options()
   options_values['REBOOT']=0
   options_values['MENU_CONFIG']='nconfig'
 
-  remote_parameters['REMOTE']=''
+  remote_parameters['REMOTE_IP']=''
+  remote_parameters['REMOTE_PORT']=''
+  remote_parameters['REMOTE_USER']=''
 
   # Set basic default values
   if [[ -n ${configurations[default_deploy_target]} ]]; then
@@ -421,8 +422,9 @@ function modules_install()
 
       remote="${remote_parameters['REMOTE_IP']}"
       port="${remote_parameters['REMOTE_PORT']}"
+      user="${remote_parameters['REMOTE_USER']}"
 
-      prepare_remote_dir "$remote" "$port" "" "$flag"
+      prepare_remote_dir "$remote" "$port" "$user" "$flag"
 
       # 2. Send files modules
       modules_install_to "$KW_CACHE_DIR/$LOCAL_REMOTE_DIR/" "$flag"

src/remote.sh
@@ -77,9 +77,9 @@ function cp_host2remote()
 {
   local src=${1:-"$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"}
   local dst=${2:-"$REMOTE_KW_DEPLOY"}
-  local remote=${3:-${configurations[ssh_ip]}}
-  local port=${4:-${configurations[ssh_port]}}
-  local user=${5:-${configurations[ssh_user]}}
+  local remote=${3:-${remote_parameters['REMOTE_IP']}}
+  local port=${4:-${remote_parameters['REMOTE_PORT']}}
+  local user=${5:-${remote_parameters['REMOTE_USER']}}
   local flag=${6:-"HIGHLIGHT_CMD"}
 
   if [[ -v configurations['ssh_configfile'] && -v configurations['hostname'] ]]; then
@@ -103,12 +103,12 @@ function cp_host2remote()
 # subshell and save it to a variable.
 function which_distro()
 {
-  local remote=${1:-${configurations[ssh_ip]}}
-  local port=${2:-${configurations[ssh_port]}}
-  local user=${3:-${configurations[ssh_user]}}
+  local remote=${1:-${remote_parameters[ssh_ip]}}
+  local port=${2:-${remote_parameters[ssh_port]}}
+  local user=${3:-${remote_parameters[ssh_user]}}
   local flag=${4:-"SILENT"}
 
-  cmd="cat /etc/os-release | grep -w ID | cut -d = -f 2"
+  cmd='cat /etc/os-release | grep -w ID | cut -d = -f 2'
   cmd_remotely "$cmd" "$flag" "$remote" "$port" "$user"
 }
 

tests/remote_test.sh
@@ -29,6 +29,9 @@ function oneTimeSetUp()
   mkdir -p "$FAKE_KW"
   mkdir -p "$TEST_PATH"
 
+  cp -f 'tests/samples/kworkflow.config' "$TEST_PATH"
+  cp -f 'tests/samples/dmesg' "$TEST_PATH"
+
   local -r current_path="$PWD"
 
   cp -f tests/samples/kworkflow.config "$TEST_PATH"
@@ -39,6 +42,7 @@ function oneTimeSetUp()
     return
   }
   load_configuration
+  populate_remote_info ''
   cd "$current_path" || {
     fail "($LINENO) It was not possible return to original directory"
     return
@@ -47,10 +51,15 @@ function oneTimeSetUp()
   local -r kernel_install_path="kernel_install"
 
   export KW_CACHE_DIR="$FAKE_KW"
-  export KW_PLUGINS_DIR=$FAKE_KW
-  export DEPLOY_SCRIPT=$FAKE_KW/$kernel_install_path/deploy.sh
+  export KW_PLUGINS_DIR="$FAKE_KW"
+  export DEPLOY_SCRIPT="$FAKE_KW/$kernel_install_path/deploy.sh"
   export DEPLOY_SCRIPT_SUPPORT="$FAKE_KW/$kernel_install_path/utils.sh"
+  export KW_ETC_DIR="$TEST_PATH"
   export modules_path="$FAKE_KW/$kernel_install_path/lib/modules"
+  export INVALID_ARG='Invalid arguments'
+  export NO_SUCH_FILE='No such file'
+  export SSH_OK='ssh -p 3333 127.0.0.1'
+
   rm -rf "$FAKE_KW"
 
   mk_fake_remote "$FAKE_KW" "$modules_path"
@@ -59,6 +68,23 @@ function oneTimeSetUp()
 function oneTimeTearDown()
 {
   unset KW_CACHE_DIR
+  rm -rf "$SHUNIT_TMPDIR"
+}
+
+function setUp()
+{
+  local -r current_path="$PWD"
+
+  load_configuration
+  remote_parameters['REMOTE_IP']=${configurations[ssh_ip]}
+  remote_parameters['REMOTE_PORT']=${configurations[ssh_port]}
+  remote_parameters['REMOTE_USER']=${configurations[ssh_user]}
+}
+
+function tearDown()
+{
+  remote_parameters=()
+  configurations=()
 }
 
 function test_populate_remote_info()
@@ -107,160 +133,170 @@ function test_populate_remote_info()
 
 function test_cmd_remote()
 {
-  local command="ls -lah"
-  local remote="178.31.38.12"
-  local port="2222"
-  local user="kw"
-  local flag="TEST_MODE"
-  local ID
+  local command='ls -lah'
+  local remote='178.31.38.12'
+  local port='2222'
+  local user='kw'
+  local flag='TEST_MODE'
+
   parse_configuration "$SAMPLES_DIR/kworkflow_template.config"
 
-  ID=1
   expected_command="ssh -p $port $user@$remote sudo \"$command\""
   output=$(cmd_remotely "$command" "$flag" "$remote" "$port" "$user")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  assertEquals "($LINENO):" "$expected_command" "$output"
 
-  ID=2
   expected_command="ssh -p $port $user@localhost sudo \"$command\""
-  output=$(cmd_remotely "$command" "$flag" "" "$port" "$user")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  output=$(cmd_remotely "$command" "$flag" '' "$port" "$user")
+  assertEquals "($LINENO):" "$expected_command" "$output"
 
-  ID=3
   expected_command="ssh -p 22 $user@localhost sudo \"$command\""
-  output=$(cmd_remotely "$command" "$flag" "" "" "$user")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  output=$(cmd_remotely "$command" "$flag" '' '' "$user")
+  assertEquals "($LINENO):" "$expected_command" "$output"
 
-  ID=4
   expected_command="ssh -p 22 root@localhost sudo \"$command\""
   output=$(cmd_remotely "$command" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  assertEquals "($LINENO):" "$expected_command" "$output"
 
-  ID=5
   expected_command="No command specified"
-  output=$(cmd_remotely "" "$flag")
-  assertEquals "cmd_remotely should not work ($ID)" "$expected_command" "$output"
+  output=$(cmd_remotely '' "$flag")
+  assertEquals "($LINENO):" "$expected_command" "$output"
 }
 
 function test_cp_host2remote()
 {
-  local src="/any/path"
-  local dst="/any/path/2"
-  local remote="172.16.224.1"
-  local port="2222"
-  local user="kw"
-  local flag="TEST_MODE"
-  local ID
+  local src='/any/path'
+  local dst='/any/path/2'
+  local remote='172.16.224.1'
+  local port='2222'
+  local user='kw'
+  local flag='TEST_MODE'
+  declare -a expected_cmd=()
 
-  ID=1
-  expected_command="rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'
-ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
+  # Load default configureation, because we want to test default values
   output=$(cp_host2remote "$src" "$dst" "$remote" "$port" "$user" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  expected_command=(
+    "rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'"
+    "ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
+  )
 
-  ID=2
+  compare_command_sequence 'expected_command' "$output" "$LINENO"
+
   src="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"
-  expected_command="rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'
-ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
-  output=$(cp_host2remote "" "$dst" "$remote" "$port" "$user" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  expected_command=(
+    "rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'"
+    "ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
+  )
 
-  ID=3
-  src="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"
-  dst="$REMOTE_KW_DEPLOY"
-  expected_command="rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'
-ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
-  output=$(cp_host2remote "" "" "$remote" "$port" "$user" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  output=$(cp_host2remote '' "$dst" "$remote" "$port" "$user" "$flag")
+  compare_command_sequence 'expected_command' "$output" "$LINENO"
 
-  ID=4
   src="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"
   dst="$REMOTE_KW_DEPLOY"
-  expected_command="rsync -e 'ssh -p $port' -La $src $user@localhost:$dst --rsync-path='sudo rsync'
-ssh -p $port ${user}@localhost sudo \"chown -R root:root $dst\""
-  output=$(cp_host2remote "" "" "" "$port" "$user" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  expected_command=(
+    "rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'"
+    "ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
+  )
 
-  ID=5
+  output=$(cp_host2remote '' '' "$remote" "$port" "$user" "$flag")
+  compare_command_sequence 'expected_command' "$output" "$LINENO"
+
   src="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"
   dst="$REMOTE_KW_DEPLOY"
-  expected_command="rsync -e 'ssh -p 22' -La $src $user@localhost:$dst --rsync-path='sudo rsync'
-ssh -p 22 ${user}@localhost sudo \"chown -R root:root $dst\""
-  output=$(cp_host2remote "" "" "" "" "$user" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  remote='127.0.0.1'
+  expected_command=(
+    "rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'"
+    "ssh -p $port ${user}@${remote} sudo \"chown -R root:root $dst\""
+  )
 
-  ID=6
+  output=$(cp_host2remote '' '' '' "$port" "$user" "$flag")
+  compare_command_sequence 'expected_command' "$output" "$LINENO"
+
   src="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"
   dst="$REMOTE_KW_DEPLOY"
-  expected_command="rsync -e 'ssh -p 22' -La $src root@localhost:$dst --rsync-path='sudo rsync'
-ssh -p 22 root@localhost sudo \"chown -R root:root $dst\""
-  output=$(cp_host2remote "" "" "" "" "" "$flag")
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  remote='127.0.0.1'
+  port='3333'
+  expected_command=(
+    "rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'"
+    "ssh -p $port ${user}@$remote sudo \"chown -R root:root $dst\""
+  )
+
+  output=$(cp_host2remote '' '' '' '' "$user" "$flag")
+  compare_command_sequence 'expected_command' "$output" "$LINENO"
+
+  src="$KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR/*"
+  dst="$REMOTE_KW_DEPLOY"
+  remote='127.0.0.1'
+  port='3333'
+  user='juca'
+  expected_command=(
+    "rsync -e 'ssh -p $port' -La $src $user@$remote:$dst --rsync-path='sudo rsync'"
+    "ssh -p $port ${user}@$remote sudo \"chown -R root:root $dst\""
+  )
+
+  output=$(cp_host2remote '' '' '' '' '' "$flag")
+  compare_command_sequence 'expected_command' "$output" "$LINENO"
 }
 
 function test_which_distro()
 {
-  local cmd="cat /etc/os-release | grep -w ID | cut -d = -f 2"
-  local remote="172.16.224.1"
-  local user="root"
-  local port="2222"
-  local flag="TEST_MODE"
-  local ID
+  local cmd='cat /etc/os-release | grep -w ID | cut -d = -f 2'
+  local remote='172.16.224.1'
+  local user='xpto'
+  local port='2222'
+  local flag='TEST_MODE'
+  local expected_str
 
-  ID=1
   output=$(which_distro "$remote" "$port" "$user" "$flag")
-  expected_command="ssh -p $port $user@$remote sudo \"$cmd\""
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  expected_str="ssh -p $port $user@$remote sudo \"$cmd\""
+  assertEquals "($LINENO):" "$expected_str" "$output"
 
-  ID=2
-  output=$(which_distro "$remote" "$port" "" "$flag")
-  expected_command="ssh -p $port root@$remote sudo \"$cmd\""
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  user='juca'
+  output=$(which_distro "$remote" "$port" '' "$flag")
+  expected_str="ssh -p $port $user@$remote sudo \"$cmd\""
+  assertEquals "($LINENO)" "$expected_str" "$output"
 
-  ID=3
-  output=$(which_distro "$remote" "" "" "$flag")
-  expected_command="ssh -p 22 root@$remote sudo \"$cmd\""
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  user='juca'
+  port=3333
+  output=$(which_distro "$remote" '' '' "$flag")
+  expected_str="ssh -p $port $user@$remote sudo \"$cmd\""
+  assertEquals "($LINENO)" "$expected_str" "$output"
 
-  ID=2
-  output=$(which_distro "" "" "" "$flag")
-  expected_command="ssh -p 22 root@localhost sudo \"$cmd\""
-  assertEquals "Command did not match ($ID)" "$expected_command" "$output"
+  user='juca'
+  port=3333
+  remote='127.0.0.1'
+  output=$(which_distro '' '' '' "$flag")
+  expected_str="ssh -p $port $user@$remote sudo \"$cmd\""
+  assertEquals "Command did not match ($ID)" "$expected_str" "$output"
 }
 
 function test_preapre_host_deploy_dir()
 {
-  local ID
+  local output
+  local ret
 
   prepare_host_deploy_dir
 
-  ID=1
-  assertTrue "$ID - Check if kw dir was created" '[[ -d $KW_CACHE_DIR ]]'
+  assertTrue "($LINENO): Check if kw dir was created" '[[ -d $KW_CACHE_DIR ]]'
+  assertTrue "($LINENO): Check if kw dir was created" '[[ -d $KW_CACHE_DIR/$LOCAL_REMOTE_DIR ]]'
+  assertTrue "($LINENO): Check if kw dir was created" '[[ -d $KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR ]]'
 
-  ID=2
-  assertTrue "$ID - Check if kw dir was created" '[[ -d $KW_CACHE_DIR/$LOCAL_REMOTE_DIR ]]'
-
-  ID=3
-  assertTrue "$ID - Check if kw dir was created" '[[ -d $KW_CACHE_DIR/$LOCAL_TO_DEPLOY_DIR ]]'
-
-  ID=4
   oneTimeTearDown
 
   output=$(prepare_host_deploy_dir)
-  ret=$?
-  assertEquals "$ID - Expected an error" "22" "$ret"
+  ret="$?"
+  assertEquals "($LINENO): Expected an error" 22 "$ret"
 
   oneTimeSetUp
 }
 
 function test_prepare_remote_dir()
 {
-  local cmd="cat /etc/os-release | grep -w ID | cut -d = -f 2"
-  local remote="172.16.224.1"
-  local user="root"
-  local port="2222"
-  local flag="TEST_MODE"
+  local cmd='cat /etc/os-release | grep -w ID | cut -d = -f 2'
+  local remote='172.16.224.1'
+  local user='root'
+  local port='2222'
+  local flag='TEST_MODE'
   local count=0
-  local ID
 
   declare -a expected_cmd_sequence=(
     "ssh -p 2222 root@172.16.224.1 sudo \"mkdir -p /root/kw_deploy\""
@@ -270,62 +306,52 @@ function test_prepare_remote_dir()
     "ssh -p $port ${user}@${remote} sudo \"chown -R root:root /root/kw_deploy/\""
     "rsync -e 'ssh -p 2222' -La $FAKE_KW/kernel_install/utils.sh root@172.16.224.1:/root/kw_deploy/ --rsync-path='sudo rsync'"
     "ssh -p $port ${user}@${remote} sudo \"chown -R root:root /root/kw_deploy/\""
-
   )
 
   setupMockFunctions
+
   output=$(prepare_remote_dir "$remote" "$port" "$user" "$flag")
-  while read -r cmd; do
-    if [[ ${expected_cmd_sequence[$count]} != "${cmd}" ]]; then
-      fail "Expected command \"${cmd}\" to be \"${expected_cmd_sequence[$count]}\""
-    fi
-    ((count++))
-  done <<< "$output"
+  compare_command_sequence 'expected_cmd_sequence' "$output" "$LINENO"
 
   tearDownMockFunctions
 }
 
-### SSH tests ###
-#
-# NOTE: We're not testing the ssh command here, just the kw ssh operation
-#
-
-INVALID_ARG="Invalid arguments"
-NO_SUCH_FILE="No such file"
-
-SSH_OK="ssh -p 3333 127.0.0.1"
+#### SSH tests ###
+##
+## NOTE: We're not testing the ssh command here, just the kw ssh operation
+##
 
 function test_kw_ssh_check_fail_cases()
 {
-  local args="--lala"
+  local args='--lala'
   local ret
 
-  ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
+  ret=$(kw_ssh "$args")
+  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
-  args="-m"
-  ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
+  args='-m'
+  ret=$(kw_ssh "$args")
+  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
-  args="-d="
-  ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
+  args='-d='
+  ret=$(kw_ssh "$args")
+  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
-  args="-c"
-  ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
+  args='-c'
+  ret=$(kw_ssh "$args")
+  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
-  args="-s"
-  ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
+  args='-s'
+  ret=$(kw_ssh "$args")
+  assertTrue "($LINENO): We expected a substring \"$INVALID_ARG: $args\", but we got \"$ret\"" '[[ $ret =~ "$INVALID_ARG: $args" ]]'
 
-  args="-s="
+  args='-s='
   ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
+  assertTrue "($LINENO): We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
 
-  args="--script="
-  ret=$(kw_ssh $args)
-  assertTrue "We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
+  args='--script='
+  ret=$(kw_ssh "$args")
+  assertTrue "($LINENO): We expected a substring \"$NO_SUCH_FILE\", but we got \"$ret\"" '[[ $ret =~ "$NO_SUCH_FILE" ]]'
 }
 
 function test_kw_ssh_basic()