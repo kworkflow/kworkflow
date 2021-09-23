406b190aa1b9c08a009814d2116e1b9ea5491084
Eduardo Pinheiro <eduardo.pinheiro482@hotmail.com>
2021-09-22 23:23:33 -0300

    src: vm: create vm option Moves the mount, umount and up under it. Solves #453
    Signed-off-by: Eduardo Pinheiro <eduardo.pinheiro482@hotmail.com>

--- src/vm.sh
+++ src/vm.sh
@@ -2,6 +2,48 @@ include "$KW_LIB_DIR/kw_config_loader.sh"
 include "$KW_LIB_DIR/kwlib.sh"
 
 declare -g prefix='/'
+declare -gA options_values
+
+function vm_main()
+{
+  if [[ "$#" -eq 0 ]]; then
+    complain 'Expected string or parameter. See man for detail.'
+    exit 22 # EINVAL
+  fi
+
+  if [[ "$1" =~ -h|--help ]]; then
+    vm_help "$1"
+    exit 0
+  fi
+
+  parse_vm_options "$@"
+  if [[ "$?" -gt 0 ]]; then
+    complain "${options_values['ERROR']}"
+    return 22 # EINVAL
+  fi
+
+  if [[ -n "${options_values['MOUNT']}" ]]; then
+    shift
+    vm_mount "$@"
+
+    alert_completion 'kw vm' "${options_values['ALERT_COMPLETION']}"
+    return "$?"
+  fi
+
+  if [[ -n "${options_values['UMOUNT']}" ]]; then
+    shift
+    vm_umount "$@"
+
+    alert_completion 'kw vm' "${options_values['ALERT_COMPLETION']}"
+    return "$?"
+  fi
+
+  if [[ -n "${options_values['UP']}" ]]; then
+    shift
+    vm_up "$@"
+    exit 0
+  fi
+}
 
 function vm_mount()
 {
@@ -11,11 +53,6 @@ function vm_mount()
   local guestmount_cmd
   local ret
   local distro
-
-  if [[ "$1" =~ -h|--help ]]; then
-    vm_help "$1" 'mount'
-    exit 0
-  fi
 
   flag=${flag:-'SILENT'}
   qemu_img_path="${qemu_img_path:-${configurations[qemu_path_image]}}"
@@ -61,11 +98,6 @@ function vm_umount()
   local guestumount_cmd
   local ret
 
-  if [[ "$1" =~ -h|--help ]]; then
-    vm_help "$1" 'umount'
-    exit 0
-  fi
-
   flag=${flag:-'SILENT'}
   qemu_img_path="${qemu_img_path:-${configurations[qemu_path_image]}}"
   mount_point_path="${mount_point_path:-${configurations[mount_point]}}"
@@ -91,11 +123,6 @@ function vm_up()
   local cmd
   local flag='SILENT'
 
-  if [[ "$1" =~ -h|--help ]]; then
-    vm_help "$1" 'up'
-    exit 0
-  fi
-
   say 'Starting Qemu with:'
   printf '%s' "${configurations[virtualizer]} " \
     "${configurations[qemu_hw_options]} " \
@@ -109,15 +136,66 @@ function vm_up()
   cmd_manager "$flag" "$cmd"
 }
 
+function parse_vm_options()
+{
+  local long_options='mount,umount,up,alert:'
+  local short_options='m,n,u'
+
+  options="$(kw_parse "$short_options" "$long_options" "$@")"
+
+  if [[ "$?" != 0 ]]; then
+    options_values['ERROR']="$(kw_parse_get_errors 'kw vm' "$short_options" \
+      "$long_options" "$@")"
+    return 22 # EINVAL
+  fi
+
+  # Default values
+  options_values['MOUNT']=''
+  options_values['UMOUNT']=''
+  options_values['UP']=''
+  options_values['ALERT_COMPLETION']=''
+
+  eval "set -- $options"
+
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      --mount | -m)
+        options_values['MOUNT']=1
+        shift
+        ;;
+      --umount | -n)
+        options_values['UMOUNT']=1
+        shift
+        ;;
+      --up | -u)
+        options_values['UP']=1
+        shift
+        ;;
+      --alert)
+        options_values['ALERT_COMPLETION']="--alert=$2"
+        shift 2
+        ;;
+      --) # End of options, beginning of arguments
+        shift
+        ;;
+      *)
+        options_values['ERROR']="Unrecognized argument: $1"
+        return 22 # EINVAL
+        shift
+        ;;
+    esac
+  done
+}
+
 function vm_help()
 {
   if [[ "$1" == --help ]]; then
     include "$KW_LIB_DIR/help.sh"
-    kworkflow_man "$2"
+    kworkflow_man 'vm'
     return
   fi
-  printf '%s\n' 'kw (mount | umount | up):' \
-    '  mo | mount - Mount VM' \
-    '  um | umount - Unmount VM' \
-    '  u | up - Start VM'
+  printf '%s\n' 'kw vm:' \
+    ' vm (-m|--mount) - Mount VM' \
+    ' vm (-n|--umount) - Unmount VM' \
+    ' vm (-u|--up) - Starts VM'
 }
