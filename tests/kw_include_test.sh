9e48f66dfd4ccb41ce121b4e9c4c397901024b53
Isabella Basso <isabellabdoamaral@usp.br>
2021-10-25 08:37:28 -0300

    src: kw_include: fix variable naming
    In some systems, kw couldn't deal properly with hidden folders
    (e.g. #473), as the variables created for naming includes had dots on
    them, which made them improper (i.e. the user would see 'not a valid
    identifier' errors popping up for every `kw` call).
    
    This commit swaps both dots and spaces for underscores in those file
    paths.
    
    Also, as the include command returns 0 in spite of the declare having
    failed, check the declare retval output to make sure it is working as
    expected.
    
    Signed-off-by: Isabella Basso <isabbasso@riseup.net>

--- tests/kw_include_test.sh
+++ tests/kw_include_test.sh
@@ -4,13 +4,51 @@ include './tests/utils.sh'
 
 function oneTimeSetUp()
 {
+  # specifies where to source the lib
   KW_LIB_DIR="$PWD/src"
+
+  # we'll use these folders to run our tests
+  KW_INC_TEST_PATH="$SHUNIT_TMPDIR/inc_test"
+  KW_INC_TEST_SPACED_PATH="$SHUNIT_TMPDIR/inc test"
+  KW_INC_TEST_HIDDEN_PATH="$SHUNIT_TMPDIR/.inc_test"
+  mkdir "$KW_INC_TEST_PATH" \
+    "$KW_INC_TEST_SPACED_PATH" \
+    "$KW_INC_TEST_HIDDEN_PATH"
+
+  # those test files can be empty, as we're only testing the created varname
+  test_files=(
+    "$KW_INC_TEST_HIDDEN_PATH/include_hidden.sh"
+    "$KW_INC_TEST_HIDDEN_PATH/include_test.sh"
+    "$KW_INC_TEST_SPACED_PATH/include_spaced.sh"
+    "$KW_INC_TEST_PATH/include_test.sh"
+  )
+
+  touch "${test_files[@]}"
+
+  # the next files will be checked for name collisions
+  printf "%s\n" "function test1(){ printf '%s\n' 'output of test1';}" > \
+    "$SHUNIT_TMPDIR/include_test_same_name"
+
+  printf "%s\n" "function test2(){ printf '%s\n' 'output of test2';}" > \
+    "$KW_INC_TEST_PATH/include_test_same_name"
+
+  printf "%s\n" "function test3(){ printf '%s\n' 'output of test3';}" > \
+    "$KW_INC_TEST_PATH/include_test_similar_path.sh"
+
+  printf "%s\n" "function test4(){ printf '%s\n' 'output of test4';}" > \
+    "$KW_INC_TEST_HIDDEN_PATH/include_test_similar_path.sh"
+}
+
+function oneTimeTearDown()
+{
+  rm -rf "$SHUNIT_TMPDIR"
+  mkdir -p "$SHUNIT_TMPDIR"
 }
 
 function test_include()
 {
   include ./src/kwio.sh
-  output="$KWIO_IMPORTED"
+  output="$KWIO_SH_IMPORTED"
   assertEquals "($LINENO)" 1 "$output"
 }
 
@@ -18,15 +56,13 @@ function test_include_twice()
 {
   include ./src/kwlib.sh
   include ./src/kwlib.sh
-  ret="$?"
-  assertEquals "($LINENO)" 0 "$ret"
+  assertEquals "($LINENO)" 0 "$?"
 }
 
 function test_include_wrong_path()
 {
   output=$(include ./src/batata.sh)
-  ret="$?"
-  assertEquals "($LINENO)" 2 "$ret"
+  assertEquals "($LINENO)" 2 "$?"
 }
 
 function test_include_same_name()
@@ -36,16 +72,47 @@ function test_include_same_name()
   local test1_expected
   local test2_expected
 
-  include 'tests/samples/include_test'
-  include 'tests/samples/include_test_dir/include_test'
+  include "$SHUNIT_TMPDIR/include_test_same_name"
+  include "$KW_INC_TEST_PATH/include_test_same_name"
 
   test1_output=$(test1)
   test2_output=$(test2)
   test1_expected='output of test1'
   test2_expected='output of test2'
 
-  assertEquals "($LINENO)" "${test1_output}" "${test1_expected}"
-  assertEquals "($LINENO)" "${test2_output}" "${test2_expected}"
+  assertEquals "($LINENO)" "$test1_output" "$test1_expected"
+  assertEquals "($LINENO)" "$test2_output" "$test2_expected"
+}
+
+function test_include_hidden_files()
+{
+  include "$KW_INC_TEST_HIDDEN_PATH/include_hidden.sh"
+  assertEquals "($LINENO)" 0 "$?"
+}
+
+function test_include_spaced_path()
+{
+  include "$KW_INC_TEST_SPACED_PATH/include_spaced.sh"
+  assertEquals "($LINENO)" 0 "$?"
+}
+
+function test_include_similar_hidden_path()
+{
+  local test3_output
+  local test4_output
+  local test3_expected
+  local test4_expected
+
+  include "$KW_INC_TEST_PATH/include_test_similar_path.sh"
+  include "$KW_INC_TEST_HIDDEN_PATH/include_test_similar_path.sh"
+
+  test3_output=$(test3)
+  test4_output=$(test4)
+  test3_expected='output of test3'
+  test4_expected='output of test4'
+
+  assertEquals "($LINENO)" "$test3_output" "$test3_expected"
+  assertEquals "($LINENO)" "$test4_output" "$test4_expected"
 }
 
 invoke_shunit
