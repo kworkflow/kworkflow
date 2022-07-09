kworkflow/kworkflow

commit 305d9e17c79b3659e0e2f95b86a93e584c3662f7

src: configm: Rename configm to kernel-config-manager
We plan to add a config interface similar to the git config option to
make it easier for new users to learn how to set up kw. However, we are
using the name configm for the kernel config manager feature, which will
be super confusing for users. This commit address this issue by renaming
configm to kernel-config-manager.

Signed-off-by: Rodrigo Siqueira <siqueirajordao@riseup.net>

src/help.sh
@@ -16,7 +16,7 @@ function kworkflow_help()
     '  build,b - Build kernel' \
     '  clear-cache - Clear files generated by kw' \
     '  codestyle,c - Apply checkpatch on directory or file' \
-    '  configm,g - Manage config files' \
+    '  kernel-config-manager,g - Manage config files' \
     '  debug - Linux kernel debug utilities' \
     '  deploy,d - Deploy a new kernel image to a target machine' \
     '  device - Show basic hardware information' \

documentation/conf.py
@@ -159,7 +159,7 @@ man_pages = [
     ('man/features/backup', 'backup', 'save and restore kw data', [author], 1),
     ('man/features/build', 'build', 'build the kernel', [author], 1),
     ('man/features/codestyle', 'codestyle', 'checkpatch wrapper', [author], 1),
-    ('man/features/configm', 'configm', 'config manager', [author], 1),
+    ('man/features/kernel-config-manager', 'kernel-config-manager', 'config manager', [author], 1),
     ('man/features/debug', 'debug', 'kernel debug', [author], 1),
     ('man/features/deploy', 'deploy', 'deploy the kernel', [author], 1),
     ('man/features/device', 'device', 'hardware information', [author], 1),

kw
@@ -143,11 +143,11 @@ function kw()
         execute_get_maintainer "$@"
       )
       ;;
-    configm | g)
+    kernel-config-manager | k)
       (
         include "$KW_LIB_DIR/config_manager.sh"
 
-        execute_config_manager "$@"
+        kernel_config_manager_main "$@"
       )
       ;;
     help | --help)

src/bash_autocomplete.sh
@@ -13,7 +13,7 @@ function _kw_autocomplete()
   current_command="${COMP_WORDS[$COMP_CWORD]}"
   previous_command="${COMP_WORDS[$comp_curr - 1]}"
 
-  kw_options['kw']='backup bd build clear-cache codestyle configm debug deploy
+  kw_options['kw']='backup bd build clear-cache codestyle kernel-config-manager debug deploy
                     device diff drm explore help init maintainers mail man mount
                     pomodoro report ssh umount up vars version'
 
@@ -23,9 +23,9 @@ function _kw_autocomplete()
                        --save-log-to --llvm --help'
   kw_options['b']="${kw_options['build']}"
 
-  kw_options['configm']='--fetch --get --list --remove --save --force
+  kw_options['kernel-config-manager']='--fetch --get --list --remove --save --force
                          --description --output --optimize --remote'
-  kw_options['g']="${kw_options['configm']}"
+  kw_options['k']="${kw_options['kernel-config-manager']}"
 
   kw_options['debug']='--local --remote --event --ftrace --dmesg --cmd
                        --history --disable --list --follow --reset --help'

src/kernel_config_manager.sh
@@ -9,13 +9,13 @@ declare -gA options_values
 declare -g root='/'
 declare -g PROC_CONFIG_PATH='/proc/config.gz'
 
-# This function handles the options available in 'configm'.
+# This function handles the options available in 'kernel-config-manager'.
 #
 # @* This parameter expects a list of parameters, such as '-n', '-d', and '-f'.
 #
 # Returns:
 # Return 0 if everything ends well, otherwise return an errno code.
-function execute_config_manager()
+function kernel_config_manager_main()
 {
   local name_config
   local description_config
@@ -37,7 +37,7 @@ function execute_config_manager()
     exit 0
   fi
 
-  parse_configm_options "$@"
+  parse_kernel_config_manager_options "$@"
 
   if [[ "$?" -gt 0 ]]; then
     complain "${options_values['ERROR']}"
@@ -464,7 +464,7 @@ function list_configs()
   done
 }
 
-# Remove and Get operation in the configm has similar criteria for working,
+# Remove and Get operation in the kernel-config-manager has similar criteria for working,
 # because of this, basic_config_validations centralize the basic requirement
 # validation.
 #
@@ -558,9 +558,9 @@ function remove_config()
   fi
 }
 
-# This function parses the options from 'kw configm', and populates the global
+# This function parses the options from 'kw kernel-config-manager', and populates the global
 # variable options_values accordingly.
-function parse_configm_options()
+function parse_kernel_config_manager_options()
 {
   local short_options
   local long_options
@@ -572,7 +572,7 @@ function parse_configm_options()
   options="$(kw_parse "$short_options" "$long_options" "$@")"
 
   if [[ "$?" != 0 ]]; then
-    options_values['ERROR']="$(kw_parse_get_errors 'kw configm' "$short_options" \
+    options_values['ERROR']="$(kw_parse_get_errors 'kw kernel-config-manager' "$short_options" \
       "$long_options" "$@")"
     return 22 # EINVAL
   fi
@@ -680,13 +680,13 @@ function config_manager_help()
 {
   if [[ "$1" == --help ]]; then
     include "$KW_LIB_DIR/help.sh"
-    kworkflow_man 'configm'
+    kworkflow_man 'kernel-config-manager'
     return
   fi
   printf '%s\n' 'kw config manager:' \
-    '  configm --fetch [(-o | --output) <filename>] [-f | --force] [--optimize] [--remote [<user>@<ip>:<port>]] - Fetch a config' \
-    '  configm (-s | --save) <name> [(-d | --description) <description>] [-f | --force] - Save a config' \
-    '  configm (-l | --list) - List config files under kw management' \
-    '  configm --get <name> [-f | --force] - Get a config labeled with <name>' \
-    '  configm (-r | --remove) <name> [-f | --force] - Remove config labeled with <name>'
+    '  kernel-config-manager --fetch [(-o | --output) <filename>] [-f | --force] [--optimize] [--remote [<user>@<ip>:<port>]] - Fetch a config' \
+    '  kernel-config-manager (-s | --save) <name> [(-d | --description) <description>] [-f | --force] - Save a config' \
+    '  kernel-config-manager (-l | --list) - List config files under kw management' \
+    '  kernel-config-manager --get <name> [-f | --force] - Get a config labeled with <name>' \
+    '  kernel-config-manager (-r | --remove) <name> [-f | --force] - Remove config labeled with <name>'
 }

tests/kernel_config_manager_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-include './src/config_manager.sh'
+include './src/kernel_config_manager.sh'
 include './tests/utils.sh'
 
 COMMAND_MSG_UNKNOWN='Unknown option'
@@ -50,30 +50,30 @@ function tearDown()
   unset expected_cmd
 }
 
-function test_execute_config_manager_SAVE_fails()
+function test_kernel_config_manager_main_SAVE_fails()
 {
   local msg_prefix=" --save"
   local ret
 
-  ret=$(execute_config_manager --save 2>&1 > /dev/null)
+  ret=$(kernel_config_manager_main --save 2>&1 > /dev/null)
   assert_equals_helper "$msg_prefix" "$LINENO" "$?" 22
 
-  ret=$(execute_config_manager --save --lala)
+  ret=$(kernel_config_manager_main --save --lala)
   assert_equals_helper "$msg_prefix --lala" "$LINENO" "$ret" "$COMMAND_MSG_INVALID_ARG"
 
-  ret=$(execute_config_manager --save -n)
+  ret=$(kernel_config_manager_main --save -n)
   assert_equals_helper "$msg_prefix -n" "$LINENO" "$ret" "$COMMAND_MSG_INVALID_ARG"
 
-  ret=$(execute_config_manager --save -d)
+  ret=$(kernel_config_manager_main --save -d)
   assert_equals_helper "$msg_prefix -d" "$LINENO" "$ret" "$COMMAND_MSG_INVALID_ARG"
 
-  ret=$(execute_config_manager --save -d)
+  ret=$(kernel_config_manager_main --save -d)
   assert_equals_helper "$msg_prefix -d" "$LINENO" "$ret" "$COMMAND_MSG_INVALID_ARG"
 
-  ret=$(execute_config_manager --save -d "lalala and xpto")
+  ret=$(kernel_config_manager_main --save -d "lalala and xpto")
   assert_equals_helper "$msg_prefix -d" "$LINENO" "$ret" "$COMMAND_MSG_INVALID_ARG"
 
-  ret=$(execute_config_manager --save -f)
+  ret=$(kernel_config_manager_main --save -f)
   assert_equals_helper "$msg_prefix -f" "$LINENO" "$ret" "$COMMAND_MSG_INVALID_ARG"
 }
 
@@ -302,14 +302,14 @@ function test_list_config_normal_output()
   assertTrue "$LINENO:$msg" '[[ $ret =~ $DESCRIPTION_2 ]]'
 }
 
-function test_execute_config_manager_get_config_invalid_option()
+function test_kernel_config_manager_main_get_config_invalid_option()
 {
   local msg_prefix=" --get"
 
-  ret=$(execute_config_manager --get 2>&1 > /dev/null)
+  ret=$(kernel_config_manager_main --get 2>&1 > /dev/null)
   assert_equals_helper "$msg_prefix" "$LINENO" "$?" 22
 
-  ret=$(execute_config_manager --get something_wrong)
+  ret=$(kernel_config_manager_main --get something_wrong)
   assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_NO_SUCH_FILE: something_wrong" "$ret"
 }
 
@@ -404,14 +404,14 @@ function test_get_config_with_force()
   assertTrue "$LINENO: We expected $CONTENT, but we got $ret" '[[ $ret =~ $CONTENT ]]'
 }
 
-function test_execute_config_manager_remove_that_should_fail()
+function test_kernel_config_manager_main_remove_that_should_fail()
 {
   local msg_prefix=" -r"
 
-  ret=$(execute_config_manager -r 2>&1 > /dev/null)
+  ret=$(kernel_config_manager_main -r 2>&1 > /dev/null)
   assert_equals_helper "$msg_prefix" "$LINENO" "$?" 22
 
-  ret=$(execute_config_manager -r something_wrong)
+  ret=$(kernel_config_manager_main -r something_wrong)
   assert_equals_helper "$msg_prefix" "$LINENO" "$COMMAND_NO_SUCH_FILE: something_wrong" "$ret"
 }
 
@@ -721,7 +721,7 @@ function test_fetch_config()
   }
 }
 
-function test_configm_parser()
+function test_kernel_config_manager_parser()
 {
   unset options_values
   declare -gA options_values
@@ -729,76 +729,76 @@ function test_configm_parser()
   local ret
 
   # Invalid options
-  parse_configm_options '--save'
+  parse_kernel_config_manager_options '--save'
   ret="$?"
-  parse_configm_options '-s'
+  parse_kernel_config_manager_options '-s'
   ret=$((ret + $?))
   assert_equals_helper 'Option without argument' "$LINENO" "$ret" 44
 
-  parse_configm_options '--remove'
+  parse_kernel_config_manager_options '--remove'
   ret="$?"
-  parse_configm_options '-r'
+  parse_kernel_config_manager_options '-r'
   ret=$((ret + $?))
   assert_equals_helper 'Option without argument' "$LINENO" "$ret" 44
 
-  parse_configm_options '--description'
+  parse_kernel_config_manager_options '--description'
   ret="$?"
-  parse_configm_options '-d'
+  parse_kernel_config_manager_options '-d'
   ret=$((ret + $?))
   assert_equals_helper 'Option without argument' "$LINENO" "$ret" 44
 
-  parse_configm_options '--output'
+  parse_kernel_config_manager_options '--output'
   ret="$?"
-  parse_configm_options '-o'
+  parse_kernel_config_manager_options '-o'
   ret=$((ret + $?))
   assert_equals_helper 'Option without argument' "$LINENO" "$ret" 44
 
-  parse_configm_options '--get'
+  parse_kernel_config_manager_options '--get'
   ret="$?"
   assert_equals_helper 'Option without argument' "$LINENO" "$ret" 22
 
-  parse_configm_options '--remote'
+  parse_kernel_config_manager_options '--remote'
   ret="$?"
   assert_equals_helper 'Option without argument' "$LINENO" "$ret" 22
 
-  parse_configm_options '--LalaXpto' 'lala xpto'
+  parse_kernel_config_manager_options '--LalaXpto' 'lala xpto'
   ret="$?"
   assert_equals_helper 'Invalid option' "$LINENO" "$ret" 22
 
-  parse_configm_options '--wrongOption' 'lala xpto'
+  parse_kernel_config_manager_options '--wrongOption' 'lala xpto'
   ret="$?"
   assert_equals_helper 'Invalid option' "$LINENO" "$ret" 22
 
   # valid options
-  parse_configm_options '--force'
+  parse_kernel_config_manager_options '--force'
   expected=1
   assert_equals_helper 'Set force flag' "$LINENO" "${options_values['FORCE']}" "$expected"
 
-  parse_configm_options '-s' "$NAME_1" '-d' "$DESCRIPTION_1"
+  parse_kernel_config_manager_options '-s' "$NAME_1" '-d' "$DESCRIPTION_1"
   assert_equals_helper 'Set save options' "$LINENO" "${options_values['SAVE']}" "$NAME_1"
   assert_equals_helper 'Set description options' "$LINENO" "${options_values['DESCRIPTION']}" "$DESCRIPTION_1"
 
-  parse_configm_options '--get' "$NAME_1"
+  parse_kernel_config_manager_options '--get' "$NAME_1"
   assert_equals_helper 'Set get flag' "$LINENO" "${options_values['GET']}" "$NAME_1"
 
-  parse_configm_options '--remove' "$NAME_1"
+  parse_kernel_config_manager_options '--remove' "$NAME_1"
   assert_equals_helper 'Set remove flag' "$LINENO" "${options_values['REMOVE']}" "$NAME_1"
 
-  parse_configm_options '--list'
+  parse_kernel_config_manager_options '--list'
   expected=1
   assert_equals_helper 'Set list flag' "$LINENO" "${options_values['LIST']}" "$expected"
 
-  parse_configm_options '--fetch'
+  parse_kernel_config_manager_options '--fetch'
   expected=1
   assert_equals_helper 'Set fetch flag' "$LINENO" "${options_values['FETCH']}" "$expected"
 
-  parse_configm_options '--output' "$NAME_1"
+  parse_kernel_config_manager_options '--output' "$NAME_1"
   assert_equals_helper 'Set output flag' "$LINENO" "${options_values['OUTPUT']}" "$NAME_1"
 
-  parse_configm_options '--remote' "$NAME_1"
+  parse_kernel_config_manager_options '--remote' "$NAME_1"
   assert_equals_helper 'Set remote flag' "$LINENO" "${options_values['TARGET']}" 3
 
-  parse_configm_options '--optimize'
+  parse_kernel_config_manager_options '--optimize'
   expected=1
   assert_equals_helper 'Set optimize flag' "$LINENO" "${options_values['OPTIMIZE']}" "$expected"
 }

commit f44b489679e5e5fa7f593c1b55d43837f9e73c64

src: help: Fix renaming of configm to kernel-config-manager
The commit 305d9e1 changed the command from configm to
kernel-config-manager, but didn't change the short command from g to k.
This commit fixes the new short command for kernel-config-manager
command and also reorganize the commands in alphabetical order.

Fixes: 305d9e1 ('src: configm: Rename configm to kernel-config-manager')
Signed-off-by: Maíra Canal <mairacanal@riseup.net>

src/help.sh
@@ -16,7 +16,6 @@ function kworkflow_help()
     '  build,b - Build kernel' \
     '  clear-cache - Clear files generated by kw' \
     '  codestyle,c - Apply checkpatch on directory or file' \
-    '  kernel-config-manager,g - Manage config files' \
     '  debug - Linux kernel debug utilities' \
     '  deploy,d - Deploy a new kernel image to a target machine' \
     '  device - Show basic hardware information' \
@@ -25,6 +24,7 @@ function kworkflow_help()
     '  explore,e - Explore string patterns' \
     '  help,h - displays this help mesage' \
     '  init - Initialize kworkflow config file' \
+    '  kernel-config-manager,k - Manage config files' \
     '  maintainers,m - Get maintainers and mailing list' \
     '  man - Show manual pages' \
     '  pomodoro,p - kw pomodoro support' \