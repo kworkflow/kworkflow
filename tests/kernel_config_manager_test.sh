c89abd5ad9fa9bd2504c83e7ceb5dd59f61adf98
David Tadokoro <davidbtadokoro@usp.br>
2022-09-26 02:30:45 -0300

    tests: kernel_config_manager: corrects code style
    The previous commit added code that did not follow the project's coding
    style (my fault as I did not configured the pre-commit in my machine)
    and this commit corrects this.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- tests/kernel_config_manager_test.sh
+++ tests/kernel_config_manager_test.sh
@@ -81,8 +81,8 @@ function test_save_config_file_check_save_failures()
 {
   local current_path="$PWD"
   local ret=0
-  local git_name=$(git config user.name)
-  local git_email=$(git config user.email)
+  local git_name=""
+  local git_email=""
 
   # Test without config file -> should fail
   cd "$SHUNIT_TMPDIR" || {
@@ -101,9 +101,11 @@ function test_save_config_file_check_save_failures()
 
   # Test git commit fail scenario
   touch .config
+  git_name=$(git config user.name)
+  git_email=$(git config user.email)
   git config --global user.name ""
   git config --global user.email ""
-  ret=$(save_config_file "$NO_FORCE $NAME_1" "$DESCRITION_1")
+  ret=$(save_config_file "$NO_FORCE $NAME_1" "$DESCRIPTION_1")
   assert_equals_helper 'git commit should fail without username and/or email set' "$LINENO" "$ret" 'Could not save user config files'
   assertTrue "$LINENO: Failing git commit should not persist .config file" '[[ ! -f $configs_path/configs/$NAME_1 ]]'
   assertTrue "$LINENO: Failing git commit should not persist .config metadata" '[[ ! -f $configs_path/metadata/$NAME_1 ]]'
