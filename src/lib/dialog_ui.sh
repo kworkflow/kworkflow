74c0b5c71a97aee5345cf76bf10953499105017f
Rodrigo Siqueira <siqueirajordao@riseup.net>
2022-11-19 08:58:52 -0500

    src: lib: upstream_patches_ui: Add new screens
    For the kw upstream UI, we created a tiny mockup that shows our basic
    goal with this interface which can be checked at:
    
     https://gitlab.freedesktop.org/siqueira/lore-prototype/-/issues/4
    
    This commit leverages the already available dialog functions to
    implement the following screens: bookmark, list patches, and register
    mailing list. In the process of adding those new screens, it was
    required to expand the dialog library to support more options.
    
    Signed-off-by: Rodrigo Siqueira <siqueirajordao@riseup.net>

--- src/lib/dialog_ui.sh
+++ src/lib/dialog_ui.sh
@@ -33,10 +33,11 @@ function create_menu_options()
   local cancel_label="$5"
   local height="$6"
   local width="$7"
-  local flag="$8"
   local max_elements_displayed_in_the_menu="$8"
+  local no_index="$9"
   local index=1
   local cmd
+  local ret
 
   if [[ "${#_menu_list_string_array[@]}" -eq 0 ]]; then
     return 22 # EINVAL
@@ -63,10 +64,95 @@ function create_menu_options()
   cmd+=" '${height}' '${width}' '${max_elements_displayed_in_the_menu}'"
 
   for item in "${_menu_list_string_array[@]}"; do
+    if [[ -n "$no_index" ]]; then
+      cmd+=" $\"${item}\" ''"
+      continue
+    fi
+
     cmd+=" '${index}' $\"${item}\""
     ((index++))
   done
 
+  exec 3>&1
   menu_return_string=$(cmd_manager "$flag" "$cmd" 2>&1 1>&3)
-  return "$?"
+  ret="$?"
+  exec 3>&-
+  return "$ret"
+}
+
+# Create simple checklist without index
+#
+# @menu_title: This is the menu title used on the top left of the dialog screen.
+# @back_tittle: This is the menu title that will be directly visible to the user.
+# @menu_message_box: The instruction text used for this menu.
+# @_menu_list_string_array: An array reference containing all the strings to be used in the menu.
+# @cancel_label: Cancel label. If not set, the default is 'Exit']
+# @height: Menu height in lines size
+# @width: Menu width in column size
+# @flag How to display a command, the default value is
+#   "SILENT". For more options see `src/kwlib.sh` function `cmd_manager`
+#
+# Return:
+# If everything works as expected, the user option is saved in the
+# menu_return_string variable, and the return code is 0. Otherwise, an errno
+# code is returned.
+function create_simple_checklist()
+{
+  local menu_title="$1"
+  local back_title="$2"
+  local menu_message_box="$3"
+  local -n _menu_list_string_array="$4"
+  local cancel_label="$5"
+  local height="$6"
+  local width="$7"
+  local list_height="$8"
+  local flag="$9"
+  local cmd
+  local ret
+
+  flag=${flag:-'SILENT'}
+  back_title=${back_title:-'kw'}
+  height=${height:-'0'}
+  width=${width:-'0'}
+  list_height=${list_height:-'0'}
+  cancel_label=${cancel_label:-'Exit'}
+
+  cmd="dialog --backtitle '${back_title}' --title '${menu_title}' --clear"
+
+  # Change cancel label
+  if [[ -n "$cancel_label" ]]; then
+    cmd+=" --cancel-label '${cancel_label}'"
+  fi
+
+  # Start to compose menu
+  cmd+=" --checklist $\"${menu_message_box}\""
+
+  # Set height, width, and max display itens
+  cmd+=" '${height}' '${width}' '${list_height}'"
+
+  for item in "${_menu_list_string_array[@]}"; do
+    cmd+=" '${item}' '' 'off' "
+  done
+
+  exec 3>&1
+  menu_return_string=$(cmd_manager "$flag" "$cmd" 2>&1 1>&3)
+  ret="$?"
+  exec 3>&-
+  return "$ret"
+}
+
+# This function is responsible for handling the dialog exit.
+#
+# @exit_status: Exit code
+function handle_exit()
+{
+  local exit_status="$1"
+
+  # Handling stop
+  case "$exit_status" in
+    1 | 255) # Exit
+      clear
+      exit 0
+      ;;
+  esac
 }
--- src/upstream_patches_ui.sh
+++ src/upstream_patches_ui.sh
@@ -3,56 +3,176 @@
 # list. We try not to add UI-specific technology here since we want to be able
 # to plug in a new interface in the future. Also, this file connects the lore
 # API with the UI.
+
+include "${KW_LIB_DIR}/kw_config_loader.sh"
 include "${KW_LIB_DIR}/lib/dialog_ui.sh"
+
+declare -gr KW_UPSTREAM_TITLE='kw upstream patches manager'
+declare -ga registered_lists
+
+# TODO: Bookmarked patches must come from local database
+declare -ga bookmarked_patches=(
+  '2022-11-10 | #1   | drm/amd/pm: Enable bad memory page/channel recording support for smu v13_0_0'
+  '2021-05-10 | #255 | DC Patches November 19, 2022'
+  '2000-01-29 | #12  | drm/amdgpu: add drv_vram_usage_va for virt data exchange'
+  '2022-07-01 | #7   | drm/amdgpu: fix pci device refcount leak'
+)
+
+# To make it easier to change between different screens, we implement a
+# mechanism that uses this associative array to indicate the current screen to
+# be shown.
+declare -gA screen_sequence=(
+  ['SHOW_SCREEN']='register'
+  ['SHOW_SCREEN_PARAMETER']=''
+)
 
 # This function is responsible for managing the UI interface loop; here, it is
 # possible to use the screen sequence used by this feature.
 upstream_patches_ui_main()
 {
+  local all_registered_list="${lore_config['lists']}"
+  local ret
+
+  IFS=',' read -r -a registered_lists <<< "$all_registered_list"
+  if [[ "${#registered_lists[@]}" != 0 ]]; then
+    screen_sequence['SHOW_SCREEN']='dashboard'
+  fi
+
   # Main loop
   while true; do
-    exec 3>&1
-    bookmark_menu
-    exit_status="$?"
-    exec 3>&-
-
-    # Handling stop
-    case "$exit_status" in
-      1 | 255) # Exit
-        clear
-        exit 0
+    case "${screen_sequence['SHOW_SCREEN']}" in
+      'register')
+        # First time here? Let's register some public mailing list
+        register_mailing_list
+        ret="$?"
+        ;;
+      'dashboard')
+        # Main menu
+        dashboard_entry_menu
+        ret="$?"
+        ;;
+      'registered_mailing_list')
+        registered_mailing_list
+        ret="$?"
+        ;;
+      'bookmarked_patches')
+        show_bookmarked_patches
+        ret="$?"
+        ;;
+      'show_patch_details')
+        show_patche_details
+        ret="$?"
         ;;
     esac
 
-    case "$menu_return_string" in
-      1)
-        printf 'This is a simple test\n'
-        exit
-        ;;
-      2)
-        printf 'Yet, another simple test\n'
-        exit
-        ;;
-    esac
-
+    handle_exit "$ret"
   done
 }
 
 # Build the bookmark screen. Developers may want to save some patch for
 # verification later, and the bookmark page address this requirement by
 # providing a list of patches saved by the developers.
-function bookmark_menu()
+function dashboard_entry_menu()
 {
   local message_box
   local -a menu_list_string_array
 
   # TODO: Get list from liblore
-  menu_list_string_array=('amdgfx' 'drm' 'Linux iio' 'kernel jenitors' 'git list')
+  menu_list_string_array=('Registered mailing list' 'Bookmarked patches')
 
   message_box="It looks like that you don't have any lore list registered; please"
   message_box+=' select one or more of the below list:'
 
-  create_menu_options 'Simple test' 'Mailing list' \
+  create_menu_options 'Dashboard' "$KW_UPSTREAM_TITLE" \
     "$message_box" 'menu_list_string_array' \
     'Exit' '' '' ''
+
+  case "$menu_return_string" in
+    1) # Registered mailing list
+      screen_sequence['SHOW_SCREEN']='registered_mailing_list'
+      ;;
+    2) # Bookmarked patches
+      screen_sequence['SHOW_SCREEN']='bookmarked_patches'
+      ;;
+  esac
 }
+
+# The bookmarked screen it is just a list of patches previously saved by the
+# developer. For this reason, we just need to display the save patches.
+function show_bookmarked_patches()
+{
+  # TODO: Get bookmarked_patches from the local database
+  list_patches 'Bookmarked patches' bookmarked_patches
+}
+
+# Show all mailing list that the developer is registered
+function registered_mailing_list()
+{
+  local message_box
+
+  # TODO: Get list from liblore
+  message_box="Below you can see all the mailing lists that you are registered:"
+
+  create_menu_options 'Mailing lists' "$KW_UPSTREAM_TITLE" \
+    "$message_box" 'registered_lists' \
+    'Exit' '' '' '' 'no_index'
+}
+
+# This is a generic function used to show a list of patches. If the user select
+# one specific patch, this function will set the next screen to be
+# show_patch_details with the patch index saved in the SHOW_SCREEN_PARAMETER
+# option.
+#
+# @message_box: Text description to the list of patches
+# @_target_array_list: List of patches to be displayed
+function list_patches()
+{
+  local message_box="$1"
+  local -n _target_array_list="$2"
+  local selected_patch
+
+  # TODO: Get list from local store
+  # Format patche like this:
+  # printf 'V%-2s |#%-3s| %-100s' "$patch_version" "$total_patches" "$patch_title"
+
+  message_box='List of bookmarked patches'
+
+  create_menu_options 'Bookmarked patches' "$KW_UPSTREAM_TITLE" \
+    "$message_box" '_target_array_list' \
+    'Exit' '' '' ''
+
+  selected_patch="$menu_return_string"
+
+  if [[ -n "$selected_patch" ]]; then
+    screen_sequence['SHOW_SCREEN']='show_patch_details'
+    screen_sequence['SHOW_SCREEN_PARAMETER']="$selected_patch"
+  fi
+}
+
+# Screen used to register to a new mailing list
+function register_mailing_list()
+{
+  local message_box
+  local new_list
+  local -a menu_list_string_array
+
+  # TODO: Get list from liblore
+  menu_list_string_array=('lkml' 'amd-gfx' 'netdev' 'u-boot')
+
+  message_box="It looks like that you don't have any lore list registered; please"
+  message_box+=" select one or more of the below list:"
+
+  create_simple_checklist 'Lore list' "$KW_UPSTREAM_TITLE" \
+    "$message_box" 'menu_list_string_array' \
+    'Exit' '' '' ''
+
+  new_list=$(printf '%s' "$menu_return_string" | tr -s '[:blank:]' ',')
+
+  if [[ -n "$new_list" ]]; then
+    screen_sequence['SHOW_SCREEN']='dashboard'
+    lore_config['lists']="${new_list}"
+    # TODO: Update config file
+  fi
+}
+
+load_lore_config
