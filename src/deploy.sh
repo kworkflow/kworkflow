9150845a589d887612a6ddd03ae3f61dbb260c61
David Tadokoro <davidbtadokoro@usp.br>
2023-02-03 12:47:43 -0300

    src: deploy: Fix individual redirects to same file
    Previously, there were individual printf redirects to the same file,
    which conflicted with rule SC2129 of ShellCheck that says to bundle
    those redirects in only one (https://www.shellcheck.net/wiki/SC2129).
    The reasoning is that the file is opened and closed only once and thus
    there is a performance gain, although, in this case, the gain is
    irrelevant being more an issue of keeping the project coding style.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/deploy.sh
+++ src/deploy.sh
@@ -1086,10 +1086,12 @@ function create_pkg_metadata_file_for_deploy()
 
   cache_pkg_metadata_file_path="${base_kw_deploy_store_path}/kw.pkg.info"
 
-  printf 'kernel_name=%s\n' "$kernel_name" > "${cache_pkg_metadata_file_path}"
-  printf 'kernel_binary_image_file=%s\n' "$kernel_binary_file_name" >> "${cache_pkg_metadata_file_path}"
-  printf 'architecture=%s\n' "$arch" >> "${cache_pkg_metadata_file_path}"
-  printf 'previous_kernel_backup=%s\n' "${deploy_config[previous_kernel_backup]}" >> "${cache_pkg_metadata_file_path}"
+  {
+    printf 'kernel_name=%s\n' "$kernel_name"
+    printf 'kernel_binary_image_file=%s\n' "$kernel_binary_file_name"
+    printf 'architecture=%s\n' "$arch"
+    printf 'previous_kernel_backup=%s\n' "${deploy_config[previous_kernel_backup]}"
+  } > "${cache_pkg_metadata_file_path}"
 }
 
 # This function is responsible for putting all the required boot files in a
