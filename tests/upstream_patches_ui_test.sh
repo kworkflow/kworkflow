c0721acb9ea32e551ca60a22ce1d355487abef91
David Tadokoro <davidbtadokoro@usp.br>
2023-03-14 00:09:11 -0300

    src: upstream_patches_ui: Add bookmark feature
    This commit adds a barebones implementation of the bookmark feature.
    This feature refers to the ability to choose a certain patch series from
    a public mailing list and save it to later consultation. After
    bookmarking a patch series, it will become available in the 'Bookmarked
    Patches' screen. At the moment, the only action available is to
    'Unbookmark' a patch series, but it should be kind of simple to plug in
    new actions.
    
    From the implementation point of view, kw needs to maintain a local
    database of all bookmarked patch series and this is done by creating and
    managing a file inside the kw data directory (much like kw
    kernel-config-manager). Although efficient and simple to manage, if the
    feature needs scaling (change/add entities/fields), a real DBMS should
    be considered. Also worth mentioning that, under the hood, bookmarking a
    patch series downloads it first in the same manner of the 'Download'
    action, i.e., saves the .mbox files inside a directory defined by the
    user in lore.config. Unbookmarking patches series also removes them
    first from local storage.
    
    Closes: #796
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- tests/upstream_patches_ui_test.sh
+++ tests/upstream_patches_ui_test.sh
@@ -6,6 +6,21 @@ include './tests/utils.sh'
 function setUp()
 {
   screen_sequence['SHOW_SCREEN']=''
+
+  export ORIGINAL_PATH="$PWD"
+
+  cd "${SHUNIT_TMPDIR}" || {
+    fail "($LINENO): setUp(): It was not possible to move into ${SHUNIT_TMPDIR}"
+    return
+  }
+}
+
+function tearDown()
+{
+  cd "${ORIGINAL_PATH}" || {
+    fail "($LINENO): tearDown(): It was not possible to move into ${ORIGINAL_PATH}"
+    return
+  }
 }
 
 function test_dashboard_entry_menu_check_valid_options()
@@ -46,13 +61,6 @@ function test_dashboard_entry_menu_check_failed()
   assert_equals_helper 'Expected failure' "$LINENO" "$?" 22
 }
 
-declare -ga bookmarked_patches=(
-  '2022-11-10 | #1   | drm/amd/pm: Enable bad memory page/channel recording support for smu v13_0_0'
-  '2021-05-10 | #255 | DC Patches November 19, 2022'
-  '2000-01-29 | #12  | drm/amdgpu: add drv_vram_usage_va for virt data exchange'
-  '2022-07-01 | #7   | drm/amdgpu: fix pci device refcount leak'
-)
-
 declare -ga patch_list_with_metadata=(
   'Joe DoeÆjoedoe@lala.comÆV1Æ1Ædrm/amd/pm: Enable bad memory page/channel recording support for smu v13_0_0Æhttp://something.la'
   'Juca PiramaÆjucapirama@xpto.comÆV1Æ255ÆDC Patches November 19, 2022Æhttp://anotherthing.la'
@@ -85,6 +93,39 @@ function test_show_series_details()
   assert_equals_helper 'Expected failure' "$LINENO" $"$output" $"$expected_result"
 }
 
+function test_show_bookmarked_series_details()
+{
+  local output
+  local expected_result='Bookmarked Series info and actions'
+
+  expected_result+=' \Zb\Z6Series:\ZnDC Patches November 19, 2022\n'
+  expected_result+='\Zb\Z6Author:\ZnJuca Pirama\n\Zb\Z6Version:\ZnV1\n'
+  expected_result+='\Zb\Z6Patches:\Zn255\n'
+  expected_result+=' action_list'
+
+  # shellcheck disable=SC2317
+  function get_bookmarked_series_by_index()
+  {
+    if [[ "$1" == 2 ]]; then
+      printf 'Juca PiramaÆjucapirama@xpto.comÆV1Æ255ÆDC Patches November 19, 2022Æhttp://anotherthing.la'
+    fi
+  }
+
+  # Mock failed scenario
+  # shellcheck disable=SC2317
+  function create_simple_checklist()
+  {
+    local title="$1"
+    local message_box="$2"
+    local action="$3"
+
+    printf '%s %s %s' "$title" "$message_box" "$action"
+  }
+
+  output=$(show_bookmarked_series_details 1)
+  assert_equals_helper 'Expected failure' "$LINENO" $"$output" $"$expected_result"
+}
+
 function test_list_patches()
 {
   # shellcheck disable=SC2317
@@ -93,8 +134,12 @@ function test_list_patches()
     menu_return_string='3'
   }
 
-  list_patches 'Message test' 'bookmarked_patches'
+  list_patches 'Message test' 'patches_from_mailing_list' 'show_new_patches_in_the_mailing_list'
   assert_equals_helper 'Expected screen' "$LINENO" "${screen_sequence['SHOW_SCREEN']}" 'show_series_details'
+  assert_equals_helper 'Expected screen' "$LINENO" "${screen_sequence['SHOW_SCREEN_PARAMETER']}" 2
+
+  list_patches 'Message test' 'bookmarked_patches' 'bookmarked_patches'
+  assert_equals_helper 'Expected screen' "$LINENO" "${screen_sequence['SHOW_SCREEN']}" 'show_bookmarked_series_details'
   assert_equals_helper 'Expected screen' "$LINENO" "${screen_sequence['SHOW_SCREEN_PARAMETER']}" 2
 }
 
