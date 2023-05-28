fd7ebeb7494f78be63b2eed6028f83d37de3ce3c
Aquila Macedo <aquilamacedo@riseup.net>
2023-05-28 17:10:58 -0300

    src: kw_ssh: Adding verbose mode
    This commit adds support for the verbose parameter within `kw ssh`.
    The verbose parameter gives details of the commands that are executed
    behind the scenes.
    
    Closes: #851
    
    Reviewed-by: Rodrigo Siqueira <siqueirajordao@riseup.net>
    Signed-off-by: Aquila Macedo <aquilamacedo@riseup.net>

--- tests/kw_ssh_test.sh
+++ tests/kw_ssh_test.sh
@@ -81,7 +81,7 @@ function test_parser_ssh_options()
   assertEquals "($LINENO)" 'ls -lah' "${options_values['CMD']}"
 
   parser_ssh_options --verbose
-  assertEquals "($LINENO)" '' "${options_values['VERBOSE']}"
+  assertEquals "($LINENO)" '1' "${options_values['VERBOSE']}"
 
   parser_ssh_options --remote 'jozzi@something:3232'
   assertEquals "($LINENO)" 'something' "${remote_parameters['REMOTE_IP']}"
@@ -97,7 +97,7 @@ function test_kw_ssh_main_no_parameter()
   )
 
   # Remote
-  output=$(kw_ssh_main test_mode)
+  output=$(kw_ssh_main 'TEST_MODE')
   compare_command_sequence '' "$LINENO" 'expected_cmd' "$output"
 }
 
@@ -110,10 +110,10 @@ function test_kw_ssh_main_command()
     "ssh -F ${SHUNIT_TMPDIR}/.kw/remote.config origin pwd"
   )
 
-  output=$(kw_ssh_main test_mode -c 'pwd')
+  output=$(kw_ssh_main 'TEST_MODE' -c 'pwd')
   compare_command_sequence '' "$LINENO" 'expected_cmd' "$output"
 
-  output=$(kw_ssh_main test_mode --command "ls /etc/" 2>&1)
+  output=$(kw_ssh_main 'TEST_MODE' --command "ls /etc/" 2>&1)
   expected_cmd[1]="ssh -F ${SHUNIT_TMPDIR}/.kw/remote.config origin ls /etc/"
   compare_command_sequence '' "$LINENO" 'expected_cmd' "$output"
 }
@@ -123,7 +123,7 @@ function test_kw_ssh_main_script()
   local output
   local msg
 
-  ret=$(kw_ssh_main test_mode -s "/not/a/valid/path/xpto" 2>&1)
+  ret=$(kw_ssh_main 'TEST_MODE' -s "/not/a/valid/path/xpto" 2>&1)
   msg="$NO_SUCH_FILE: /not/a/valid/path/xpto"
   assertTrue "($LINENO): We expected a substring '$msg', but we got '$ret'" \
     '[[ $ret =~ "$msg" ]]'
@@ -133,7 +133,7 @@ function test_kw_ssh_main_script()
     "ssh -F ${SHUNIT_TMPDIR}/.kw/remote.config origin \"bash -s\" -- < $TEST_PATH/dmesg"
   )
 
-  output=$(kw_ssh_main test_mode -s "${TEST_PATH}/dmesg" 2>&1)
+  output=$(kw_ssh_main 'TEST_MODE' -s "${TEST_PATH}/dmesg" 2>&1)
   compare_command_sequence '' "$LINENO" 'expected_cmd' "$output"
 }
 
@@ -165,7 +165,7 @@ function test_handle_ssh_with_config_file_verbose()
   options_values['VERBOSE']=''
 
   output=$(handle_ssh)
-  expected_result="ssh -v -F ${PWD}/ssh_config target"
+  expected_result="ssh -F ${PWD}/ssh_config target"
   assertEquals "($LINENO)" "$expected_result" "$output"
 }
 
@@ -181,15 +181,13 @@ function test_handle_ssh_with_port_user_remote()
   remote_parameters['REMOTE_USER']='tadeu'
   remote_parameters['REMOTE_IP']='192.168.3.1'
   remote_parameters['REMOTE_PORT']='3244'
-  options_values['VERBOSE']='TEST_MODE'
 
   output=$(handle_ssh)
   expected_result="ssh -p 3244 tadeu@192.168.3.1"
   assertEquals "($LINENO)" "$expected_result" "$output"
 
-  options_values['VERBOSE']=''
   output=$(handle_ssh)
-  expected_result="ssh -v -p 3244 tadeu@192.168.3.1"
+  expected_result="ssh -p 3244 tadeu@192.168.3.1"
   assertEquals "($LINENO)" "$expected_result" "$output"
 }
 
@@ -202,9 +200,8 @@ function test_run_command_in_the_remote()
   remote_parameters['REMOTE_FILE']="${PWD}/ssh_config"
   remote_parameters['REMOTE_FILE_HOST']='target'
   options_values['CMD']='do something'
-  options_values['VERBOSE']='TEST_MODE'
 
-  output=$(run_command_in_the_remote)
+  output=$(run_command_in_the_remote 'TEST_MODE')
   expected_result="ssh -F ${PWD}/ssh_config target do something"
   assertEquals "($LINENO)" "$expected_result" "$output"
 }
@@ -220,9 +217,8 @@ function test_run_script_in_the_remote()
   remote_parameters['REMOTE_FILE']="${PWD}/ssh_config"
   remote_parameters['REMOTE_FILE_HOST']='target'
   options_values['SCRIPT']="${PWD}/some_script"
-  options_values['VERBOSE']='TEST_MODE'
 
-  output=$(run_script_in_the_remote)
+  output=$(run_script_in_the_remote 'TEST_MODE')
   expected_result="ssh -F ${PWD}/ssh_config target \"bash -s\" -- < ${options_values['SCRIPT']}"
   assertEquals "($LINENO)" "$expected_result" "$output"
 }
@@ -236,9 +232,8 @@ function test_ssh_remote()
 
   remote_parameters['REMOTE_FILE']="${PWD}/ssh_config"
   remote_parameters['REMOTE_FILE_HOST']='target'
-  options_values['VERBOSE']='TEST_MODE'
 
-  output=$(ssh_remote)
+  output=$(ssh_remote 'TEST_MODE')
   expected_result="ssh -F ${PWD}/ssh_config target"
   assertEquals "($LINENO)" "$expected_result" "$output"
 }
