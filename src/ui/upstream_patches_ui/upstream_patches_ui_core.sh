c2991866866963425016450371ba9cac6e4b4bc0
David Tadokoro <davidbtadokoro@usp.br>
2023-06-22 17:41:16 -0300

    src: ui: upstream_patches_ui: settings: Extract 'Settings' to a file
    In `upstream_patches_ui_core.sh` the 'Settings' screen-sequence is
    represented by the `show_settings_screen` function. It is an example of
    complicated, hard to read and that does many things function.
    
    This commit extracts the screen-sequence to a file
    `src/ui/upstream_patches_ui/settings.sh`. The `show_settings_screen`
    function now is broken down into modular functions. This way there is
    increased readibility and allows us to test it better.
    
    [Note]
    Lacks documentation and tests for the extracted functions. Ammend this
    commit with these changes.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/ui/upstream_patches_ui/upstream_patches_ui_core.sh
+++ src/ui/upstream_patches_ui/upstream_patches_ui_core.sh
@@ -14,6 +14,7 @@ include "${KW_LIB_DIR}/lib/dialog_ui.sh"
 include "${KW_LIB_DIR}/lib/lore.sh"
 include "${KW_LIB_DIR}/kwio.sh"
 include "${KW_LIB_DIR}/kwlib.sh"
+include "${KW_LIB_DIR}/ui/upstream_patches_ui/settings.sh"
 
 # These are references to data structures used all around the state-machine.
 declare -ga registered_lists
@@ -202,132 +203,6 @@ function show_bookmarked_series_details()
     3) # Return
       screen_sequence['SHOW_SCREEN']='bookmarked_patches'
       screen_sequence['RETURNING']=1
-      ;;
-  esac
-}
-
-# Screen that shows all types of settings available.
-function show_settings_screen()
-{
-  local lore_config_path="${PWD}/.kw/lore.config"
-  local -a menu_list_string_array
-  local new_value
-  local output
-  declare -A branches
-  local index
-  local -a check_statuses
-  local ret
-
-  if [[ ! -f "${lore_config_path}" ]]; then
-    lore_config_path="${KW_ETC_DIR}/lore.config"
-  fi
-
-  menu_list_string_array=(
-    'Register/Unregister Mailing Lists'
-    'Save Patches To'
-    'Kernel Tree Path'
-    'Kernel Tree Target Branch'
-  )
-  create_menu_options 'Settings' '' 'menu_list_string_array' 1
-  ret="$?"
-
-  case "$ret" in
-    0) # OK
-      case "$menu_return_string" in
-        1) # Register/Unregister Mailing Lists
-          screen_sequence['SHOW_SCREEN']='manage_mailing_lists'
-          ;;
-        2) # Save Patches To
-          create_directory_selection_screen "${lore_config['save_patches_to']}" 'Select directory where patches will be downloaded'
-          case "$?" in
-            0) # OK
-              new_value=$(printf '%s' "$menu_return_string" | sed 's/\/$//')
-              if [[ ! -d "$new_value" ]]; then
-                create_message_box 'Error' "${new_value}: No such directory."
-              else
-                output=$(save_new_lore_config 'save_patches_to' "$new_value" "$lore_config_path")
-                if [[ "$?" != 0 ]]; then
-                  create_message_box 'Error' "Failed to save new value ${new_value} in ${lore_config_path}:"$'\n'"$output"
-                fi
-                # As we altered the settings, we need to reload lore.config
-                load_lore_config
-              fi
-              ;;
-            1) # Cancel
-              ;;
-            2) # Help
-              create_help_screen 'directory_selection'
-              if [[ "$?" != 0 ]]; then
-                create_message_box 'Error' 'Cannot create help screen'
-              fi
-              ;;
-          esac
-
-          # Just to be safe
-          screen_sequence['SHOW_SCREEN']='settings'
-          ;;
-        3) # Kernel Tree Path
-          create_directory_selection_screen "${lore_config['kernel_tree_path']}" 'Select Linux kernel source tree'
-          case "$?" in
-            0) # OK
-              new_value=$(printf '%s' "$menu_return_string" | sed 's/\/$//')
-              if ! is_kernel_root "$new_value"; then
-                create_message_box 'Error' "${new_value}: Not a Linux kernel source tree."
-              else
-                new_value=$(realpath "$new_value")
-                save_new_lore_config 'kernel_tree_path' "$new_value" "$lore_config_path"
-                # As we changed the kernel tree, we set the target branch to empty
-                save_new_lore_config 'kernel_tree_branch' '' "$lore_config_path"
-                # As we altered the settings, we need to reload lore.config
-                load_lore_config
-              fi
-              ;;
-            1) # Cancel
-              ;;
-            2) # Help
-              create_help_screen 'directory_selection'
-              if [[ "$?" != 0 ]]; then
-                create_message_box 'Error' 'Cannot create help screen'
-              fi
-              ;;
-          esac
-          # Just to be safe
-          screen_sequence['SHOW_SCREEN']='settings'
-          ;;
-        4) # Kernel Tree Target Branch
-          if [[ -z "${lore_config['kernel_tree_path']}" ]]; then
-            create_message_box 'Error' 'You need to set "Kernel Tree Path" first.'
-          else
-            get_git_repository_branches "${lore_config['kernel_tree_path']}" 'branches'
-            index=0
-            for branch in "${!branches[@]}"; do
-              [[ "${lore_config['kernel_tree_branch']}" == "$branch" ]] && check_statuses["$index"]=1
-              ((index++))
-            done
-            message_box='Select the target branch of the Linux kernel tree.'$'\n'
-            message_box+='When applying patches, this branch will be used as base.'
-            create_choice_list_screen 'Kernel Tree Target Branch' "$message_box" 'branches' 'check_statuses'
-            case "$?" in
-              0) # OK
-                new_value=$(printf '%s' "$menu_return_string" | sed 's/\/$//')
-                save_new_lore_config 'kernel_tree_branch' "$new_value" "$lore_config_path"
-                # As we altered the settings, we need to reload lore.config
-                load_lore_config
-                ;;
-              1) # Cancel
-                ;;
-            esac
-          fi
-          # Just to be safe
-          screen_sequence['SHOW_SCREEN']='settings'
-          ;;
-      esac
-      ;;
-    1) # Exit
-      handle_exit "$ret"
-      ;;
-    3) # Return
-      screen_sequence['SHOW_SCREEN']='dashboard'
       ;;
   esac
 }
