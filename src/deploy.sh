8f14471e2283184cc367278512949df9641afca1
Aquila Macedo <aquilamacedo@riseup.net>
2023-07-01 14:30:35 -0300

    src: lib: signal_manager: align folder organization with structure diagram.
    This commit moves the signal_manager.sh file to the lib folder for better organization
    and maintenance, aligning with the recommended structure and facilitating
    effective code management.
    
    Part of issue: #833
    
    Signed-off-by: Aquila Macedo <aquilamacedo@riseup.net>

--- src/deploy.sh
+++ src/deploy.sh
@@ -20,7 +20,7 @@ declare ENV_DIR='envs'
 
 include "${KW_LIB_DIR}/lib/kw_config_loader.sh"
 include "${KW_LIB_DIR}/lib/remote.sh"
-include "$KW_LIB_DIR/signal_manager.sh"
+include "${KW_LIB_DIR}/lib/signal_manager.sh"
 
 # To make the deploy to a remote machine straightforward, we create a directory
 # on the host that will be used for centralizing files required for the new
