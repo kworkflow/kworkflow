2b92cc8792aaaeb20f0d4668cbbf264acb91fb45
David Tadokoro <davidbtadokoro@usp.br>
2023-09-28 15:57:07 -0300

    tracing: tracing: Add functions to inject code to library files
    As mentioned in the previous commit, to trace the flow of execution of
    kw we need to inject code into the main kw executable (that was done in
    the previous commit) and into library files (almost all files in `src`).
    
    With that in mind, add three functions `sync_kw_lib_files_with_tracing`,
    `inject_lib_file_with_tracing` and `get_tracing_log_line` to
    `tracing/tracing.sh` that, combined, take care of injecting code that
    enables tracing into all necessary files and synchronizes these with kw
    local library (usually `~/.local/lib/kw`).
    
    The code injected is the one that actually logs every entry and exit of
    a function defined in the kw project. In other words, for every file
    injected with code, we have:
    
    - The first command of every function is to log the entrance in it;
    - Before every `return` and `exit` statement in a function, there is a
      command to log the exiting of it;
    - Before the end of a function definition (closing curly braces), there
      is a command to log the exiting of it preserving the return value of
      the last command;
    - For every command that launches a background execution - which spuns
      another thread of execution - there is setup and teardown code (before
      and after the command) to separate the log of different threads.
    
    Reviewed-by: Rodrigo Siqueira <siqueirajordao@riseup.net>
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- tests/tracing/tracing_test.sh
+++ tests/tracing/tracing_test.sh
@@ -72,4 +72,158 @@ EOF
   assert_equals_helper 'The file was not correctly generated' "$LINENO" "$expected" "$output"
 }
 
+function test_sync_kw_lib_files_with_tracing()
+{
+  local input_dir='src'
+  local output_dir="${SHUNIT_TMPDIR}/lib"
+  local output
+
+  cp --recursive "${SAMPLES_DIR}/tracing/src" './'
+  mkdir --parents "$output_dir"
+
+  sync_kw_lib_files_with_tracing "$input_dir" "$output_dir"
+
+  output=$(find "$output_dir" -type f)
+  assert_line_match "$LINENO" "${SHUNIT_TMPDIR}/lib/nothing_to_inject.sh" "$output"
+  assert_line_match "$LINENO" "${SHUNIT_TMPDIR}/lib/simple_return_and_exit.sh" "$output"
+  assert_line_match "$LINENO" "${SHUNIT_TMPDIR}/lib/alternate_return_and_exit.sh" "$output"
+  assert_line_match "$LINENO" "${SHUNIT_TMPDIR}/lib/background_execution.sh" "$output"
+  assert_line_match "$LINENO" "${SHUNIT_TMPDIR}/lib/lib/stub.sh" "$output"
+}
+
+function test_inject_lib_file_with_tracing_nothing_to_inject()
+{
+  local output
+  local expected
+
+  output=$(inject_lib_file_with_tracing "${SAMPLES_DIR}/tracing/src/nothing_to_inject.sh")
+  expected='# Comment'$'\n''command1'$'\n'$'\n''command2'
+  assert_equals_helper 'The file was not correctly injected' "$LINENO" "$expected" "$output"
+
+  output=$(inject_lib_file_with_tracing "${SAMPLES_DIR}/tracing/src/simple_return_and_exit.sh")
+  expected=$(
+    cat << 'EOF'
+function foo()
+{
+printf 'entry,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+printf 'return,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+return 0
+# shellcheck disable=SC2317
+printf 'exit,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+exit 0
+local _return_val="$?"
+printf 'return,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+return "$_return_val"
+}
+EOF
+  )
+  assert_equals_helper 'The file was not correctly injected' "$LINENO" "$expected" "$output"
+}
+
+function test_inject_lib_file_with_alternate_return_and_exit()
+{
+  local output
+  local expected
+
+  output=$(inject_lib_file_with_tracing "${SAMPLES_DIR}/tracing/src/alternate_return_and_exit.sh")
+  expected=$(
+    cat << 'EOF'
+function foo()
+{
+printf 'entry,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+command1 && command2 && printf 'return,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv" && return 0
+command3 && command4 && printf 'exit,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv" && exit 0
+local _return_val="$?"
+printf 'return,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+return "$_return_val"
+}
+EOF
+  )
+  assert_equals_helper 'The file was not correctly injected' "$LINENO" "$expected" "$output"
+}
+
+function test_inject_lib_file_with_background_execution()
+{
+  local output
+  local expected
+
+  output=$(inject_lib_file_with_tracing "${SAMPLES_DIR}/tracing/src/background_execution.sh")
+  expected=$(
+    cat << 'EOF'
+function foo()
+{
+printf 'entry,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+THREAD_NUMBER=$((++TOTAL_NUMBER_OF_THREADS))
+command &
+THREAD_NUMBER=0
+local _return_val="$?"
+printf 'return,foo,%s\n' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"
+return "$_return_val"
+}
+EOF
+  )
+  assert_equals_helper 'The file was not correctly injected' "$LINENO" "$expected" "$output"
+}
+
+function test_process_function_line()
+{
+  local line
+  local function_name='foo'
+  local output
+  local expected
+
+  line='generic_line "$that_should_not" "$be_altered"'
+  output=$(process_function_line "$line" "$function_name")
+  expected="$line"
+  assert_equals_helper 'Non-special line should remain unaltered' "$LINENO" "$expected" "$output"
+
+  line='}'
+  output=$(process_function_line "$line" "$function_name")
+  expected='local _return_val="$?"'$'\n'
+  expected+='printf '"'"'return,foo,%s\n'"'"' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"'$'\n'
+  expected+='return "$_return_val"'$'\n'
+  expected+='}'
+  assert_equals_helper 'End of function definition should log return and preserve last command return' "$LINENO" "$expected" "$output"
+
+  line='    return 235'
+  output=$(process_function_line "$line" "$function_name")
+  expected='printf '"'"'return,foo,%s\n'"'"' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"'$'\n'
+  expected+='    return 235'
+  assert_equals_helper 'Simple return should log return' "$LINENO" "$expected" "$output"
+
+  line='command && return 235'
+  output=$(process_function_line "$line" "$function_name")
+  expected='command && printf '"'"'return,foo,%s\n'"'"' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv" && return 235'
+  assert_equals_helper '&& return should log return in same line' "$LINENO" "$expected" "$output"
+
+  line='  exit 271'
+  output=$(process_function_line "$line" "$function_name")
+  expected='printf '"'"'exit,foo,%s\n'"'"' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv"'$'\n'
+  expected+='  exit 271'
+  assert_equals_helper 'Simple exit should log exit' "$LINENO" "$expected" "$output"
+
+  line='command && exit 271'
+  output=$(process_function_line "$line" "$function_name")
+  expected='command && printf '"'"'exit,foo,%s\n'"'"' "$(date +%s%N)" >> "${TMP_TRACING_DIR}/${THREAD_NUMBER}.csv" && exit 271'
+  assert_equals_helper '&& exit should log exit in same line' "$LINENO" "$expected" "$output"
+}
+
+function test_get_tracing_log_line()
+{
+  local output
+  local expected
+
+  output=$(get_tracing_log_line 'entry' 'foo')
+  expected="printf 'entry,foo,%s\n' \"\$(date +%s%N)\" >> \"\${TMP_TRACING_DIR}/\${THREAD_NUMBER}.csv\""
+  assert_equals_helper 'Wrong output' "$LINENO" "$expected" "$output"
+
+  output=$(get_tracing_log_line 'exit' 'foo')
+  expected="printf 'exit,foo,%s\n' \"\$(date +%s%N)\" >> \"\${TMP_TRACING_DIR}/\${THREAD_NUMBER}.csv\""
+  assert_equals_helper 'Wrong output' "$LINENO" "$expected" "$output"
+
+  output=$(get_tracing_log_line 'return' 'foo')
+  expected="printf 'return,foo,%s\n' \"\$(date +%s%N)\" >> \"\${TMP_TRACING_DIR}/\${THREAD_NUMBER}.csv\""
+  assert_equals_helper 'Wrong output' "$LINENO" "$expected" "$output"
+}
+
 invoke_shunit
