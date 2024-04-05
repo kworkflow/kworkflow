3c11d233a373d7deefcd5ec5082d75c04c47bff3
JGBSouza <joaosouzaaa12@usp.br>
2024-04-04 22:09:16 -0300

    src: lib: kw_db: Add update_into function
    The database functions had no implementation for when we needed to
    update one or multiples attributes of an entry in the database without
    replacing it. In this case, add a new function update_into to make it
    possible.
    
    Reviewed-by: David Tadokoro <davidbtadokoro@gmail.com>
    Signed-off-by: JGBSouza <joaosouzaaa12@usp.br>
    Signed-off-by: David Tadokoro <davidbtadokoro@gmail.com>

--- src/lib/kw_db.sh
+++ src/lib/kw_db.sh
@@ -237,6 +237,61 @@ function select_from()
   cmd_manager "$flag" "$cmd"
 }
 
+# This function updates the set of values in the table of given database
+# with the given conditions.
+#
+# @table: Table to select info from
+# @_updates_array: An array reference of updates pairs that will be updated
+#   in the db
+# @pre_cmd: Pre command to execute
+# @_condition_array: An array reference of condition pairs specifing the data
+#   that will be updated
+# @flag: Flag to control function output
+# @db: Name of the database file
+# @db_folder: Path to the folder that contains @db
+#
+# Return:
+# 2 if db doesn't exist; 22 if table is empty
+# 0 if succesful; non-zero otherwise
+function update_into()
+{
+  local table="$1"
+  local _updates_array="$2"
+  local pre_cmd="$3"
+  local _condition_array="$4"
+  local flag=${5:-'SILENT'}
+  local db="${6:-"$DB_NAME"}"
+  local db_folder="${7:-"$KW_DATA_DIR"}"
+  local where_clause=''
+  local db_path
+  local query
+
+  db_path="$(join_path "$db_folder" "$db")"
+
+  if [[ ! -f "$db_path" ]]; then
+    complain 'Database does not exist'
+    return 2
+  fi
+
+  if [[ -z "$table" ]]; then
+    complain 'Empty table.'
+    return 22 # EINVAL
+  fi
+
+  if [[ -z "$_condition_array" || -z "$_updates_array" ]]; then
+    complain 'Empty condition or updates array.'
+    return 22 #EINVAL
+  fi
+
+  where_clause="$(generate_where_clause "$_condition_array")"
+  set_clause="$(generate_set_clause "$_updates_array")"
+
+  query="UPDATE ${table} SET ${set_clause} ${where_clause} ;"
+
+  cmd="sqlite3 -init "${KW_DB_DIR}/pre_cmd.sql" -cmd \"${pre_cmd}\" \"${db_path}\" -batch \"${query}\""
+  cmd_manager "$flag" "$cmd"
+}
+
 # This function receives a condition_array and then generate
 # the infos that will be used by the WHERE clause to specify
 # the data we want.
@@ -267,6 +322,29 @@ function generate_where_clause()
   done
 
   printf '%s' "${where_clause::-5}" # Remove trailing ' AND '
+}
+
+# This function receives a updates_array and then generates the infos that
+# will be used by the SET clause to update the data fields we want.
+#
+# @updates_array_ref: The updates array reference containing the conditions updates
+#
+# Returns:
+# A string containing the generated clause
+function generate_set_clause()
+{
+  local -n updates_array_ref="$1"
+  local attribute
+  local set_clause
+  local value
+
+  for attribute in "${!updates_array_ref[@]}"; do
+    value="${updates_array_ref["${attribute}"]}"
+    set_clause+="${attribute} = '${value}'"
+    set_clause+=', '
+  done
+
+  printf '%s' "${set_clause::-2}" # Remove trailing ', '
 }
 
 # This function takes arguments and assembles them into the correct format to
