c996eec22f6869a3f9ff3139e2346a62d62abbb2
JGBSouza <joaosouzaaa12@usp.br>
2024-05-03 00:13:39 -0300

    src: kw_manage_contacts: Add new function kw manager create-group
    To support the new feature `kw manage-contacts`, it should be able to
    create a new group. In this sense, add a new functionality `create-group`
    that receives a group name and creates a group in the `email_group`
    database with the group id, name, creation-date.
    
    Signed-off-by: JGBSouza <joaosouzaaa12@gmail.com>

--- src/kw_manage_contacts.sh
+++ src/kw_manage_contacts.sh
@@ -3,6 +3,7 @@
 include "${KW_LIB_DIR}/lib/kw_config_loader.sh"
 include "${KW_LIB_DIR}/lib/kwlib.sh"
 include "${KW_LIB_DIR}/lib/kw_string.sh"
+include "${KW_LIB_DIR}/send_patch.sh"
 
 # Hash containing user options
 declare -gA options_values
@@ -18,6 +19,7 @@ declare -Ag condition_array
 function manage_contacts_main()
 {
   local flag
+  local ret
 
   flag=${flag:-'SILENT'}
 
@@ -33,6 +35,135 @@ function manage_contacts_main()
     return 22 # EINVAL
   fi
 
+  if [[ "${options_values['GROUP_CREATE']}" ]]; then
+    create_email_group "${option_values['GROUP']}"
+
+    if [[ "$?" -eq 0 ]]; then
+      success "New group ${group_name} created successfully!"
+    fi
+  fi
+
+  return 0
+}
+
+# This function receives the name of a new group and then check for
+# incidences in the database and validate the group name before creating
+# the group.
+#
+# @group_name: The name of the group that will be created.
+#
+# Returns:
+# returns 0 if successful, non-zero otherwise
+function create_email_group()
+{
+  local group_name="$1"
+  local values
+
+  validate_group_name "$group_name"
+
+  if [[ "$?" -ne 0 ]]; then
+    return 22 # EINVAL
+  fi
+
+  check_existent_group "$group_name"
+
+  if [[ "$?" -ne 0 ]]; then
+    warning 'This group already exists'
+    return 22 # EINVAL
+  fi
+
+  create_group "$group_name"
+
+  if [[ "$?" -ne 0 ]]; then
+    return 22 # EINVAL
+  fi
+
+  return 0
+}
+
+# This function creates a new kw mail group
+#
+# @group_name: The name of the group that will be created.
+#
+# Returns:
+# returns 0 if successful, non-zero otherwise
+function create_group()
+{
+  local group_name="$1"
+  local sql_operation_result
+
+  values="$(format_values_db 1 "$group_name")"
+
+  sql_operation_result=$(insert_into "$DATABASE_TABLE_GROUP" '(name)' "$values" '' 'VERBOSE')
+  ret="$?"
+
+  if [[ "$ret" -eq 2 || "$ret" -eq 61 ]]; then
+    complain "$sql_operation_result"
+    return 22 # EINVAL
+  elif [[ "$ret" -ne 0 ]]; then
+    complain "($LINENO):" $'Error while inserting group into the database with command:\n' "${sql_operation_result}"
+    return 22 # EINVAL
+  fi
+
+  return 0
+}
+
+# This function validate a given group name
+#
+# @group_name: the group name that will be checked
+#
+# Return:
+# returns 0 if successful, 61 if group name is empty,
+# 75 if size of the group name is over 50 characters
+# and 22 if group name has invalid characters as:
+# [!, @, #, $, %, ^, &, (, ), (' ), (" ) and +].
+function validate_group_name()
+{
+  local group_name="$1"
+  local name_length
+  local has_special_character
+  local ret
+
+  if [[ -z "$group_name" ]]; then
+    complain 'The group name is empty'
+    return 61 # ENODATA
+  fi
+
+  name_length="$(str_length "$group_name")"
+
+  if [[ "$name_length" -ge 50 ]]; then
+    complain 'The group name must be less than 50 characters'
+    return 75 # OVERFLOW
+  fi
+
+  str_has_special_characters "$group_name"
+
+  if [[ "$?" -eq 0 ]]; then
+    complain 'The group name must not contain special characters'
+    return 22 #EINVAL
+  fi
+
+  return 0
+}
+
+# This function checks the existence of a given group
+#
+# @group_name: the group name that will be checked
+#
+# Return:
+# returns group id if group exists, 0 if it doesn't
+function check_existent_group()
+{
+  local group_name="$1"
+  local group_id
+
+  condition_array=(['name']="${group_name}")
+  group_id="$(select_from "$DATABASE_TABLE_GROUP" 'id' '' 'condition_array')"
+
+  if [[ -n "$group_id" ]]; then
+    return "$group_id"
+  fi
+
   return 0
 }
 
@@ -43,8 +174,8 @@ function parse_manage_contacts_options()
   local setup_token=0
   local patch_version=''
   local commit_count=''
-  local short_options=''
-  local long_options=''
+  local short_options='c:,'
+  local long_options='group-create:,'
   local pass_option_to_send_email
 
   options="$(kw_parse "$short_options" "$long_options" "$@")"
@@ -56,8 +187,20 @@ function parse_manage_contacts_options()
 
   eval "set -- $options"
 
+  options_values['GROUPS']=''
+  options_values['GROUP']=''
+  options_values['GROUP_CREATE']=''
+
   while [[ "$#" -gt 0 ]]; do
     case "$1" in
+      --group-create | -c)
+        options_values['GROUP_CREATE']=1
+        options_values['GROUP']="$2"
+        shift 2
+        ;;
+      --)
+        shift
+        ;;
     esac
   done
 
@@ -71,5 +214,6 @@ function manage_contacts_help()
     kworkflow_man 'manage-contacts'
     exit
   fi
-  printf '%s\n' 'kw manage-contacts:'
+  printf '%s\n' 'kw manage-contacts:' \
+    '  manage-contacts (-c | --group-create) [<name>] - create new group'
 }
---
+++ tests/unit/kw_manager_test.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+include './src/kw_manage_contacts.sh'
+include './tests/unit/utils.sh'
+
+function oneTimeSetUp()
+{
+  declare -g DB_FILES
+
+  export KW_ETC_DIR="${SHUNIT_TMPDIR}/etc/"
+  export KW_CACHE_DIR="${SHUNIT_TMPDIR}/cache/"
+  export KW_DATA_DIR="${SHUNIT_TMPDIR}"
+
+  DB_FILES="$(realpath './tests/unit/samples/db_files')"
+  KW_DB_DIR="$(realpath './database')"
+}
+
+function setUp()
+{
+  declare -gA options_values
+  declare -gA set_confs
+
+  setupDatabase
+}
+
+function tearDown()
+{
+  unset options_values
+  unset set_confs
+
+  tearDownDatabase
+}
+
+function setupDatabase()
+{
+  declare -g TEST_GROUP_NAME='TEST_GROUP'
+  declare -g TEST_GROUP_ID
+
+  execute_sql_script "${KW_DB_DIR}/kwdb.sql" > /dev/null 2>&1
+  sqlite3 "${KW_DATA_DIR}/kw.db" -batch "INSERT INTO \"${DATABASE_TABLE_GROUP}\" (name) VALUES (\"${TEST_GROUP_NAME}\");"
+  TEST_GROUP_ID="$(sqlite3 "${KW_DATA_DIR}/kw.db" -batch "SELECT id FROM \"${DATABASE_TABLE_GROUP}\" WHERE name='${TEST_GROUP_NAME}';")"
+}
+
+function tearDownDatabase()
+{
+  is_safe_path_to_remove "${KW_DATA_DIR}/kw.db"
+  if [[ "$?" == 0 ]]; then
+    rm "${KW_DATA_DIR}/kw.db"
+  fi
+}
+
+function test_validate_group_name()
+{
+  local expected
+  local output
+  local ret
+
+  # invalid values
+  output=$(validate_group_name '')
+  ret="$?"
+  expected='The group name is empty'
+  assert_equals_helper 'Empty group should not be valid' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected an error' "$LINENO" "$ret" 61
+
+  output=$(validate_group_name '012345678901234567890123456789012345678901234567890')
+  ret="$?"
+  expected='The group name must be less than 50 characters'
+  assert_equals_helper 'Group name length should not be valid' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected an error' "$LINENO" "$ret" 75
+
+  output=$(validate_group_name ',@#$!')
+  ret="$?"
+  expected='The group name must not contain special characters'
+  assert_equals_helper 'Special character in group name should not be valid' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected an error' "$LINENO" "$ret" 22
+
+  output=$(validate_group_name 'validName')
+  ret="$?"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 0
+}
+
+function test_check_existent_group()
+{
+  local expected
+  local output
+  local ret
+
+  sqlite3 "${KW_DATA_DIR}/kw.db" -batch "INSERT INTO \"${DATABASE_TABLE_GROUP}\" (name) VALUES ('existent_group');"
+
+  # invalid values
+  output=$(check_existent_group "$TEST_GROUP_NAME")
+  ret="$?"
+  assert_equals_helper 'Expected an error' "$LINENO" "$ret" "$TEST_GROUP_ID"
+
+  # valid values
+  output=$(check_existent_group 'unexistent_name')
+  ret="$?"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 0
+}
+
+function test_create_email_group()
+{
+  local expected
+  local output
+  local ret
+
+  # invalid cases
+  output=$(create_email_group "$TEST_GROUP_NAME")
+  ret="$?"
+  expected='This group already exists'
+  assert_equals_helper 'The group should not have been created' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 22
+
+  output=$(create_email_group '')
+  ret="$?"
+  expected='The group name is empty'
+  assert_equals_helper 'The group should not have been created' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 22
+
+  output=$(create_email_group '012345678901234567890123456789012345678901234567890')
+  ret="$?"
+  expected='The group name must be less than 50 characters'
+  assert_equals_helper 'The group should not have been created' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 22
+
+  output=$(create_email_group ',@#$!')
+  ret="$?"
+  expected='The group name must not contain special characters'
+  assert_equals_helper 'The group should not have been created' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 22
+
+  # valid values
+  create_email_group 'create_unexistent_email_group_name'
+  ret="$?"
+  output=$(sqlite3 "${KW_DATA_DIR}/kw.db" -batch "SELECT name FROM \"${DATABASE_TABLE_GROUP}\" WHERE name='create_unexistent_email_group_name';")
+  expected='create_unexistent_email_group_name'
+  assert_equals_helper 'The group should have been created' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 0
+}
+
+function test_create_group()
+{
+  local expected
+  local output
+  local ret
+
+  # invalid cases
+  output=$(create_group "$TEST_GROUP_NAME")
+  ret="$?"
+  expected='This group already exists'
+  expected=$'Error while inserting group into the database with command:\nsqlite3 -init '
+  expected+="${KW_DB_DIR}/kw.db ${KW_DATA_DIR}/kw.db"
+  expected+='-batch "INSERT INTO email_group (name) VALUES ('TEST_GROUP');"'
+  assertContains 'The group should not have been created' "$output" "$expected"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 22
+
+  # valid values
+  create_group 'create_unexistent_group'
+  ret="$?"
+  output=$(sqlite3 "${KW_DATA_DIR}/kw.db" -batch "SELECT name FROM \"${DATABASE_TABLE_GROUP}\" WHERE name='create_unexistent_group';")
+  expected='create_unexistent_group'
+  assert_equals_helper 'The group should have been created' "$LINENO" "$expected" "$output"
+  assert_equals_helper 'Expected no error' "$LINENO" "$ret" 0
+}
+
+function test_manage_contacts_parser()
+{
+  local expected
+  local output
+  local ret
+
+  parse_manage_contacts_options '--group-create' 'fake_group'
+  expected='1'
+  assert_equals_helper 'Set group-create' "$LINENO" "${options_values['GROUP_CREATE']}" "$expected"
+  expected='fake_group'
+  assert_equals_helper 'Set group-create' "$LINENO" "${options_values['GROUP']}" "$expected"
+
+  parse_manage_contacts_options '-c' 'fake_group'
+  expected='1'
+  assert_equals_helper 'Set group-create' "$LINENO" "${options_values['GROUP_CREATE']}" "$expected"
+  expected='fake_group'
+  assert_equals_helper 'Set group-create' "$LINENO" "${options_values['GROUP']}" "$expected"
+}
+
+invoke_shunit
