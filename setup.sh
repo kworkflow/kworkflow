07136e35f76aede5b2147579d1ebdc7da5e8aa59
Aquila Macedo <aquilamacedo@riseup.net>
2024-05-24 12:35:13 -0300

    setup: refactor check_dependencies function
    This commit refactors the check_dependencies function by extracting
    missing package detection into get_missing_packages, package
    installation into install_packages, updating check_dependencies to use
    these new functions, and adding function documentation.
    
    Reviewed-by: David Tadokoro <davidbtadokoro@usp.br>
    Signed-off-by: Aquila Macedo <aquilamacedo@riseup.net>
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- setup.sh
+++ setup.sh
@@ -51,39 +51,91 @@ declare -r CONFIGS_PATH='configs'
 
 declare -r DOCS_VIRTUAL_ENV='docs_virtual_env'
 
+# This function identifies the missing packages required by the distribution.
+#
+# @distro: The distribution name (arch, debian, fedora, etc.)
+# @deps_file: The path to the dependencies file for the distribution.
+#
+# Returns:
+# Outputs a space-separated string with the missing packages.
+function get_missing_packages()
+{
+  local distro="$1"
+  local deps_file="$2"
+  local package_list
+  local installed
+
+  if [[ "$distro" =~ 'arch' ]]; then
+    while IFS='' read -r package; do
+      pacman -Ql "$package" &> /dev/null
+      [[ "$?" != 0 ]] && package_list="${package} ${package_list}"
+    done < "$deps_file"
+  elif [[ "$distro" =~ 'debian' ]]; then
+    while IFS='' read -r package; do
+      installed=$(dpkg-query -W --showformat='${Status}\n' "$package" 2> /dev/null | grep -c 'ok installed')
+      [[ "$installed" -eq 0 ]] && package_list="${package} ${package_list}"
+    done < "$deps_file"
+  elif [[ "$distro" =~ 'fedora' ]]; then
+    while IFS='' read -r package; do
+      rpm -q "$package" &> /dev/null
+      [[ "$?" -ne 0 ]] && package_list="${package} ${package_list}"
+    done < "$deps_file"
+  fi
+
+  printf '%s\n' "$package_list"
+}
+
+# This function installs the given packages.
+#
+# @cmd: The installation command with the packages to be installed.
+#
+# Returns:
+# The return status of the installation command.
+function install_packages()
+{
+  local cmd="$1"
+
+  if [[ "$EUID" -eq 0 ]]; then
+    eval "$cmd"
+  else
+    eval "sudo ${cmd}"
+  fi
+}
+
+# This function checks and installs the necessary dependencies for the current
+# distribution.
+#
+# Returns:
+# Returns 0 if all dependencies are already installed or installation is
+# successful.
 function check_dependencies()
 {
-  local package_list=''
-  local cmd=''
+  local package_list
+  local cmd
   local distro
   local ret
 
   distro=$(detect_distro '/')
 
-  if [[ "$distro" =~ 'arch' ]]; then
-    while IFS='' read -r package; do
-      installed=$(pacman -Ql "$package" &> /dev/null)
-      [[ "$?" != 0 ]] && package_list="$package $package_list"
-    done < "$DOCUMENTATION/dependencies/arch.dependencies"
-    cmd="pacman -Sy --noconfirm ${package_list}"
-  elif [[ "$distro" =~ 'debian' ]]; then
-    while IFS='' read -r package; do
-      installed=$(dpkg-query -W --showformat='${Status}\n' "$package" 2> /dev/null | grep -c 'ok installed')
-      [[ "$installed" -eq 0 ]] && package_list="$package $package_list"
-    done < "$DOCUMENTATION/dependencies/debian.dependencies"
-    cmd="apt install -y $package_list"
-  elif [[ "$distro" =~ 'fedora' ]]; then
-    while IFS='' read -r package; do
-      installed=$(rpm -q "$package" &> /dev/null)
-      [[ "$?" -ne 0 ]] && package_list="$package $package_list"
-    done < "$DOCUMENTATION/dependencies/fedora.dependencies"
-    cmd="dnf install -y $package_list"
-  else
-    warning 'Unfortunately, we do not have official support for your distro (yet)'
-    warning 'Please, try to find the following packages:'
-    warning "$(cat "$DOCUMENTATION/dependencies/arch.dependencies")"
-    return 0
-  fi
+  package_list=$(get_missing_packages "$distro" "${DOCUMENTATION}/dependencies/${distro}.dependencies")
+
+  case "$distro" in
+    arch*)
+      cmd="pacman -Sy --noconfirm ${package_list}"
+      ;;
+    debian*)
+      cmd="apt install -y ${package_list}"
+      ;;
+    fedora*)
+      cmd="dnf install -y ${package_list}"
+      ;;
+    *)
+      warning 'Unfortunately, we do not have official support for your distro (yet)'
+      warning 'Please, try to find the following packages:'
+      warning "$(cat "${DOCUMENTATION}/dependencies/arch.dependencies")"
+      return 0
+      ;;
+  esac
 
   if [[ -n "$package_list" ]]; then
     if [[ "$FORCE" == 0 ]]; then
@@ -93,12 +145,7 @@ function check_dependencies()
       fi
     fi
 
-    # Install system packages
-    if [[ "$EUID" -eq 0 ]]; then
-      eval "$cmd"
-    else
-      eval "sudo $cmd"
-    fi
+    install_packages "$cmd"
     ret="$?"
 
     # Installation failed...
@@ -106,7 +153,6 @@ function check_dependencies()
       complain '[ERROR] Dependencies installation has failed. Aborting kw installation...'
       exit "$ret"
     fi
-
   fi
 }
 
