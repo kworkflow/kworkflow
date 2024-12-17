kworkflow/kworkflow

commit a330623b7b3846510f570257bfc6f1f4507c6388

src: config: Introduce config option
Whenever users want to change a permanent config option, they have to
open the config file, which is not friendly behavior. This commit
introduces a new option named config, which provides a CLI for dealing
with our config file. Notice that I tried to make it as close as
possible to the git config option.

Signed-off-by: Rodrigo Siqueira <siqueirajordao@riseup.net>

src/bash_autocomplete.sh
@@ -15,7 +15,7 @@ function _kw_autocomplete()
 
   kw_options['kw']='backup bd build clear-cache codestyle kernel-config-manager debug deploy
                     device diff drm explore help init maintainers mail man mount
-                    pomodoro report ssh umount up vars version'
+                    pomodoro report ssh umount up vars version config'
 
   kw_options['backup']='--restore --force --help'
 
@@ -62,6 +62,8 @@ function _kw_autocomplete()
 
   kw_options['ssh']='--command --script'
   kw_options['s']="${kw_options['ssh']}"
+
+  kw_options['config']='--local --global'
 
   mapfile -t COMPREPLY < <(compgen -W "${kw_options[${previous_command}]} " -- "${current_command}")
 

documentation/conf.py
@@ -160,6 +160,7 @@ man_pages = [
     ('man/features/build', 'build', 'build the kernel', [author], 1),
     ('man/features/codestyle', 'codestyle', 'checkpatch wrapper', [author], 1),
     ('man/features/kernel-config-manager', 'kernel-config-manager', 'config manager', [author], 1),
+    ('man/features/config', 'config', 'config manager', [author], 1),
     ('man/features/debug', 'debug', 'kernel debug', [author], 1),
     ('man/features/deploy', 'deploy', 'deploy the kernel', [author], 1),
     ('man/features/device', 'device', 'hardware information', [author], 1),

kw
@@ -150,6 +150,13 @@ function kw()
         kernel_config_manager_main "$@"
       )
       ;;
+    config | g)
+      (
+        include "$KW_LIB_DIR/config.sh"
+
+        config_main "$@"
+      )
+      ;;
     help | --help)
       (
         include "$KW_LIB_DIR/help.sh"

src/config.sh
@@ -1,0 +1,204 @@
+include "$KW_LIB_DIR/kwlib.sh"
+include "$KW_LIB_DIR/kwio.sh"
+
+declare -gA options_values
+
+# List of config files with possible values
+declare -gA config_file_list=(
+  ['build']='arch kernel_img_name cross_compile menu_config doc_type
+             cpu_scaling_factor enable_ccache warning_level use_llvm'
+  ['kworkflow']='ssh_user ssh_ip ssh_port kw_files_remote_path
+             deploy_temporary_files_path deploy_default_compression
+             strip_modules_debug_option ssh_configfile hostname dtb_copy_pattern
+             virtualizer mount_point qemu_hw_options qemu_net_options alert
+             sound_alert_command visual_alert_command default_deploy_target
+             reboot_after_deploy disable_statistics_data_track gui_on gui_off
+             send_opts blocked_emails checkpatch_opts get_maintainer_opts'
+)
+
+function config_main()
+{
+  local parameters
+  local dot_separator
+  local target_config_file
+  local option_and_value
+  local config_options
+  local base_path="${PWD}/.kw"
+  local option
+  local value
+
+  if [[ "$1" =~ -h|--help ]]; then
+    config_help "$1"
+    exit 0
+  fi
+
+  parse_config_options "$@"
+  if [[ "$?" != 0 ]]; then
+    complain "Invalid option: ${options_values['ERROR']}"
+    return 22 # EINVAL
+  fi
+
+  # Validate and decompose options
+  parameters="${options_values['PARAMETERS']}"
+
+  dot_separator=$(str_count_char_repetition "$parameters" '.')
+  if [[ "$dot_separator" != 1 ]]; then
+    complain "Invalid options: ${parameters}"
+    complain "Try: target_config.option value"
+    return 22 # EINVAL
+  fi
+
+  # option value
+  target_config_file=$(printf '%s' "$parameters" | cut -d '.' -f 1)
+  option_and_value="${parameters#*.}"
+  option=$(printf '%s' "$option_and_value" | cut -d ' ' -f 1)
+  value=$(printf '%s' "$option_and_value" | cut -d ' ' -f 2)
+
+  # Check if config files are valid
+  if ! is_config_file_valid "$target_config_file"; then
+    complain "Invalid config option: ${target_config_file}"
+    return 22 # EINVAL
+  fi
+
+  # Check config option
+  if ! is_a_valid_config_option "$target_config_file" "$option"; then
+    return 22 # EINVAL
+  fi
+
+  if [[ -z "$value" ]]; then
+    complain 'You did not specify a value to be set'
+    return 22 # EINVAL
+  fi
+
+  if [[ "${options_values['SCOPE']}" == 'global' ]]; then
+    base_path="${KW_ETC_DIR}"
+  fi
+
+  target_config_file+='.config'
+  base_path=$(join_path "$base_path" "$target_config_file")
+  set_config_value "$option" "$value" "$base_path"
+}
+
+# The associative array config_file_list uses the target config file as a key
+# and the options as a value; this function checks if the parameter gets as an
+# attribute is part of the config option.
+#
+# @target_config_file String with the config name
+#
+# Return:
+# In case of success return 0, otherwise, return 22.
+function is_config_file_valid()
+{
+  local target_config_file="$1"
+
+  if [[ ! "${!config_file_list[*]}" =~ ${target_config_file} ]]; then
+    return 22 # EINVAL
+  fi
+
+  return 0
+}
+
+# The associative array config_file_list has the target file as a key followed
+# by the valid options. This function check if the target option is valid for
+# the specific config file.
+#
+# @target_config_file String with the config name
+# @option Option used in the config file
+#
+# Return:
+# In case of success return 0, otherwise, return 22.
+function is_a_valid_config_option()
+{
+  local target_config_file="$1"
+  local option="$2"
+  local config_options
+
+  # Check config option
+  if [[ -z "$option" ]]; then
+    complain 'You did not specify a target option'
+    return 22 # EINVAL
+  fi
+
+  config_options="${config_file_list[$target_config_file]}"
+
+  if [[ ! "$config_options" == *"${option}"* ]]; then
+    complain "The ${target_config_file} config, does not support the ${option} option"
+    return 95 # ENOTSUP
+  fi
+}
+
+# This function sets variables in the config file to a specified value.
+#
+# @option: option name in kw config file
+# @value: value to set option to
+#
+# Return:
+# In case of success return 0, otherwise, return 22.
+function set_config_value()
+{
+  local option="$1"
+  local value="$2"
+  local path="$3"
+
+  path=${path:-"${PWD}/${KW_DIR}/${name}"}
+
+  sed -i -r "s/($option=).*/\1$value/" "$path"
+  sed -i -r "s/#\s*$option/$option/" "$path"
+}
+
+function parse_config_options()
+{
+  local long_options='help,global,local'
+  local short_options='h,g,l'
+
+  options="$(kw_parse "$short_options" "$long_options" "$@")"
+
+  if [[ "$?" != 0 ]]; then
+    options_values['ERROR']="$(kw_parse_get_errors 'kw config' "$short_options" \
+      "$long_options" "$@")"
+    return 22 # EINVAL
+  fi
+
+  # Default values
+  options_values['SCOPE']='local'
+  options_values['PARAMETERS']=''
+
+  eval "set -- $options"
+
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      --help | -h)
+        config_help "$1"
+        exit
+        ;;
+      --global | -g)
+        options_values['SCOPE']='global'
+        shift
+        ;;
+      --local | -l)
+        options_values['SCOPE']='local'
+        shift
+        ;;
+      --)
+        shift
+        ;;
+      *)
+        options_values['PARAMETERS']+="$1 "
+        shift
+        ;;
+    esac
+  done
+}
+
+function config_help()
+{
+  if [[ "$1" == --help ]]; then
+    include "$KW_LIB_DIR/help.sh"
+    kworkflow_man 'config'
+    return
+  fi
+  printf '%s\n' 'kw config <config.option value>:' \
+    '  config - Show config values' \
+    '  config (-g | --global) <config.option value> - Change global config' \
+    '  config (-l | --local) <config.option value> - Change local config'
+}

tests/config_test.sh
@@ -1,0 +1,119 @@
+#!/bin/bash
+
+include './src/config.sh'
+include './tests/utils.sh'
+
+function setUp()
+{
+  export KW_CONFIG_BASE_PATH="${SHUNIT_TMPDIR}/.kw"
+  export ORIGINAL_PATH="$PWD"
+
+  mkdir -p "${KW_CONFIG_BASE_PATH}"
+
+  # Copy sample files
+  cp "${KW_BUILD_CONFIG_SAMPLE}" "${KW_CONFIG_BASE_PATH}"
+  cp "${KW_CONFIG_SAMPLE_X86}" "${KW_CONFIG_BASE_PATH}"
+
+  # Let's run all test in a well-contained folder
+  cd "${KW_CONFIG_BASE_PATH}" || {
+    fail "($LINENO): setUp: It was not possible to move into ${KW_CONFIG_BASE_PATH}"
+    return
+  }
+}
+
+function tearDown()
+{
+  cd "$ORIGINAL_PATH" || {
+    fail "($LINENO): tearDown: It was not possible to move into ${ORIGINAL_PATH}"
+    return
+  }
+
+  rm -rf "${KW_CONFIG_BASE_PATH}"
+}
+
+function test_is_config_file_valid()
+{
+  is_config_file_valid 'invalid'
+  assertEquals "($LINENO)" "$?" 22
+
+  is_config_file_valid 'builds'
+  assertEquals "($LINENO)" "$?" 22
+
+  is_config_file_valid 'kworkflows'
+  assertEquals "($LINENO)" "$?" 22
+
+  # Valid options
+  is_config_file_valid 'kworkflow'
+  assertEquals "($LINENO)" "$?" 0
+
+  is_config_file_valid 'build'
+  assertEquals "($LINENO)" "$?" 0
+}
+
+function test_is_a_valid_config_option_only_valid_options()
+{
+  is_a_valid_config_option 'build' 'cross_compile'
+  assertEquals "($LINENO)" "$?" 0
+
+  is_a_valid_config_option 'kworkflow' 'ssh_ip'
+  assertEquals "($LINENO)" "$?" 0
+}
+
+function test_is_a_valid_config_invalid_parameters()
+{
+  local output
+
+  output=$(is_a_valid_config_option 'build')
+  assertEquals "($LINENO)" "$?" 22
+
+  output=$(is_a_valid_config_option 'kworkflow')
+  assertEquals "($LINENO)" "$?" 22
+
+  output=$(is_a_valid_config_option 'kworkflow' 'this_is_invalid')
+  assertEquals "($LINENO)" "$?" 95
+
+  output=$(is_a_valid_config_option 'build' 'nop')
+  assertEquals "($LINENO)" "$?" 95
+}
+
+function test_set_config_value_changing_default_value()
+{
+  local output
+
+  set_config_value 'use_llvm' 'lala' "${KW_CONFIG_BASE_PATH}/build.config"
+  output=$(grep 'use_llvm' "${KW_CONFIG_BASE_PATH}/build.config")
+  assert_equals_helper 'Change llvm' "($LINENO)" "$output" 'use_llvm=lala'
+
+  set_config_value 'menu_config' 'menuconfig' "${KW_CONFIG_BASE_PATH}/build.config"
+  output=$(grep 'menu_config' "${KW_CONFIG_BASE_PATH}/build.config")
+  assert_equals_helper 'Change llvm' "($LINENO)" "$output" 'menu_config=menuconfig'
+}
+
+function test_parse_config_options()
+{
+  unset options_values
+  declare -gA options_values
+
+  parse_config_options
+  assert_equals_helper 'Expected local as a default scope' \
+    "($LINENO)" 'local' "${options_values['SCOPE']}"
+
+  # test default options
+  parse_config_options --global
+
+  assert_equals_helper 'Set global scope' \
+    "($LINENO)" 'global' "${options_values['SCOPE']}"
+
+  parse_config_options --local
+  assert_equals_helper 'Set local scope' \
+    "($LINENO)" 'local' "${options_values['SCOPE']}"
+
+  parse_config_options 'build.something=xpto'
+  assert_equals_helper 'Expected <build.something=xpto>' \
+    "($LINENO)" 'build.something=xpto ' "${options_values['PARAMETERS']}"
+
+  parse_config_options --invalid
+  assertEquals "($LINENO)" 22 "$?"
+}
+
+invoke_shunit

commit e15e06c3f7955c22cca942562767636b94ff8967

Add missing `g` to bash completion
The long `config` bash completion also has a short option `g`
that can be used to enter it.

Fixes: a330623 ("src: config: Introduce config option")
Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

src/bash_autocomplete.sh
@@ -50,6 +50,7 @@ function _kw_autocomplete()
   kw_options['k']="${kw_options['kernel-config-manager']}"
 
   kw_options['config']='--local --global --show --help --verbose'
+  kw_options['g']="${kw_options['config']}"
 
   kw_options['remote']='--add --remove --rename --list --global --set-default --verbose'
 
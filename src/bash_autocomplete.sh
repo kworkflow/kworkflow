e15e06c3f7955c22cca942562767636b94ff8967
Mario Limonciello <mario.limonciello@amd.com>
2024-12-17 00:24:10 -0600

    Add missing `g` to bash completion
    The long `config` bash completion also has a short option `g`
    that can be used to enter it.
    
    Fixes: a330623 ("src: config: Introduce config option")
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- src/bash_autocomplete.sh
+++ src/bash_autocomplete.sh
@@ -50,6 +50,7 @@ function _kw_autocomplete()
   kw_options['k']="${kw_options['kernel-config-manager']}"
 
   kw_options['config']='--local --global --show --help --verbose'
+  kw_options['g']="${kw_options['config']}"
 
   kw_options['remote']='--add --remove --rename --list --global --set-default --verbose'
 
