9c62bac8cb890c6f826aa43335e7263a0e280390
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2025-06-19 15:58:29 -0300

    src: kw_env: Adjust functions to use a encoded PWD as a cache_build_path
    This commit implements get_encoded_pwd function in the global context
    for solving duplicated env_names in different kernel trees. For every
    env inside a tree, it will be created a folder in the global context
    with its encoded pwd. The proper env_name folder will be created inside
    it.
    
    Signed-off-by: Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
    Co-developed-by: Nicolas Borsari <nicborsari@usp.br>
    Signed-off-by: Nicolas Borsari <nicborsari@usp.br>

--- src/kw_env.sh
+++ src/kw_env.sh
@@ -194,6 +194,7 @@ function create_new_env()
   local local_kw_build_config="${local_kw_configs}/build.config"
   local local_kw_deploy_config="${local_kw_configs}/deploy.config"
   local env_name=${options_values['CREATE']}
+  local encoded_pwd=$(get_encoded_pwd)
   local cache_build_path="$KW_CACHE_DIR"
   local current_env_name
   local output
@@ -237,18 +238,18 @@ function create_new_env()
   done
 
   # Handle build and config folder
-  cmd="mkdir -p ${cache_build_path}/${ENV_DIR}/${env_name}"
+  cmd="mkdir --parents ${cache_build_path}/${ENV_DIR}/${encoded_pwd}/${env_name}"
   cmd_manager "$flag" "$cmd"
 
   current_env_name=$(get_current_env_name)
   ret="$?"
   # If we already have an env, we should copy the config file from it.
   if [[ "$ret" == 0 ]]; then
-    cmd="cp ${cache_build_path}/${ENV_DIR}/${current_env_name}/.config ${cache_build_path}/${ENV_DIR}/${env_name}/.config"
+    cmd="cp ${cache_build_path}/${ENV_DIR}/${encoded_pwd}/${current_env_name}/.config ${cache_build_path}/${ENV_DIR}/${encoded_pwd}/${env_name}/.config"
     cmd_manager "$flag" "$cmd"
     return
   elif [[ -f "${PWD}/.config" ]]; then
-    cmd="cp ${PWD}/.config ${cache_build_path}/${ENV_DIR}/${env_name}"
+    cmd="cp ${PWD}/.config ${cache_build_path}/${ENV_DIR}/${encoded_pwd}/${env_name}"
     cmd_manager "$flag" "$cmd"
     return
   fi
@@ -258,10 +259,10 @@ function create_new_env()
   warning 'It is recommended to use kw kernel-config-manager.'
 
   if [[ -e /proc/config.gz ]]; then
-    cmd="zcat /proc/config.gz > ${cache_build_path}/${ENV_DIR}/${env_name}/.config"
+    cmd="zcat /proc/config.gz > ${cache_build_path}/${ENV_DIR}/${encoded_pwd}/${env_name}/.config"
     cmd_manager "$flag" "$cmd"
   elif [[ -e "/boot/config-$(uname -r)" ]]; then
-    cmd="cp /boot/config-$(uname -r) ${cache_build_path}/${ENV_DIR}/${env_name}/.config"
+    cmd="cp /boot/config-$(uname -r) ${cache_build_path}/${ENV_DIR}/${encoded_pwd}/${env_name}/.config"
     cmd_manager "$flag" "$cmd"
   else
     warning 'kw was not able to find any valid config file for the new env'
@@ -276,6 +277,7 @@ function destroy_env()
   local cache_build_path="$KW_CACHE_DIR"
   local current_env
   local env_name=${options_values['DESTROY']}
+  local encoded_pwd=$(get_encoded_pwd)
   local cmd
 
   flag=${flag:-'SILENT'}
@@ -302,7 +304,7 @@ function destroy_env()
     fi
   fi
 
-  cmd="rm -rf ${local_kw_configs:?}/${ENV_DIR}/${env_name} && rm -rf ${cache_build_path:?}/${ENV_DIR}/${env_name}"
+  cmd="rm --recursive --force ${local_kw_configs:?}/${ENV_DIR}/${env_name} && rm -rf ${cache_build_path:?}/${ENV_DIR}/${encoded_pwd}/${env_name}"
   cmd_manager "$flag" "$cmd"
   success "The \"${env_name}\" environment has been destroyed."
 }
@@ -344,14 +346,14 @@ function list_env_available_envs()
   if [[ -f "${local_kw_configs}/${ENV_CURRENT_FILE}" ]]; then
     current_env=$(< "${local_kw_configs}/${ENV_CURRENT_FILE}")
     say 'Current env:'
-    printf ' -> %s: %s\n\n' "$current_env" "${KW_CACHE_DIR}/${ENV_DIR}/${current_env}"
+    printf ' -> %s: %s\n\n' "$current_env" "${KW_CACHE_DIR}/${ENV_DIR}/$(get_encoded_pwd)/${current_env}"
   fi
 
   warning 'Other kw environments:'
   # For the below loop, we want to split the array
   # shellcheck disable=SC2068
   for env in ${all_envs[@]}; do
-    printf ' * %s: %s\n' "$env" "${KW_CACHE_DIR}/${ENV_DIR}/${env}"
+    printf ' * %s: %s\n' "$env" "${KW_CACHE_DIR}/${ENV_DIR}/$(get_encoded_pwd)/${env}"
   done
 }
 
